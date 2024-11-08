44d4ca2cf72a7b03dced9554878ee1effa131f42
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2024-11-08 12:07:57 +0000

    Make API query DB

--- api/DHSC.FingertipsNext.Api/DHSC.FingertipsNext.Api/Controllers/WeatherForecastController.cs
+++ api/DHSC.FingertipsNext.Api/DHSC.FingertipsNext.Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace DHSC.FingerTipsNext.Api.Controllers;
 
@@ -12,21 +13,66 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly string _connectionString;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        var dbServer = Environment.GetEnvironmentVariable("DB_SERVER");
+        var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+        var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+        var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        if (dbServer == null || dbName == null || dbUser == null || dbPassword == null)
+        {
+            throw new ArgumentException("Invalid environment variables provided. " +
+                                        "Check DB_SERVER, DB_NAME, DB_USER & DB_PASSWORD have been set appropriately");
+        }
+
+        var builder = new SqlConnectionStringBuilder
+        {
+            DataSource = dbServer,
+            UserID = dbUser,
+            Password = dbPassword,
+            InitialCatalog = dbName,
+        };
+
+        _connectionString = builder.ConnectionString;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+        try
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            const string sql = "SELECT TOP (1000) [Id], [Date], [TemperatureC], [Summary] FROM [fingertips-db].[dbo].[WeatherForecast]";
+            await using var command = new SqlCommand(sql, connection);
+            await using var reader = await command.ExecuteReaderAsync();
+
+            var forecasts = new List<WeatherForecast>();
+            while (await reader.ReadAsync())
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var forecast = new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(reader.GetDateTime(1)),
+                    TemperatureC = reader.GetInt32(2),
+                    Summary = reader.GetString(3),
+                };
+                forecasts.Add(forecast);
+            }
+
+            return forecasts;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to retrieve weather forecasts from database. {connectionString}",
+                [_connectionString]);
+            throw;
+        }
     }
 }
