4981c5d081241e58c1e00b4016f42dd7bb7d77cc
Fawad Ali <alibombaye@users.noreply.github.com>
2024-11-28 17:22:36 +0000

    add server action on search form

--- frontend/fingertips-frontend/components/forms/SearchForm/index.tsx
+++ frontend/fingertips-frontend/components/forms/SearchForm/index.tsx
@@ -1,11 +1,21 @@
 'use client'
 
+import { searchIndicator, State } from "@/lib/actions/searchActions";
 import { Button, InputField } from "govuk-react"
+import { useActionState } from "react";
 
 export const SearchForm = () => {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(searchIndicator, initialState);
+  
   return (
-    <form>
+    <form action={formAction}>
       <InputField
+        id='indicator1'
+        input={{
+          id: 'indicator',
+          name: 'indicator'
+        }}
         hint={<>Type in here the IndicatorId.</>}
         data-testid='input-indicator-search'
       >
---
+++ frontend/fingertips-frontend/lib/actions/searchActions.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import { z } from 'zod';
+import { redirect } from 'next/navigation';
+
+const $SearchFormSchema = z.object({
+  indicator: z.string({
+    invalid_type_error: 'Please enter an indicator id',
+  })
+});
+
+export type State = {
+  errors?: {
+    indicator?: string[],
+  },
+  message?: string | null;
+}
+
+
+export type SearchForm = z.infer<typeof $SearchFormSchema>;
+
+export type SearchFormState<T = SearchForm> = State & T;
+
+export async function searchIndicator(prevState: SearchFormState, formData: FormData) {
+  console.log(`formData ${JSON.stringify(formData)}`);
+
+  const validatedFields = $SearchFormSchema.safeParse({
+    indicator: formData.get('indicator'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields'
+    }
+  }
+
+  const { indicator } = validatedFields.data;
+
+  console.log(`indicator ${indicator}`);
+
+  redirect(`/search?indicator=${indicator}`);
+}
