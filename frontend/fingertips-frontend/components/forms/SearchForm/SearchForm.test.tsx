5f7b66a2a05b4aec2b1d914ddbc0203400ff92a5
Fawad Ali <alibombaye@users.noreply.github.com>
2024-11-28 16:06:03 +0000

    update tests

--- frontend/fingertips-frontend/components/forms/SearchForm/SearchForm.test.tsx
+++ frontend/fingertips-frontend/components/forms/SearchForm/SearchForm.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { SearchForm } from '.';
+import { SearchForm } from '@/components/forms/SearchForm';
 
 test('snapshot test - renders the form', () => {
   const container = render(
