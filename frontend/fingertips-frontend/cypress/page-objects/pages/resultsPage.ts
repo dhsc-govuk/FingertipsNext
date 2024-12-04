4e4c0e076a1a58a312210f21322796a68cfca1c2
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2024-12-04 16:51:47 +0000

    Update frontend/fingertips-frontend/cypress/page-objects/pages/resultsPage.ts
    Co-authored-by: Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
    Signed-off-by: williamwillis11 <92571853+williamwillis11@users.noreply.github.com>

--- frontend/fingertips-frontend/cypress/page-objects/pages/resultsPage.ts
+++ frontend/fingertips-frontend/cypress/page-objects/pages/resultsPage.ts
@@ -2,6 +2,7 @@ import BasePage from '../basePage';
 
 export default class ResultsPage extends BasePage {
   readonly resultsText = 'You searched for indicator';
+  
   checkSearchResults = (contains: string) => {
     return cy.contains(this.resultsText + ` "${contains}"`);
   };
