12d6b0658ff52525036913df77a13e56b256cf96
Fawad Ali <alibombaye@users.noreply.github.com>
2024-12-05 08:54:38 +0000

    fix prettier issue

--- frontend/fingertips-frontend/cypress/page-objects/pages/resultsPage.ts
+++ frontend/fingertips-frontend/cypress/page-objects/pages/resultsPage.ts
@@ -2,7 +2,7 @@ import BasePage from '../basePage';
 
 export default class ResultsPage extends BasePage {
   readonly resultsText = 'You searched for indicator';
-  
+
   checkSearchResults = (contains: string) => {
     return cy.contains(this.resultsText + ` "${contains}"`);
   };
