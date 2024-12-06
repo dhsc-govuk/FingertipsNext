e06294cf4b2231158e99acc4b31dbea4cd3c18d0
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2024-12-06 13:44:18 +0000

    fix lint error

--- frontend/fingertips-frontend/lib/registry.test.tsx
+++ frontend/fingertips-frontend/lib/registry.test.tsx
@@ -1,5 +1,5 @@
 import { expect } from '@jest/globals';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import StyledComponentsRegistry from './registry';
 
 const SomeComponent = () => {
