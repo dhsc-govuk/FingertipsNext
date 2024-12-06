16f79f49241f16b1b19b55a6c5427ac546c38ac9
steves-bjss <83714226+steves-bjss@users.noreply.github.com>
2024-12-06 16:29:58 +0000

    FTN-14 Tech Setup (#10)
    Signed-off-by: steves-bjss <83714226+steves-bjss@users.noreply.github.com>
    Co-authored-by: Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>

---
+++ api/DHSC.FingertipsNext.Modules.Search.ModuleInterfaces/ISearchController.cs
@@ -0,0 +1,10 @@
+﻿namespace DHSC.FingertipsNext.Modules.Search.ModuleInterfaces;
+
+/// <summary>
+/// Sample interface for cross-module communication to interface with the search module 
+/// </summary>
+/// <param name="searchTerm">The term to search for.</param>
+public interface ISearchController
+{
+    public string Search(string searchTerm);
+}
