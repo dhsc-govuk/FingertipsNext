82e4fba9b1d8e0795973227a3f69728f92dbc69d
Andrew Evans <andrew.evans@bjss.com>
2024-12-09 17:17:02 +0000

    fix typo

--- database/fingertips-db/Tables/HealthMeasure.sql
+++ database/fingertips-db/Tables/HealthMeasure.sql
@@ -16,11 +16,11 @@ CREATE TABLE [dbo].[HealthMeasure](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[HealthMeasure]  WITH CHECK ADD  CONSTRAINT [FK_HealthMeasure_AgeDimesnion] FOREIGN KEY([AgeKey])
+ALTER TABLE [dbo].[HealthMeasure]  WITH CHECK ADD  CONSTRAINT [FK_HealthMeasure_AgeDimension] FOREIGN KEY([AgeKey])
 REFERENCES [dbo].[AgeDimension] ([AgeKey])
 GO
 
-ALTER TABLE [dbo].[HealthMeasure] CHECK CONSTRAINT [FK_HealthMeasure_AgeDimesnion]
+ALTER TABLE [dbo].[HealthMeasure] CHECK CONSTRAINT [FK_HealthMeasure_AgeDimension]
 GO
 ALTER TABLE [dbo].[HealthMeasure]  WITH CHECK ADD  CONSTRAINT [FK_HealthMeasure_AreaDimension] FOREIGN KEY([AreaKey])
 REFERENCES [dbo].[AreaDimension] ([AreaKey])
