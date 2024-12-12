5b9c0cbf98275f6c9ea7e2e05ea224822b15f361
adawari-thomas-bjss <adawari.thomas@bjss.com>
2024-12-12 12:02:16 +0000

    FTN-72: Create search results page

--- frontend/fingertips-frontend/playwright/page-objects/pages/resultsPage.ts
+++ frontend/fingertips-frontend/playwright/page-objects/pages/resultsPage.ts
@@ -4,15 +4,17 @@ import { expect } from '../pageFactory';
 export default class ResultsPage extends BasePage {
   readonly resultsText = 'You searched for indicator';
   readonly backLink = 'search-results-back-link';
+  readonly searchResult = 'search-result';
 
   async checkSearchResults(searchTerm: string) {
     await expect(
       this.page.getByText(this.resultsText + ` "${searchTerm}"`)
     ).toBeVisible();
+    await expect(this.page.getByTestId(this.searchResult)).toHaveCount(2);
   }
 
   async clickBackLink() {
-    await this.page.getByTestId(this.backLink);
+    await this.page.getByTestId(this.backLink).click();
   }
 
   async checkURLIsCorrect(indicator: string) {
