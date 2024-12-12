eba85024fa5bc6afe536a687205af0d205c10d07
Priya Thorpe <priya.thorpe@bjss.com>
2024-12-12 11:35:45 +0000

    unit tests

---
+++ frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
@@ -0,0 +1,40 @@
+import {render, screen} from "@testing-library/react";
+import {LineChart} from "@/components/pages/chart/index";
+import {expect} from "@jest/globals";
+
+const mockData = [
+    {
+        "date": "2024-11-01",
+        "temperatureC": -30,
+        "temperatureF": -21,
+        "summary": "Freezing"
+    },
+    {
+        "date": "2024-11-01",
+        "temperatureC": 0,
+        "temperatureF": 32,
+        "summary": "Bracing"
+    }
+];
+
+
+test('snapshot test - should render the chart', () => {
+    const container = render(<LineChart data={mockData}/>);
+
+    expect(container.asFragment()).toMatchSnapshot();
+});
+
+test('should render the HighchartsReact component', () => {
+    render(<LineChart data={mockData} />);
+
+    expect(screen.getByTestId('highcharts-react-component')).toBeInTheDocument();
+})
+
+
+test('should pass the options to highcharts', () => {
+    render(<LineChart data={mockData} />);
+    
+    const chart = screen.getByTestId('highcharts-react-component')
+    expect(chart).toHaveTextContent('Weather Forecast')
+    expect(chart).toHaveTextContent('Temperature (°C)')
+})
