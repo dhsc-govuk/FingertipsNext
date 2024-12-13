4d2820e1b9447a9b1fec500094aea0b5967336c2
Priya Thorpe <priya.thorpe@bjss.com>
2024-12-13 17:09:48 +0000

    some fixes for check failures

--- frontend/fingertips-frontend/app/chart/page.tsx
+++ frontend/fingertips-frontend/app/chart/page.tsx
@@ -1,7 +1,15 @@
 import { LineChart } from '@/components/pages/chart';
+import {connection} from "next/server";
 
 async function fetchLineChartData() {
-  const response = await fetch('http://localhost:5144/weatherforecast');
+  // We don't want to render this page statically
+  await connection();
+  
+  const response = await fetch('http://localhost:5144/weatherforecast', {
+    // Cache the data for 60s
+    next: { revalidate: 60 },
+  });
+  
   if (!response.ok) {
     throw new Error('Failed to fetch forecast data');
   }
