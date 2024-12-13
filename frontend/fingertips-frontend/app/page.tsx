e7b1ee813b779b02fd001d6a6bd281fd38945d69
Fawad Ali <alibombaye@users.noreply.github.com>
2024-12-13 16:01:47 +0000

    pass config to API to override basePath

--- frontend/fingertips-frontend/app/page.tsx
+++ frontend/fingertips-frontend/app/page.tsx
@@ -1,11 +1,18 @@
 import { HomePage } from '@/components/pages/home';
-import { WeatherForecastApi } from '@/generated-sources/api-client/src';
+import {
+  Configuration,
+  WeatherForecastApi,
+} from '@/generated-sources/api-client/src';
 import { connection } from 'next/server';
 
 export default async function Home() {
   await connection();
 
-  const forecastApi = new WeatherForecastApi();
+  const config: Configuration = new Configuration({
+    basePath: process.env.FINGERTIPS_API_URL,
+  });
+
+  const forecastApi = new WeatherForecastApi(config);
   const forecasts = await forecastApi.getWeatherForecast();
 
   return <HomePage forecasts={forecasts} />;
