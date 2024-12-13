262b6977e5abde3c5c5762432be5ae4e211ff33a
Steve Scott <steve.scott@bjss.com>
2024-12-13 14:16:41 +0000

    FTN-37 - Front end monitoring / logging

--- frontend/fingertips-frontend/next.config.ts
+++ frontend/fingertips-frontend/next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  serverExternalPackages: ['@azure/monitor-opentelemetry',
+    '@opentelemetry/api',
+    '@opentelemetry/resources',
+    '@opentelemetry/sdk-metrics',
+    '@opentelemetry/sdk-node',
+    '@opentelemetry/sdk-trace-base',
+    '@opentelemetry/semantic-conventions',
+    '@opentelemetry/exporter-jaeger'],
 };
 
 export default nextConfig;
+
+
