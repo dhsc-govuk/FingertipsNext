99dff0abe16534e42d243ba4f86f1607d22b4a35
Steve Scott <steve.scott@bjss.com>
2024-12-16 14:03:23 +0000

    Updates

--- api/DHSC.FingertipsNext.Modules.Core/LoggingExtensions.cs
+++ api/DHSC.FingertipsNext.Modules.Core/LoggingExtensions.cs
@@ -3,5 +3,5 @@ namespace DHSC.FingertipsNext.Modules.Core;
 public static partial class LoggingExtensions
 {
     [LoggerMessage(0, LogLevel.Error, "Log Example failed operation during: {Context}")]
-    public static partial void LogExample(this ILogger logger, Exception exception, string context, string bobbins);
+    public static partial void LogExample(this ILogger logger, Exception exception, string context);
 } 
