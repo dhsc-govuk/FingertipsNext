d11742b4739a61d63f870803a3e35ddf93bc92b5
Steve Scott <steve.scott@bjss.com>
2024-12-16 13:30:18 +0000

    FTN-37 Logging

--- api/DHSC.FingertipsNext.Modules.Core.Service/LoggingExtensions.cs
+++ api/DHSC.FingertipsNext.Modules.Core.Service/LoggingExtensions.cs
@@ -4,6 +4,6 @@ namespace DHSC.FingertipsNext.Modules.Core.Service;
 
 public static partial class LoggingExtensions
 {
-    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Log Example failed operation during: `{Context}`")]
-    public static partial void LogExample(this ILogger logger, string context, Exception exception);
+    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Search Specific Log Example failed operation during: `{Context}`")]
+    public static partial void LogExampleTwo(this ILogger logger, string context, Exception exception);
 } 
