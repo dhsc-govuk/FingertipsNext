cf2e9719737ba1d736ae7e0c6c203e1352fccd2f
Steve Scott <steve.scott@bjss.com>
2024-12-18 11:52:47 +0000

    Update

--- frontend/fingertips-frontend/instrumentation.ts
+++ frontend/fingertips-frontend/instrumentation.ts
@@ -8,6 +8,28 @@ const startMockServer = async () => {
   }
 };
 
+const configureApplicationInsights = () => {
+
+  console.log('Configuring logging');
+
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+
+    if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
+
+      const { useAzureMonitor } = require('@azure/monitor-opentelemetry');
+
+      useAzureMonitor();
+      console.log('Application Insights monitoring enabled');
+
+    } else {
+
+      console.log('** Application Insights Connection String missing - monitoring disabled **');
+    }
+  }
+}
+
 export async function register() {
+
+  configureApplicationInsights();
   await startMockServer();
 }
