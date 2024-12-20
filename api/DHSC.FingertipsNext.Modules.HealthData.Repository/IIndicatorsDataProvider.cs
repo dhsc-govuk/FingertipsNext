70d4374618ec6b9dba8aa0764ce4c715f1200fe7
Gary <80116067+MonkeyDate@users.noreply.github.com>
2024-12-20 17:13:13 +0000

    FTN-73: hardcoded indicator data endpoint (#22)
    Co-authored-by: Steve Scott <steve.scott@bjss.com>

---
+++ api/DHSC.FingertipsNext.Modules.HealthData.Repository/IIndicatorsDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using DHSC.FingertipsNext.Modules.HealthData.Schemas;
+
+namespace DHSC.FingertipsNext.Modules.HealthData.Repository;
+
+/// <summary>
+/// Provides a copy of the requested indicator data.
+/// </summary>
+public interface IIndicatorsDataProvider
+{
+    /// <summary>
+    /// Get data for a public health indicator. Returns all data for all
+    /// areas and all years for the indicators. Optionally filter the results by
+    /// supplying one or more area codes and one or more years in the query string.
+    /// </summary>
+    /// <param name="indicatorId">The unique identifier of the indicator.</param>
+    /// <param name="areaCodes">A list of area codes.</param>
+    /// <param name="years">A list of years.</param>
+    /// <returns></returns>
+    Task<IEnumerable<HealthDataForArea>> GetIndicatorData(
+        int indicatorId,
+        string[] areaCodes,
+        int[] years
+    );
+}
---
+++ api/DHSC.FingertipsNext.Modules.HealthData.Schemas/Indicator.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DHSC.FingertipsNext.Modules.HealthData.Schemas;
+
+/// <summary>
+/// A public health indicator
+/// </summary>
+public class Indicator
+{
+    /// <summary>
+    /// The unique identifier of the indicator
+    /// </summary>
+    [JsonPropertyName("indicator_id")]
+    public int IndicatorId { get; init; }
+    
+    /// <summary>
+    /// The title of the indicator
+    /// </summary>
+    [JsonPropertyName("title")]
+    public string Title { get; init; } = String.Empty;
+    
+    /// <summary>
+    /// The definition of the indicator
+    /// </summary>
+    [JsonPropertyName("definition")]
+    public string Definition  { get; init; } = String.Empty;
+}
