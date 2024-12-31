e81e57084dc108cbf3f33a954081d23909cd1208
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2024-12-31 16:47:32 +0000

    switch back to MSW and remove user

--- frontend/fingertips-frontend/playwright.config.ts
+++ frontend/fingertips-frontend/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 
   //Run your local dev server before starting the tests
   webServer: {
-    command: 'npm run devMSWOFF',
+    command: 'npm run dev',
     url: url,
     reuseExistingServer: !process.env.CI,
   },
