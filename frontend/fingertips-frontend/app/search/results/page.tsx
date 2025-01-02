7c40aca8fea0baf64771e961742d71cb17c3971e
Alan Taylor <alan.taylor@bjss.com>
2025-01-02 11:22:07 +0000

    FTN-58: added error handling

--- frontend/fingertips-frontend/app/search/results/page.tsx
+++ frontend/fingertips-frontend/app/search/results/page.tsx
@@ -1,4 +1,5 @@
 import { SearchResults } from '@/components/pages/search/results';
+import { ErrorPage } from '@/components/pages/error';
 import { getSearchService } from '@/lib/search/searchResultData';
 
 export default async function Page(
@@ -11,8 +12,15 @@ export default async function Page(
   const searchParams = await props.searchParams;
   const indicator = searchParams?.indicator ?? '';
 
-  // Perform async API call using indicator prop
-  const searchResults = await getSearchService().searchWith(indicator);
-
-  return <SearchResults indicator={indicator} searchResults={searchResults} />;
+  try {
+    // Perform async API call using indicator prop
+    const searchResults = await getSearchService().searchWith(indicator);
+    return <SearchResults indicator={indicator} searchResults={searchResults} />;
+  }
+  catch(error) {
+    // Log error response
+    // TBC
+    console.log(`Error response received from call to Search service: ${error}`);
+    return <ErrorPage errorText='An error has been returned by the search service. Please try again.' errorLink='/search' errorLinkText='Return to Search' />;
+  }
 }
