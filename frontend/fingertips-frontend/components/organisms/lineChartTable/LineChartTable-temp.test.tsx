5bcf3a650b79e56111a8335c1d2cefe80e4dc0a7
Fawad Ali <alibombaye@users.noreply.github.com>
2025-01-02 10:24:46 +0000

    another attempt

--- frontend/fingertips-frontend/components/organisms/lineChartTable/LineChartTable-temp.test.tsx
+++ frontend/fingertips-frontend/components/organisms/lineChartTable/LineChartTable-temp.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { expect } from '@jest/globals';
 import { WeatherForecast } from '@/generated-sources/api-client';
 import { registryWrapper } from '@/lib/testutils';
-import { LineChartTable } from '.';
+import { LineChartTable } from '@/components/organisms/lineChartTable';
 
 const mockData: WeatherForecast[] = [
   {
