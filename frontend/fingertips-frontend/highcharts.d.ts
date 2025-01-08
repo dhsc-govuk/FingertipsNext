1299ed37a8237b5d4c677651ac43b02f596c5628
WolfieKnee <david.wolfson@bjss.com>
2025-01-08 14:14:21 +0000

    import highcharts in order that it can be exported with extension for Templating

--- frontend/fingertips-frontend/highcharts.d.ts
+++ frontend/fingertips-frontend/highcharts.d.ts
@@ -1,8 +1,9 @@
+import Highcharts from 'highcharts';
+
 declare module 'highcharts' {
-  export * from 'node_modules/highcharts';
+  export * from 'highcharts';
 
   export const Templating: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     helpers: Record<string, (...args: any[]) => unknown>;
   };
 }
