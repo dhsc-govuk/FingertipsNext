d12defb9a49f0d19b789481df7064bf07452d179
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-01-08 17:24:55 +0000

    delete unused controller method

--- api/DHSC.FingertipsNext.Modules.HealthData/Controllers/V1/IndicatorsController.cs
+++ api/DHSC.FingertipsNext.Modules.HealthData/Controllers/V1/IndicatorsController.cs
@@ -24,23 +24,6 @@ public class IndicatorsController(IIndicatorsService indicatorsService)
     /// If more than 10 years are supplied only data for the first 10 distinct years will be returned.
     /// If more than 10 area codes are supplied only data for the first 10 distinct area codes will be returned.
     /// </remarks>
-    //[HttpGet]
-    //[Route("{indicatorId}/data/V1")]
-    //public async Task<HealthDataForArea[]> GetIndicatorData(
-    //    [FromRoute] int indicatorId,
-    //    [FromQuery] string[]? areaCodes = null,
-    //    [FromQuery] int[]? years = null
-    //)
-    //{
-    //    var indicatorData = await _indicatorsService.GetIndicatorData(
-    //        indicatorId,
-    //        areaCodes ?? [],
-    //        years ?? []
-    //    );
-
-    //    return indicatorData.ToArray();
-    //}
-
     [HttpGet]
     [Route("{indicatorId:int}/data")]
     public IActionResult GetIndicatorData(
