289418825b24486584a5b649af91bad4bd1bcad7
WolfieKnee <david.wolfson@bjss.com>
2025-01-16 12:17:48 +0000

    update tests for data perparation at higher level

--- frontend/fingertips-frontend/components/organisms/PopulationPyramid/populationPyramid.test.tsx
+++ frontend/fingertips-frontend/components/organisms/PopulationPyramid/populationPyramid.test.tsx
@@ -2,9 +2,33 @@ import { render, screen } from '@testing-library/react';
 import { expect } from '@jest/globals';
 import { mockHealthData } from '@/mock/data/healthdata';
 import { PopulationPyramid } from '.';
+import { PopulationData } from '@/lib/chartHelpers/preparePopulationData';
+
+const mockPopulationData: PopulationData = {
+  ageCategories: [
+    '90+',
+    '85-89',
+    '50-54',
+    '20-24',
+    '15-19',
+    '10-14',
+    '5-9',
+    '0-4',
+  ],
+  femaleSeries: [1.58, 2.48, 8.78, 7.67, 7.49, 7.81, 7.42, 6.78],
+  maleSeries: [0.79, 1.71, 8.49, 7.99, 7.95, 8.19, 7.77, 7.11],
+};
 
 test('should render the Highcharts react component within the PopulationPyramid component', () => {
-  render(<PopulationPyramid data={mockHealthData} />);
+  render(
+    <PopulationPyramid
+      data={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
+    />
+  );
   const highcharts = screen.getByTestId('highcharts-react-component');
   expect(highcharts).toBeInTheDocument();
 });
@@ -13,7 +37,11 @@ it('Should render the PopulationPyramid component title', () => {
   render(
     <PopulationPyramid
       populationPyramidTitle={'VALID population pyramid title'}
-      data={mockHealthData}
+      data={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
     />
   );
 
--- frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
+++ frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
@@ -2,6 +2,13 @@ import { render, screen } from '@testing-library/react';
 import { Chart } from '@/components/pages/chart/index';
 import { expect } from '@jest/globals';
 import { mockHealthData } from '@/mock/data/healthdata';
+import { PopulationData } from '@/lib/chartHelpers/preparePopulationData';
+
+const mockPopulationData: PopulationData = {
+  ageCategories: [],
+  femaleSeries: [],
+  maleSeries: [],
+};
 
 it('should render the backLink', () => {
   render(
@@ -9,6 +16,11 @@ it('should render the backLink', () => {
       data={mockHealthData}
       indicator="test"
       indicatorsSelected={['1', '2']}
+      preparedPopulationData={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
     />
   );
 
@@ -19,27 +31,63 @@ it('should render the backLink', () => {
 });
 
 it('should render the LineChart component', () => {
-  render(<Chart data={mockHealthData} />);
+  render(
+    <Chart
+      data={mockHealthData}
+      preparedPopulationData={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
+    />
+  );
   const lineChart = screen.getByTestId('lineChart-component');
   expect(lineChart).toBeInTheDocument();
 });
 
 it('should render the LineChart component title', () => {
-  render(<Chart data={mockHealthData} />);
+  render(
+    <Chart
+      data={mockHealthData}
+      preparedPopulationData={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
+    />
+  );
 
   const HTag = screen.getByRole('heading', { level: 3 });
   expect(HTag).toHaveTextContent('Title for the chart page');
 });
 
 it('should render the LineChartTable component', () => {
-  render(<Chart data={mockHealthData} />);
+  render(
+    <Chart
+      data={mockHealthData}
+      preparedPopulationData={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
+    />
+  );
 
   const table = screen.getByRole('table');
   expect(table).toBeInTheDocument();
 });
 
 it('should render the PopulationPyramid component', () => {
-  render(<Chart data={mockHealthData} />);
+  render(
+    <Chart
+      data={mockHealthData}
+      preparedPopulationData={{
+        dataForSelectedArea: mockPopulationData,
+        dataForEngland: undefined,
+        dataForBaseline: undefined,
+      }}
+    />
+  );
 
   const PyramidPlot = screen.getByTestId('PopulationPyramid-component');
   expect(PyramidPlot).toBeInTheDocument();
