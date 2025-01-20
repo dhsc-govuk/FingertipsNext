a005065ac3c34be46517178fc7c85452ec326b23
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-01-20 15:45:20 +0000

    remove redundant test

--- frontend/fingertips-frontend/components/atoms/Arrow/arrow.test.tsx
+++ frontend/fingertips-frontend/components/atoms/Arrow/arrow.test.tsx
@@ -4,31 +4,25 @@ import { expect } from '@jest/globals';
 import { Direction } from '@/lib/common-types';
 
 describe('Arrow Suite', () => {
-  it('should render expected elements', () => {
+  it('should display right arrow', () => {
+    render(<Arrow direction={Direction.RIGHT} />);
+
+    expect(screen.getByTestId('arrow-icon')).toBeInTheDocument();
+    expect(screen.getByTestId('arrow-right')).toBeInTheDocument();
+  });
+
+  it('should display up arrow', () => {
     render(<Arrow direction={Direction.UP} />);
 
     expect(screen.getByTestId('arrow-icon')).toBeInTheDocument();
     expect(screen.getByTestId('arrow-up')).toBeInTheDocument();
   });
 
-  describe('should display arrow based on direction prop', () => {
-    it('should display right arrow', () => {
-      render(<Arrow direction={Direction.RIGHT} />);
+  it('should display down arrow', () => {
+    render(<Arrow direction={Direction.DOWN} />);
 
-      expect(screen.getByTestId('arrow-right')).toBeInTheDocument();
-    });
-
-    it('should display up arrow', () => {
-      render(<Arrow direction={Direction.UP} />);
-
-      expect(screen.getByTestId('arrow-up')).toBeInTheDocument();
-    });
-
-    it('should display down arrow', () => {
-      render(<Arrow direction={Direction.DOWN} />);
-
-      expect(screen.getByTestId('arrow-down')).toBeInTheDocument();
-    });
+    expect(screen.getByTestId('arrow-icon')).toBeInTheDocument();
+    expect(screen.getByTestId('arrow-down')).toBeInTheDocument();
   });
 
   it('snapshot test', () => {
