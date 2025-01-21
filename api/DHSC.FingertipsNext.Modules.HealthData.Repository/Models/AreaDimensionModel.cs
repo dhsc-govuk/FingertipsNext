406a0f6393e9399eec0f4f94695aaedd3e19b89d
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-01-21 13:33:05 +0000

    revert unneeded changes to area dimension model file

--- api/DHSC.FingertipsNext.Modules.HealthData.Repository/Models/AreaDimensionModel.cs
+++ api/DHSC.FingertipsNext.Modules.HealthData.Repository/Models/AreaDimensionModel.cs
@@ -1,28 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
 namespace DHSC.FingertipsNext.Modules.HealthData.Repository.Models;
 
 [Serializable]
 public class AreaDimensionModel
 {
+
+    [Key] 
+    public required int AreaKey { get; set; }
+    [MaxLength(20)] 
+    public required string Code { get; set; }
+    [MaxLength(255)] 
+    public required string Name { get; set; }
     public AreaDimensionModel()
     {
     }
-
-    [SetsRequiredMembers]
-    public AreaDimensionModel(int areaKey, string code, string name, DateTime startDate, DateTime endDate)
-    {
-        AreaKey = areaKey;
-        Code = code;
-        Name = name;
-        StartDate = startDate;
-        EndDate = endDate;
-    }
-
-    [Key] public required int AreaKey { get; set; }
-    [MaxLength(20)] public required string Code { get; set; }
-    [MaxLength(255)] public required string Name { get; set; }
     public required DateTime StartDate { get; set; }
     public required DateTime EndDate { get; set; }
 }
