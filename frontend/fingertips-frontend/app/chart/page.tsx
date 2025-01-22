09e5d618de6a131c3efb7779e6afb084b2edf463
WolfieKnee <david.wolfson@bjss.com>
2025-01-22 11:28:37 +0000

    use updated prop name

--- frontend/fingertips-frontend/app/chart/page.tsx
+++ frontend/fingertips-frontend/app/chart/page.tsx
@@ -48,7 +48,7 @@ export default async function ChartPage(
 
   return (
     <Chart
-      preparedPopulationData={preparedPopulationData}
+      populationData={preparedPopulationData}
       data={data}
       searchedIndicator={searchedIndicator}
       indicatorsSelected={indicatorsSelected}
