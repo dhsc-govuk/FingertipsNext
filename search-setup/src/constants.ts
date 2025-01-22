a96adb9d84601ad17a602e365cedf2f7d85bdb13
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-01-22 12:16:31 +0000

    Rename indices in search-setup

--- search-setup/src/constants.ts
+++ search-setup/src/constants.ts
@@ -1,5 +1,5 @@
-export const INDICATOR_SEARCH_INDEX_NAME = "indicator-search-index";
-export const GEOGRAPHY_SEARCH_INDEX_NAME = "geography-search-index";
+export const INDICATOR_SEARCH_INDEX_NAME = "demo-indicator-search-index";
+export const GEOGRAPHY_SEARCH_INDEX_NAME = "demo-geography-search-index";
 export const GEOGRAPHY_SEARCH_SUGGESTER_NAME = "geographySuggester";
 
 export enum GeographySearchIndexColumnNames {
