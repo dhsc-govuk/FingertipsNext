79ef059585defced6efb9f77bd1d258793e3f043
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-01-22 10:06:38 +0000

    Feature/DHSCFT-208_configure_e2e_tests (#71)
    Co-authored-by: Euan Percy <euan.percy@bjss.com>

--- frontend/fingertips-frontend/playwright.config.ts
+++ frontend/fingertips-frontend/playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const url = 'http://localhost:3000/search';
+const url =
+  process.env.FINGERTIPS_FRONTEND_URL || 'http://localhost:3000/search';
 
 export default defineConfig({
   testDir: './playwright/tests',
@@ -29,6 +30,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: url,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !!process.env.CI,
   },
 });
