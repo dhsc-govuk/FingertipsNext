13cb504a091b32ec75acf1f154ead03c42ff4cc4
WolfieKnee <david.wolfson@bjss.com>
2025-01-23 14:55:26 +0000

    Add helper to build a HealthDataPoint

--- api/DHSC.FingertipsNext.Modules.HealthData.UnitTests/Helpers/TestHelper.cs
+++ api/DHSC.FingertipsNext.Modules.HealthData.UnitTests/Helpers/TestHelper.cs
@@ -1,4 +1,5 @@
 ﻿using DHSC.FingertipsNext.Modules.HealthData.Repository.Models;
+using DHSC.FingertipsNext.Modules.HealthData.Schemas;
 
 namespace DHSC.FingertipsNext.Modules.HealthData.Tests.Helpers;
 
@@ -54,5 +55,31 @@ public static class TestHelper
             IndicatorDimension = indicatorDimension,
             SexDimension = sexDimension
         };
+    }
+
+    /// <summary>
+    /// build a HealtDataPoint 
+    /// </summary>
+    /// <param name="year"></param>
+    /// <param name="count"></param>
+    /// <param name="value"></param>
+    /// <param name="lowerConfidenceInterval"></param>
+    /// <param name="upperConfidenceInterval"></param>
+    /// <param name="ageBand"></param>
+    /// <param name="sex"></param>
+    /// <returns></returns>
+    public static HealthDataPoint BuildHealthDataPoint(int year, string ageBand, string sex,  float count = 1, float value = 1, float lowerConfidenceInterval = 1, float upperConfidenceInterval = 1)
+    {
+        return new HealthDataPoint
+        {
+            Year = year,
+            Count = count,
+            Value = value,
+            LowerConfidenceInterval = lowerConfidenceInterval,
+            UpperConfidenceInterval = upperConfidenceInterval,
+            AgeBand = ageBand,
+            Sex = sex
+        };
+
     }
 }
