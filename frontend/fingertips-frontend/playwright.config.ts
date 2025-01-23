92db89edbc55f7ae2c983cc2f26d17b937dd78af
Fawad Ali <alibombaye@users.noreply.github.com>
2025-01-23 15:44:01 +0000

    try reverting playwright config

--- frontend/fingertips-frontend/playwright.config.ts
+++ frontend/fingertips-frontend/playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const url = process.env.FINGERTIPS_FRONTEND_URL ?? 'http://localhost:3000';
+const url = process.env.FINGERTIPS_FRONTEND_URL || 'http://localhost:3000';
 
 export default defineConfig({
   testDir: './playwright/tests',
