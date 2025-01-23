637ece41a3959a1ba36e19e451060bda2a7f0bd4
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-01-23 12:05:25 +0000

    add list to ci reporting

--- frontend/fingertips-frontend/playwright.config.ts
+++ frontend/fingertips-frontend/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI
-    ? [['@estruyf/github-actions-reporter'], ['html']]
+    ? [['list'], ['@estruyf/github-actions-reporter'], ['html']]
     : [['list'], ['html']],
   use: {
     baseURL: url,
