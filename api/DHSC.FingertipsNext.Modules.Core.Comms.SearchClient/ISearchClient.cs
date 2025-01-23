900316325de259ca70205757d48a7fcd7e277cd3
Priya Thorpe <priya.thorpe@bjss.com>
2025-01-23 12:04:29 +0000

    Feature/dhscft 241 delete redundant weather forecast code (#74)

--- api/DHSC.FingertipsNext.Modules.Core.Comms.SearchClient/ISearchClient.cs
+++
@@ -1,6 +0,0 @@
-namespace DHSC.FingertipsNext.Modules.Core.SearchAPI;
-
-public interface ISearchClient
-{
-    public string Search(string searchTerm);
-}
--- api/DHSC.FingertipsNext.Modules.Core.Service/ICoreService.cs
+++
@@ -1,6 +0,0 @@
-namespace DHSC.FingertipsNext.Modules.Core.Service;
-
-public interface ICoreService
-{
-    public string DoThing(string searchTerm);
-}
