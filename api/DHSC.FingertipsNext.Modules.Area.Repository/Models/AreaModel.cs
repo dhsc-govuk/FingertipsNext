f8d8f846348e7145cd98c0ec812f65f9a0920527
Gary <80116067+MonkeyDate@users.noreply.github.com>
2025-01-28 11:56:42 +0000

    Feature/dhscft 182 create area details api endpoint (#75)
    Add AREAS endpoint to the Api.

---
+++ api/DHSC.FingertipsNext.Modules.Area.Repository/Models/AreaModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DHSC.FingertipsNext.Modules.Area.Repository.Models;
+
+/// <summary>
+/// A area or geography that has associated public health data
+/// </summary>
+[Serializable]
+[Table("Areas", Schema = "Areas")]
+public class AreaModel
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Key]
+    public required HierarchyId Node { get; set; }
+
+    /// <summary>
+    /// The unique area code of the area / geography
+    /// </summary>
+    [MaxLength(20)]
+    public required string AreaCode { get; set; }
+
+    /// <summary>
+    /// The name of the area / geography
+    /// </summary>
+    public int Level { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [MaxLength(255)]
+    public required string AreaName { get; set; }
+
+    /// <summary>
+    /// The type of the area / geography
+    /// </summary>
+    [MaxLength(20)]
+    public required string AreaType { get; set; }
+
+    /// <summary>
+    /// The name of the associated hierarchy for the area / geography
+    /// </summary>
+    [MaxLength(20)]
+    public required string HierarchyType { get; set; }
+}
