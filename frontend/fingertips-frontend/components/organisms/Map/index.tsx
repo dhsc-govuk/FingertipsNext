51954bcab914922a1e97a0404b651b94fb35ee98
WolfieKnee <david.wolfson@bjss.com>
2025-01-28 16:57:18 +0000

    Update projection and map resolution. Add NHS regions

--- frontend/fingertips-frontend/components/organisms/Map/index.tsx
+++ frontend/fingertips-frontend/components/organisms/Map/index.tsx
@@ -3,27 +3,53 @@
 import { H3 } from 'govuk-react';
 import Highcharts from 'highcharts/highmaps';
 import HighchartsReact from 'highcharts-react-official';
-import EnglandRegionsMap from '@/assets/Regions_December_2021_EN_BGC_2022_-4522802171286724651.geo.json';
+import EnglandRegionsMapBUC from '@/assets/Regions_December_2023_Boundaries_EN_BUC_1958740832896680092.geo.json';
+import EnglandNHSRegionsMapBSC from '@/assets/NHS_England_Regions_January_2024_EN_BSC_7500404208533377417.geo.json';
 
 interface FingertipsMapProps {}
 
 export function FingertipsMap({}: Readonly<FingertipsMapProps>) {
-  const fingertipsMapOptions: Highcharts.Options = {
+  const fingertipsMapOptionsRegions: Highcharts.Options = {
     chart: {
-      map: 'mapDataGB',
-      height: 600,
-      width: 600,
+      height: 800,
     },
-    title: { text: 'Map Demo' },
+    mapView: {
+      projection: {
+        name: 'EqualEarth', //Built-in projections are `EqualEarth`, `LambertConformalConic`, `Miller`, `Orthographic` and `WebMercator`.
+      },
+    },
+    title: { text: 'England Regions BUC 2023' },
     credits: { enabled: false },
     mapNavigation: { enabled: true },
     series: [
       {
         // use be map with no data as a basemap
         name: 'Regions',
-        mapData: EnglandRegionsMap,
-        borderColor: '#A0A0A0',
-        nullColor: 'rgba(200, 200, 200, 0.3)',
+        mapData: EnglandRegionsMapBUC,
+        borderColor: '#000000',
+        showInLegend: false,
+      },
+    ],
+  };
+
+  const fingertipsMapOptionsNHSRegions: Highcharts.Options = {
+    chart: {
+      height: 800,
+    },
+    mapView: {
+      projection: {
+        name: 'EqualEarth',
+      },
+    },
+    title: { text: 'England NHS Regions BSC 2024' },
+    credits: { enabled: false },
+    mapNavigation: { enabled: true },
+    series: [
+      {
+        // use be map with no data as a basemap
+        name: 'Regions',
+        mapData: EnglandNHSRegionsMapBSC,
+        borderColor: '#000000',
         showInLegend: false,
       },
     ],
@@ -31,12 +57,18 @@ export function FingertipsMap({}: Readonly<FingertipsMapProps>) {
 
   return (
     <div>
-      <H3>Map</H3>
+      <H3>Fingertips Map</H3>
       <HighchartsReact
         constructorType={'mapChart'}
         highcharts={Highcharts}
-        options={fingertipsMapOptions}
+        options={fingertipsMapOptionsRegions}
       />
+      <HighchartsReact
+        constructorType={'mapChart'}
+        highcharts={Highcharts}
+        options={fingertipsMapOptionsNHSRegions}
+      />
+      <br />
     </div>
   );
 }
