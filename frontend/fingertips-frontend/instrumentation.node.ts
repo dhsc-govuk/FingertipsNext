03cf0e9077c41b904739a4600170d87b0fe9ed50
Euan Percy <euan.percy@bjss.com>
2025-01-29 16:07:04 +0000

    Fix opentelemetry dependency

--- frontend/fingertips-frontend/instrumentation.node.ts
+++ frontend/fingertips-frontend/instrumentation.node.ts
@@ -1,19 +1,68 @@
-import { Resource } from '@opentelemetry/resources';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { AzureMonitorTraceExporter } from '@azure/monitor-opentelemetry-exporter';
+const startMockServer = async () => {
+  if (process.env.MOCK_SERVER === 'true') {
+    const { server } = await import('./mock/server/node');
+    server.listen({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+};
 
-const traceExporter = new AzureMonitorTraceExporter({
-  connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
-});
+let telemetryInitialized = false;
 
-const sdk = new NodeSDK({
-  resource: new Resource({
-    [ATTR_SERVICE_NAME]: 'ftn_fe',
-  }),
-  spanProcessor: new SimpleSpanProcessor(traceExporter),
-});
-sdk.start();
+const configureApplicationInsights = async () => {
+  console.log('Configuring logging');
 
-console.log('Tracing configured and sending data to Application Insights');
+  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
+    const { useAzureMonitor, AzureMonitorOpenTelemetryOptions  } = await require('@azure/monitor-opentelemetry');
+
+    const options: typeof AzureMonitorOpenTelemetryOptions = {
+      azureMonitorExporterOptions: {
+          connectionString:
+              process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+      },
+      enableLiveMetrics: false
+    };
+
+    if (!telemetryInitialized) {
+      useAzureMonitor(options);
+      telemetryInitialized = true;
+      console.log('Application Insights monitoring enabled');
+    }
+  } else {
+    console.log('** Application Insights Connection String missing - monitoring disabled **');
+  }
+};
+
+const configureTracing = async () => {
+  if (!telemetryInitialized) {
+    const { NodeSDK } = require('@opentelemetry/sdk-node');
+    const { UndiciInstrumentation } = require('@opentelemetry/instrumentation-undici');
+    const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-node');
+    const { Resource } = require('@opentelemetry/resources');
+    const { SEMRES_ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
+
+    const spanProcessor = new SimpleSpanProcessor();
+    const instrumentation = new UndiciInstrumentation();
+
+    const sdk = new NodeSDK({
+      resource: new Resource({
+        [SEMRES_ATTR_SERVICE_NAME]: 'ftn_fe',
+      }),
+      traceExporter: null,
+      spanProcessors: [spanProcessor],
+      instrumentations: [instrumentation],
+    });
+
+    sdk.start();
+    telemetryInitialized = true;
+    console.log('OpenTelemetry tracing initialized');
+  }
+};
+
+(async () => {
+  await configureApplicationInsights();
+  await configureTracing();
+  await startMockServer();
+})();
+
+export {};
--- frontend/fingertips-frontend/instrumentation.ts
+++ frontend/fingertips-frontend/instrumentation.ts
@@ -1,42 +1,5 @@
-const startMockServer = async () => {
-  if (
-    process.env.NEXT_RUNTIME === 'nodejs' &&
-    process.env.MOCK_SERVER === 'true'
-  ) {
-    const { server } = await import('./mock/server/node');
-    server.listen({
-      onUnhandledRequest: 'bypass',
-    });
-  }
-};
-
-// const configureApplicationInsights = async () => {
-//   console.log('Configuring logging');
-
-//   if (process.env.NEXT_RUNTIME === 'nodejs') {
-//     if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
-//       const { useAzureMonitor } = await import('@azure/monitor-opentelemetry');
-
-//       // eslint-disable-next-line react-hooks/rules-of-hooks
-//       useAzureMonitor();
-
-//       console.log('Application Insights monitoring enabled');
-//     } else {
-//       console.log(
-//         '** Application Insights Connection String missing - monitoring disabled **'
-//       );
-//     }
-//   }
-// };
-
-const configureTracing = async () => {
+export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./instrumentation.node');
   }
-};
-
-export async function register() {
-  // configureApplicationInsights();
-  // configureTracing();
-  await Promise.all([configureTracing(), startMockServer()]);
 }
