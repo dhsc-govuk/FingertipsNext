a0a97d1ca79a76b896394ff8df51bcfdb283dd1e
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-02-05 12:04:39 +0000

    add indicator search action unit tests

---
+++ frontend/fingertips-frontend/components/forms/IndicatorSearchForm/indicatorSearchActions.test.ts
@@ -0,0 +1,85 @@
+import { expect } from '@jest/globals';
+import { mockDeep } from 'jest-mock-extended';
+import { redirect, RedirectType } from 'next/navigation';
+import { SearchParams } from '@/lib/searchStateManager';
+import {
+  IndicatorSearchFormState,
+  searchIndicator,
+} from './indicatorSearchActions';
+
+jest.mock('next/navigation');
+const redirectMock = jest.mocked(redirect);
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+function* iteratorFromList<T>(list: T[]): IterableIterator<T> {
+  for (const item of list) {
+    yield item;
+  }
+}
+
+export const getMockFormData = (formData: Record<string, string>) =>
+  mockDeep<FormData>({
+    entries: jest.fn().mockImplementation(() => {
+      const formDataEntries = Object.entries(formData);
+
+      return iteratorFromList(formDataEntries);
+    }),
+    get: (key: string) => formData[key],
+  });
+
+const initialStateWithoutAreas: IndicatorSearchFormState = {
+  indicator: 'some indicator',
+  areasSelected: [],
+};
+
+const initialStateWithAreas: IndicatorSearchFormState = {
+  indicator: 'some indicator',
+  areasSelected: ['foo', 'bar'],
+};
+
+describe('Search actions', () => {
+  it('should redirect to search results if indicator only is provided', async () => {
+    const formData = getMockFormData({ indicator: 'boom' });
+
+    await searchIndicator(initialStateWithoutAreas, formData);
+
+    expect(redirectMock).toHaveBeenCalledWith(
+      `/results?${SearchParams.SearchedIndicator}=boom`,
+      RedirectType.push
+    );
+  });
+
+  it('should redirect to search results if areas only are provided', async () => {
+    const formData = getMockFormData({ indicator: '' });
+
+    await searchIndicator(initialStateWithAreas, formData);
+
+    expect(redirectMock).toHaveBeenCalledWith(
+      `/results?${SearchParams.AreasSelected}=foo&${SearchParams.AreasSelected}=bar`,
+      RedirectType.push
+    );
+  });
+
+  it('should redirect to search results if indicator and areas are provided', async () => {
+    const formData = getMockFormData({ indicator: 'boom' });
+
+    await searchIndicator(initialStateWithAreas, formData);
+
+    expect(redirectMock).toHaveBeenCalledWith(
+      `/results?${SearchParams.SearchedIndicator}=boom&${SearchParams.AreasSelected}=foo&${SearchParams.AreasSelected}=bar`,
+      RedirectType.push
+    );
+  });
+
+  it('should return an appropriate message if no indicator and no areas are provided', async () => {
+    const formData = getMockFormData({ indicator: '' });
+
+    const state = await searchIndicator(initialStateWithoutAreas, formData);
+
+    expect(state.indicator).toBe('');
+    expect(state.message).toBe('Please enter a subject');
+  });
+});
