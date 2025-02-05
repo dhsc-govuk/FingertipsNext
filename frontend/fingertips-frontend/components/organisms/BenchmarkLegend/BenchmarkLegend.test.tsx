b44d209f48fe223d92d4549f81380cd793669906
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-02-05 12:10:34 +0000

    Starting Benchmark Legend

---
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
@@ -0,0 +1,10 @@
+import { render} from '@testing-library/react';
+import { BenchmarkLegend } from '@/components/organisms/BenchmarkLegend/index';
+
+
+test("load and display benchmark legend component", async ()=>{
+
+    render(<BenchmarkLegend />)
+
+
+})
---
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/index.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Label from "nhs"
+
+
+interface BenchmarkLegendProps{
+    
+}
+
+export const BenchmarkLegend  = ({}: BenchmarkLegendProps) => {
+    return <Label>
+
+    </Label>;
+};
+
