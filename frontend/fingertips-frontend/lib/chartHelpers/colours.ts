9bc6e489462531c567780236b035ebc77d35be85
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-02-05 14:04:56 +0000

    Use an enum for chart colours

--- frontend/fingertips-frontend/lib/chartHelpers/colours.ts
+++ frontend/fingertips-frontend/lib/chartHelpers/colours.ts
@@ -1,33 +1,35 @@
-export const chartOrange = '#F46A25';
-export const chartLightPurple = '#A285D1';
-export const chartDarkPink = '#801650';
-export const chartGreen = '#00703C';
-export const chartPink = '#D53880';
-export const chartPurple = '#4C2C92';
-export const chartYellow = '#FFDD00';
-export const chartRed = '#D4351C';
-export const chartBlue = '#1D70B8';
-export const chartLightPink = '#F499BE';
-export const chartBrown = '#B58840';
-export const chartDarkBlue = '#12436D';
-export const chartLightBlue = '#5694CA';
-export const chartOtherLightPurple = '#6F72AF';
+export enum ChartColours {
+  Orange = '#F46A25',
+  LightPurple = '#A285D1',
+  DarkPink = '#801650',
+  Green = '#00703C',
+  Pink = '#D53880',
+  Purple = '#4C2C92',
+  Yellow = '#FFDD00',
+  Red = '#D4351C',
+  Blue = '#1D70B8',
+  LightPink = '#F499BE',
+  Brown = '#B58840',
+  DarkBlue = '#12436D',
+  LightBlue = '#5694CA',
+  OtherLightPurple = '#6F72AF',
+}
 
 // The ordering of this array determines the order in which the colours are
 // used by Highcharts, so it should be preserved.
 export const chartColours = [
-  chartOrange,
-  chartLightPurple,
-  chartDarkPink,
-  chartGreen,
-  chartPink,
-  chartPurple,
-  chartYellow,
-  chartRed,
-  chartBlue,
-  chartLightPink,
-  chartBrown,
-  chartDarkBlue,
-  chartLightBlue,
-  chartOtherLightPurple,
+  ChartColours.Orange,
+  ChartColours.LightPurple,
+  ChartColours.DarkPink,
+  ChartColours.Green,
+  ChartColours.Pink,
+  ChartColours.Purple,
+  ChartColours.Yellow,
+  ChartColours.Red,
+  ChartColours.Blue,
+  ChartColours.LightPink,
+  ChartColours.Brown,
+  ChartColours.DarkBlue,
+  ChartColours.LightBlue,
+  ChartColours.OtherLightPurple,
 ];
