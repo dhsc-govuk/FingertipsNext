4236cef04bfd737d460f78d081532b6e955e9e58
Euan Percy <euan.percy@bjss.com>
2025-02-06 11:08:36 +0000

    Add frontend healthcheck (#123)

--- frontend/fingertips-frontend/generated-sources/ft-api-client/models/index.ts
+++ frontend/fingertips-frontend/generated-sources/ft-api-client/models/index.ts
@@ -4,6 +4,8 @@ export * from './Area';
 export * from './AreaType';
 export * from './AreaWithRelations';
 export * from './GetAreaHierarchies500Response';
+export * from './GetHealthcheck200Response';
+export * from './GetHealthcheck500Response';
 export * from './HealthDataForArea';
 export * from './HealthDataPoint';
 export * from './Indicator';
