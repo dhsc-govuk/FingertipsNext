8b6228e49d7801c3bba1bddd17c591d29baba28f
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-02-06 12:06:00 +0000

    DHSCFT-258: Line Chart Updates (#124)

--- frontend/fingertips-frontend/lib/chartHelpers/chartHelpers.test.ts
+++ frontend/fingertips-frontend/lib/chartHelpers/chartHelpers.test.ts
@@ -1,5 +1,4 @@
 import {
-  generateSeriesData,
   getEnglandDataForIndicatorIndex,
   seriesDataWithoutEngland,
   sortHealthDataByDate,
@@ -65,76 +64,6 @@ describe('sortHealthDataByDate', () => {
     const result = sortHealthDataByDate(mockData);
 
     expect(result).toEqual(mockSortedData);
-  });
-});
-
-describe('generateSeriesData', () => {
-  it('should generate series data without benchmark data', () => {
-    const result = generateSeriesData(mockData);
-    const mockSeriesData = [
-      {
-        data: [
-          [2006, 278.29134],
-          [2004, 703.420759],
-        ],
-        name: 'North FooBar',
-        type: 'line',
-      },
-    ];
-    expect(result).toEqual(mockSeriesData);
-  });
-
-  it('should generate series data with benchmark data', () => {
-    const mockBenchmarkData = {
-      areaCode: 'E92000001',
-      areaName: 'England',
-      healthData: [
-        {
-          count: 389,
-          lowerCi: 441.69151,
-          upperCi: 578.32766,
-          value: 278.29134,
-          year: 2006,
-          sex: 'Persons',
-          ageBand: 'All',
-        },
-        {
-          count: 267,
-          lowerCi: 441.69151,
-          upperCi: 578.32766,
-          value: 703.420759,
-          year: 2004,
-          sex: 'Persons',
-          ageBand: 'All',
-        },
-      ],
-    };
-
-    const result = generateSeriesData(mockData, mockBenchmarkData);
-    const mockSeriesData = [
-      {
-        color: 'black',
-        data: [
-          [2006, 278.29134],
-          [2004, 703.420759],
-        ],
-        marker: {
-          symbol: 'circle',
-        },
-        name: 'Benchmark: England',
-        type: 'line',
-      },
-      {
-        data: [
-          [2006, 278.29134],
-          [2004, 703.420759],
-        ],
-        name: 'North FooBar',
-        type: 'line',
-      },
-    ];
-
-    expect(result).toEqual(mockSeriesData);
   });
 });
 
