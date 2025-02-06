fe7ca877dc0b18856a343f41daeb2799370c4783
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-02-06 16:48:16 +0000

    Completed

--- frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
@@ -1,6 +1,82 @@
-import { render } from '@testing-library/react';
-import { LegendLabel } from '@/components/organisms/BenchmarkLegend/LegendLabel';
 
-test('load and display benchmark legend component', async () => {
-  render(<LegendLabel />);
+import { render, screen } from '@testing-library/react';
+import { BenchmarkLegend,  BenchmarkLegendData } from './BenchmarkLegend';
+import '@testing-library/jest-dom';
+
+
+
+describe('Testing the BenchmarkLegend component', () => {
+  it('renders without crashing with an empty model', () => {
+    render(<BenchmarkLegend model={[]} />);
+    expect(screen.queryByTestId('legend-label')).not.toBeInTheDocument();
+  });
+
+  it('renders a single legend item', () => {
+    const model: BenchmarkLegendData[] = [{ group: 'rag', labels: [{ label: 'Better (95%)', type: 'better' }] }];
+    render(<BenchmarkLegend model={model} />);
+    expect(screen.getByText('Better (95%)')).toBeInTheDocument();
+  });
+
+  it('renders multiple legend items', () => {
+    const model: BenchmarkLegendData[] = [{
+      group: 'rag',
+      labels: [
+        { label: 'Better (76%)', type: 'better' },
+        { label: 'Worse (75%)', type: 'worse' }
+      ]
+    }];
+    render(<BenchmarkLegend model={model} />);
+    expect(screen.getByText('Better (76%)')).toBeInTheDocument();
+    expect(screen.getByText('Worse (75%)')).toBeInTheDocument();
+  });
+
+  it('does not render a title if not provided', () => {
+    const model: BenchmarkLegendData[] = [{ group: 'test', labels: [{ label: 'Test Label', type: 'test' }] }];
+    render(<BenchmarkLegend model={model} />);
+    expect(screen.queryByText('legend_panel_header')).not.toBeInTheDocument();
+  });
+
+  it('renders correctly with provided benchmark data', () => {
+    const benchmarkData: BenchmarkLegendData[] = [
+      {
+        group: 'rag',
+        title: 'Areas compared to England',
+        labels: [
+          { label: 'Better (65%)', type: 'better' },
+          { label: 'Similar', type: 'similar' },
+          { label: 'Worse (95%)', type: 'worse' },
+          { label: 'Not compared', type: 'not_compared' },
+          { label: 'Lower (95%)', type: 'lower' },
+          { label: 'Higher (95%)', type: 'higher' }
+        ]
+      },
+      {
+        title: ' Quintile groupings',
+        group: 'quintile',
+        labels: [
+          { label: 'Lowest', type: 'lowest' },
+          { label: 'Low', type: 'low' },
+          { label: 'Middle', type: 'middle' },
+          { label: 'High', type: 'high' },
+          { label: 'Highest', type: 'highest' }
+        ]
+      },
+      {
+        group: 'others',
+        labels: [
+          { label: 'Worst', type: 'worst' },
+          { label: 'Worse', type: 'worse' },
+          { label: 'Middle', type: 'middle' },
+          { label: 'Better', type: 'better' },
+          { label: 'Best', type: 'best' }
+        ]
+      }
+    ];
+    
+    render(<BenchmarkLegend model={benchmarkData} />);
+    expect(screen.getByText('Areas compared to England')).toBeInTheDocument();
+    expect(screen.getByText('Quintile groupings')).toBeInTheDocument();
+    expect(screen.getByText('Better (65%)')).toBeInTheDocument();
+    expect(screen.getByText('Worst')).toBeInTheDocument();
+  });
 });
--- frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.tsx
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.tsx
@@ -46,21 +46,22 @@ export const BenchmarkLegend: React.FC<BenchmarkLegendProps> = ({ model }) => {
   return (
     <div>
       {model?.map((item, index) => (
-        <BenchmarkLegendGroupPanelStyle key={index}>
+        <BenchmarkLegendGroupPanelStyle key={index}  >
           <div>
             {item.title !== null && (
               <div>
-                <LabelText className="legend_panel_header">
+                <LabelText className="legend_panel_header"  >
                   {item.title}
                 </LabelText>
               </div>
             )}
             {item?.labels.map((label, label_key) => (
-              <LegendLabel
+              <LegendLabel 
                 group={item?.group}
                 label={label?.label}
                 type={label.type}
                 key={label_key}
+                data-testid ={item?.group +"_" + label.type + "_" +label_key}
               />
             ))}
           </div>
