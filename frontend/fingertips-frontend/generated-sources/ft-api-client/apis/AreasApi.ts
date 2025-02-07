45a8611ee7680b5a5b6f2182791ffc41995e045a
Howard May <howard.may@bjss.com>
2025-02-07 16:24:11 +0000

    DHSCFT-300: Updates swagger

--- frontend/fingertips-frontend/generated-sources/ft-api-client/apis/AreasApi.ts
+++ frontend/fingertips-frontend/generated-sources/ft-api-client/apis/AreasApi.ts
@@ -42,7 +42,7 @@ export interface GetAreaRequest {
 }
 
 export interface GetAreaTypeMembersRequest {
-    areaType: string;
+    areaTypeKey: string;
 }
 
 export interface GetAreaTypesRequest {
@@ -108,7 +108,7 @@ export interface AreasApiInterface {
     /**
      * Get the areas that have a given area type
      * @summary Get member areas for an area type
-     * @param {string} areaType The area type
+     * @param {string} areaTypeKey The area type key
      * @param {*} [options] Override http request option.
      * @throws {RequiredError}
      * @memberof AreasApiInterface
@@ -252,10 +252,10 @@ export class AreasApi extends runtime.BaseAPI implements AreasApiInterface {
      * Get member areas for an area type
      */
     async getAreaTypeMembersRaw(requestParameters: GetAreaTypeMembersRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<Array<Area>>> {
-        if (requestParameters['areaType'] == null) {
+        if (requestParameters['areaTypeKey'] == null) {
             throw new runtime.RequiredError(
-                'areaType',
-                'Required parameter "areaType" was null or undefined when calling getAreaTypeMembers().'
+                'areaTypeKey',
+                'Required parameter "areaTypeKey" was null or undefined when calling getAreaTypeMembers().'
             );
         }
 
@@ -264,7 +264,7 @@ export class AreasApi extends runtime.BaseAPI implements AreasApiInterface {
         const headerParameters: runtime.HTTPHeaders = {};
 
         const response = await this.request({
-            path: `/areas/areatypes/{area_type}/areas`.replace(`{${"area_type"}}`, encodeURIComponent(String(requestParameters['areaType']))),
+            path: `/areas/areatypes/{area_type}/areas`.replace(`{${"area_type_key"}}`, encodeURIComponent(String(requestParameters['areaTypeKey']))),
             method: 'GET',
             headers: headerParameters,
             query: queryParameters,
