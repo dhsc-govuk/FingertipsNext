88e7340d09111308e3abbf69110be664f0dd3075
WolfieKnee <david.wolfson@bjss.com>
2025-02-11 14:28:12 +0000

    naming

--- frontend/fingertips-frontend/lib/thematicMapUtils/getMapData.ts
+++ frontend/fingertips-frontend/lib/thematicMapUtils/getMapData.ts
@@ -1,3 +1,7 @@
+import union from '@turf/union';
+import { GeoJSON } from 'highcharts';
+import { Feature, Polygon, MultiPolygon, FeatureCollection } from 'geojson';
+
 import regionsMap from '@/assets/maps/Regions_December_2023_Boundaries_EN_BUC_1958740832896680092.geo.json';
 import countiesAndUAsMap from '@/assets/maps/Counties_and_Unitary_Authorities_December_2023_Boundaries_UK_BSC_4952317392296043005.geo.json';
 import districtsAndUAsMap from '@/assets/maps/Local_Authority_Districts_May_2024_Boundaries__UK_BSC_-5684348521832897108.geo.json';
@@ -6,10 +10,6 @@ import NHSRegionsMap from '@/assets/maps/NHS_England_Regions_January_2024_EN_BSC
 import NHSICBMap from '@/assets/maps/Integrated_Care_Boards_April_2023_EN_BSC_-187828753279616787.geo.json';
 import NHSSubICBMap from '@/assets/maps/NHS_SubICB_April_2023_EN_BSC_8040841744469859785.geo.json';
 
-import { GeoJSON } from 'highcharts';
-import { Feature, Polygon, MultiPolygon, FeatureCollection } from 'geojson';
-import union from '@turf/union';
-
 type MapData = {
   mapJoinKey: string;
   mapFile: GeoJSON;
@@ -17,7 +17,7 @@ type MapData = {
 };
 export function getMapData(areaType: string, areaCodes: string[]): MapData {
   const mapJoinKey = mapJoinKeyEncoder[areaType];
-  const mapFile = mapEncoder[areaType];
+  const mapFile = mapFileEncoder[areaType];
 
   const groupAreas = mapFile.features.filter((feature) =>
     areaCodes.includes(feature.properties[mapJoinKey])
@@ -47,7 +47,7 @@ const mapJoinKeyEncoder: Record<string, string> = {
   'Sub ICB': 'SICBL23CD',
 };
 
-const mapEncoder: Record<string, GeoJSON> = {
+const mapFileEncoder: Record<string, GeoJSON> = {
   'Regions Statistical': regionsMap,
   'Counties & UAs': countiesAndUAsMap,
   'Districts & UAs': districtsAndUAsMap,
