33e18454996c40c2d805f7739a01f99fcc4c54ac
andyevans-bjss <andrew.evans@bjss.com>
2025-02-12 13:08:09 +0000

    Feature/dhscft 57 create POC data (#140)

---
+++ DataCreator/DataCreator/IndicatorEntity.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DataCreator
+{
+    public class IndicatorEntity
+    {
+        private string _indicatorDefinition;
+        private string rationale;
+        private string dataSource;
+        private string method;
+        private string cIMethodDetails;
+        private string countSource;
+        private string countDefinition;
+        private string denominatorSource;
+        private string denominatorDefinition;
+        private string disclouseControl;
+        private string caveats;
+        private string copyright;
+        private string notes;
+
+        public int IndicatorID { get; set; }
+        public string ValueType { get; set; }
+
+        public string DenominatorType { get; set; }
+
+        public string CIMethod { get; set; }
+        public string UnitLabel { get; set; }
+        public string UnitValue { get; set; }
+
+        public string IndicatorName { get; set; }
+        public string IndicatorDefinition
+        {
+            get => _indicatorDefinition.StripHTML(); 
+            set { _indicatorDefinition = value; }
+        }
+
+        public string Rationale { get => rationale.StripHTML(); set => rationale = value; }
+        public string DataSource { get => dataSource.StripHTML(); set => dataSource = value; }
+        public string Method { get => method.StripHTML(); set => method = value; }
+        public string StandardPopulation { get; set; }
+        public string CIMethodDetails { get => cIMethodDetails.StripHTML(); set => cIMethodDetails = value; }
+        public string CountSource { get => countSource.StripHTML(); set => countSource = value; }
+        public string CountDefinition { get => countDefinition.StripHTML(); set => countDefinition = value; }
+        public string DenominatorSource { get => denominatorSource.StripHTML(); set => denominatorSource = value; }
+        public string DenominatorDefinition { get => denominatorDefinition.StripHTML(); set => denominatorDefinition = value; }
+        public string DisclouseControl { get => disclouseControl.StripHTML(); set => disclouseControl = value; }
+        public string Caveats { get => caveats.StripHTML(); set => caveats = value; }
+        public string Copyright { get => copyright.StripHTML(); set => copyright = value; }
+        public string Reuse { get; set; }
+        public string Notes { get => notes.StripHTML(); set => notes = value; }
+        public string Frequency { get; set; }
+        public string Rounding { get; set; }
+
+        public string Polarity { get; set; }
+
+        public bool UseProportionsForTrend { get; set; }
+
+        public string BenchmarkComparisonMethod { get; set; }
+
+        public List<string> AssociatedAreaCodes { get; set; } = [];
+
+    }
+}
+
+    
+
+
