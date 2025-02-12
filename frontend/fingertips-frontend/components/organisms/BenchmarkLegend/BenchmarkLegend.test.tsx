c26d01ceebe8b1e24793ef51df018061e23c9192
obarojohnson <obaro.johnson@bjss.com>
2025-02-12 09:44:21 +0000

    Feature/DHSCFT-215_benchmarkcomponent (#129)
    Co-authored-by: Obaro I. Johnson <obaro.johnson1@nhs.net>

---
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import { BenchmarkLegend } from '.';
+import '@testing-library/jest-dom';
+import { describe } from 'node:test';
+
+describe('Testing the benchmark component', () => {
+  it('renders correctly with provided benchmark data', () => {
+    render(<BenchmarkLegend />);
+    expect(screen.queryByText('legend_panel_header')).not.toBeInTheDocument();
+    expect(screen.getByText('Areas compared to England')).toBeInTheDocument();
+    expect(screen.getByText('Better (95%)')).toBeInTheDocument();
+    expect(screen.getByText('Similar')).toBeInTheDocument();
+    expect(screen.getByText('Not compared')).toBeInTheDocument();
+    expect(screen.getByText('Lower (95%)')).toBeInTheDocument();
+    expect(screen.getByText('Higher (95%)')).toBeInTheDocument();
+
+    expect(screen.getByText('Quintile groupings')).toBeInTheDocument();
+    expect(screen.getByText('Lowest')).toBeInTheDocument();
+    expect(screen.getByText('Low')).toBeInTheDocument();
+    expect(screen.getAllByText('Middle')).toHaveLength(2);
+    expect(screen.getByText('High')).toBeInTheDocument();
+    expect(screen.getByText('Highest')).toBeInTheDocument();
+
+    // No Labels
+    expect(screen.getByText('Worst')).toBeInTheDocument();
+    expect(screen.getByText('Worse')).toBeInTheDocument();
+    expect(screen.getByText('Better')).toBeInTheDocument();
+    expect(screen.getByText('Best')).toBeInTheDocument();
+  });
+
+  it('Snapshot testing of the UI', () => {
+    const container = render(<BenchmarkLegend />);
+    expect(container.asFragment()).toMatchSnapshot();
+  });
+});
