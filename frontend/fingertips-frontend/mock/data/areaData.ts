ba001944b76351534bea887f4463b9ca82e07073
david.wolfson@bjss.com <david.wolfson@bjss.com>
2025-02-17 09:39:15 +0000

    add preliminary parent heriachy in mock area data

--- frontend/fingertips-frontend/mock/data/areaData.ts
+++ frontend/fingertips-frontend/mock/data/areaData.ts
@@ -54,36 +54,66 @@ export const mockAreaDataForRegionsStatistical: Record<
 > = {
   E12000001: {
     ...northEastRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E12000002: {
     ...northWestRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E12000003: {
     ...yorkshireAndHumberRegion,
+    parent: {
+      ...englandArea,
+    },
   },
 };
 
 export const mockAreaDataForNHSRegion: Record<string, AreaWithRelations> = {
   E40000007: {
     ...eastEnglandNHSRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E40000003: {
     ...londonNHSRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E40000005: {
     ...southEastNHSRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E40000006: {
     ...southWestNHSRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E40000010: {
     ...northWestRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E40000011: {
     ...midlandsNHSRegion,
+    parent: {
+      ...englandArea,
+    },
   },
   E40000012: {
     ...northEastAndYorkshireNHSRegion,
+    parent: {
+      ...englandArea,
+    },
   },
 };
 
@@ -91,12 +121,17 @@ export const mockAreaDataForCountiesAndUAs: Record<string, AreaWithRelations> =
   {
     E06000047: {
       ...countyDurham,
+      parent: {
+        ...northEastRegion,
+      },
     },
     E06000005: {
       ...darlington,
+      ...northEastRegion,
     },
     E08000037: {
       ...gateshead,
+      ...northEastRegion,
     },
   };
 
