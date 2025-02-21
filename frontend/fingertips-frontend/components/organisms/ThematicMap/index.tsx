04fa6ae358fc7799af1d390c90bc17b1507c1bc8
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-02-21 12:20:02 +0000

    update useEffect to depend on mapData prop

--- frontend/fingertips-frontend/components/organisms/ThematicMap/index.tsx
+++ frontend/fingertips-frontend/components/organisms/ThematicMap/index.tsx
@@ -91,16 +91,11 @@ export function ThematicMap({
   };
   // useEffect and async loading of map module to address issue with Highcharts 12 with Next 15.
   // See: https://github.com/highcharts/highcharts-react/issues/502#issuecomment-2531711517
-  //
-  // as such, we're (mis)using useEffect to load the map on initial render, and not for interactivity.
-  // the lint directive doesn't really apply here, and having either no dependency array, or mapOptions as a dependency
-  // causes it to loop infinitely.
   useEffect(() => {
     loadHighchartsModules(async () => {
       setOptions(mapOptions);
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [mapData]);
 
   if (!options) {
     return null;
