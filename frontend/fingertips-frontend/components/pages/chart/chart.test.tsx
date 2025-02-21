71c25b5cd3378e27f5978ba89760635c9836049f
david.wolfson@bjss.com <david.wolfson@bjss.com>
2025-02-21 12:48:32 +0000

    change mock indicatorIDs to match an unused ID from search to allow journey to be tested

--- frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
+++ frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
@@ -106,7 +106,7 @@ it('should render the ThematicMap component when all map props are provided', ()
   const mapData = getMapData(areaType, areaCodes);
 
   render(
-    <Chart healthIndicatorData={[mockHealthData['318']]} mapData={mapData} />
+    <Chart healthIndicatorData={[mockHealthData['92420']]} mapData={mapData} />
   );
 
   const thematicMap = screen.queryByTestId('thematicMap-component');
@@ -114,7 +114,7 @@ it('should render the ThematicMap component when all map props are provided', ()
 });
 
 it('should _not_ render the ThematicMap component when map props are _not_ provided', async () => {
-  render(<Chart healthIndicatorData={[mockHealthData['318']]} />);
+  render(<Chart healthIndicatorData={[mockHealthData['92420']]} />);
   const thematicMap = screen.queryByTestId('thematicMap-component');
 
   expect(thematicMap).not.toBeInTheDocument();
