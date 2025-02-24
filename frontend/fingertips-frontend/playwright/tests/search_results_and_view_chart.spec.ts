8ed3da7abf40b1a36c37e339df3db24900e64f3b
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-02-24 09:01:29 +0000

    Comment end to end test to see which test fails

--- frontend/fingertips-frontend/playwright/tests/search_results_and_view_chart.spec.ts
+++ frontend/fingertips-frontend/playwright/tests/search_results_and_view_chart.spec.ts
@@ -25,123 +25,123 @@ test.describe('Search via indicator', () => {
     _indicatorIDs = getIndicatorIdsByName(typedIndicatorData, searchTerm);
   });
 
-  test('full end to end flow with accessibility checks', async ({
-    homePage,
-    resultsPage,
-    chartPage,
-    axeBuilder,
-  }) => {
-    await test.step('Navigate to and verify search page', async () => {
-      await homePage.navigateToHomePage();
-      await homePage.checkOnHomePage();
-      await expectNoAccessibilityViolations(axeBuilder);
-    });
+  // test('full end to end flow with accessibility checks', async ({
+  //   homePage,
+  //   resultsPage,
+  //   chartPage,
+  //   axeBuilder,
+  // }) => {
+  //   await test.step('Navigate to and verify search page', async () => {
+  //     await homePage.navigateToHomePage();
+  //     await homePage.checkOnHomePage();
+  //     await expectNoAccessibilityViolations(axeBuilder);
+  //   });
 
-    await test.step('Search for indicators and check results', async () => {
-      await homePage.typeIndicator(searchTerm);
-      await homePage.clickSearchButton();
+  //   await test.step('Search for indicators and check results', async () => {
+  //     await homePage.typeIndicator(searchTerm);
+  //     await homePage.clickSearchButton();
 
-      await resultsPage.waitForURLToContain(searchTerm);
-      await expectNoAccessibilityViolations(axeBuilder);
-      await resultsPage.checkSearchResults(searchTerm);
-    });
+  //     await resultsPage.waitForURLToContain(searchTerm);
+  //     await expectNoAccessibilityViolations(axeBuilder);
+  //     await resultsPage.checkSearchResults(searchTerm);
+  //   });
 
-    await test.step('Validate indicator search on results page', async () => {
-      await resultsPage.clearIndicatorSearchBox();
-      await resultsPage.clickIndicatorSearchButton();
-      await resultsPage.checkForIndicatorSearchError();
+  //   await test.step('Validate indicator search on results page', async () => {
+  //     await resultsPage.clearIndicatorSearchBox();
+  //     await resultsPage.clickIndicatorSearchButton();
+  //     await resultsPage.checkForIndicatorSearchError();
 
-      await expectNoAccessibilityViolations(axeBuilder);
+  //     await expectNoAccessibilityViolations(axeBuilder);
 
-      await resultsPage.fillIndicatorSearch(searchTerm);
-      await resultsPage.clickIndicatorSearchButtonAndWait(searchTerm);
-      await resultsPage.checkSearchResults(searchTerm);
-    });
+  //     await resultsPage.fillIndicatorSearch(searchTerm);
+  //     await resultsPage.clickIndicatorSearchButtonAndWait(searchTerm);
+  //     await resultsPage.checkSearchResults(searchTerm);
+  //   });
 
-    await test.step('Select two indicators and view charts', async () => {
-      await resultsPage.selectIndicatorCheckboxes(indicatorsToSelect);
-      await resultsPage.waitForURLToContain(
-        `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[1]}`
-      );
+  //   await test.step('Select two indicators and view charts', async () => {
+  //     await resultsPage.selectIndicatorCheckboxes(indicatorsToSelect);
+  //     await resultsPage.waitForURLToContain(
+  //       `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[1]}`
+  //     );
 
-      await resultsPage.clickViewChartsButton();
-      await chartPage.waitForURLToContain(
-        `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[1]}`
-      );
-      await expectNoAccessibilityViolations(axeBuilder);
-      await chartPage.checkChartVisibility(
-        IndicatorMode.TWO_INDICATORS,
-        AreaMode.ENGLAND_AREA // defaults to this if no selection made
-      );
-    });
+  //     await resultsPage.clickViewChartsButton();
+  //     await chartPage.waitForURLToContain(
+  //       `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[1]}`
+  //     );
+  //     await expectNoAccessibilityViolations(axeBuilder);
+  //     await chartPage.checkChartVisibility(
+  //       IndicatorMode.TWO_INDICATORS,
+  //       AreaMode.ENGLAND_AREA // defaults to this if no selection made
+  //     );
+  //   });
 
-    await test.step('Return to results page and verify selections are preselected', async () => {
-      await chartPage.clickBackLink();
+  //   await test.step('Return to results page and verify selections are preselected', async () => {
+  //     await chartPage.clickBackLink();
 
-      await resultsPage.waitForURLToContain(
-        `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[1]}`
-      );
-      await resultsPage.checkSearchResults(searchTerm);
-      await resultsPage.checkIndicatorCheckboxChecked(indicatorsToSelect[0]);
-      await resultsPage.checkIndicatorCheckboxChecked(indicatorsToSelect[1]);
-    });
+  //     await resultsPage.waitForURLToContain(
+  //       `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[1]}`
+  //     );
+  //     await resultsPage.checkSearchResults(searchTerm);
+  //     await resultsPage.checkIndicatorCheckboxChecked(indicatorsToSelect[0]);
+  //     await resultsPage.checkIndicatorCheckboxChecked(indicatorsToSelect[1]);
+  //   });
 
-    await test.step('Return to search page and verify fields are correctly prepopulated', async () => {
-      await resultsPage.clickBackLink();
+  //   await test.step('Return to search page and verify fields are correctly prepopulated', async () => {
+  //     await resultsPage.clickBackLink();
 
-      await homePage.waitForURLToContain(
-        `${SearchParams.SearchedIndicator}=${searchTerm}`
-      );
-      await homePage.checkSearchFieldIsPrePopulatedWith(searchTerm);
-    });
+  //     await homePage.waitForURLToContain(
+  //       `${SearchParams.SearchedIndicator}=${searchTerm}`
+  //     );
+  //     await homePage.checkSearchFieldIsPrePopulatedWith(searchTerm);
+  //   });
 
-    await test.step('Select single indicator and view charts', async () => {
-      await homePage.clearSearchIndicatorField();
-      await homePage.typeIndicator(searchTerm);
-      await homePage.clickSearchButton();
+  //   await test.step('Select single indicator and view charts', async () => {
+  //     await homePage.clearSearchIndicatorField();
+  //     await homePage.typeIndicator(searchTerm);
+  //     await homePage.clickSearchButton();
 
-      await resultsPage.waitForURLToContain(
-        `${SearchParams.SearchedIndicator}=${searchTerm}`
-      );
+  //     await resultsPage.waitForURLToContain(
+  //       `${SearchParams.SearchedIndicator}=${searchTerm}`
+  //     );
 
-      await resultsPage.selectIndicatorCheckboxes([indicatorsToSelect[0]]);
-      await resultsPage.waitForURLToContain(
-        `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}`
-      );
+  //     await resultsPage.selectIndicatorCheckboxes([indicatorsToSelect[0]]);
+  //     await resultsPage.waitForURLToContain(
+  //       `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}`
+  //     );
 
-      await resultsPage.clickViewChartsButton();
-      await chartPage.waitForURLToContain(
-        `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}`
-      );
-      await expectNoAccessibilityViolations(axeBuilder);
-      await chartPage.checkChartVisibility(
-        IndicatorMode.ONE_INDICATOR,
-        AreaMode.ENGLAND_AREA // defaults to this if no selection made
-      );
+  //     await resultsPage.clickViewChartsButton();
+  //     await chartPage.waitForURLToContain(
+  //       `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}`
+  //     );
+  //     await expectNoAccessibilityViolations(axeBuilder);
+  //     await chartPage.checkChartVisibility(
+  //       IndicatorMode.ONE_INDICATOR,
+  //       AreaMode.ENGLAND_AREA // defaults to this if no selection made
+  //     );
 
-      await chartPage.clickBackLink();
-      await resultsPage.waitForURLToContain(
-        `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}`
-      );
+  //     await chartPage.clickBackLink();
+  //     await resultsPage.waitForURLToContain(
+  //       `${searchTerm}&${SearchParams.IndicatorsSelected}=${indicatorsToSelect[0]}`
+  //     );
 
-      await resultsPage.clickBackLink();
+  //     await resultsPage.clickBackLink();
 
-      await homePage.checkOnHomePage();
-    });
+  //     await homePage.checkOnHomePage();
+  //   });
 
-    await test.step('Verify search page validation prevents forward navigation', async () => {
-      await homePage.clearSearchIndicatorField();
-      await homePage.clickSearchButton();
+  //   await test.step('Verify search page validation prevents forward navigation', async () => {
+  //     await homePage.clearSearchIndicatorField();
+  //     await homePage.clickSearchButton();
 
-      await homePage.waitForURLToContain(
-        `${SearchParams.SearchedIndicator}=${searchTerm}`
-      );
-      await homePage.checkSearchFieldIsPrePopulatedWith();
-      await homePage.checkSummaryValidation(
-        `There is a problemAt least one of the following fields must be populated:Search subjectSearch area`
-      );
-    });
-  });
+  //     await homePage.waitForURLToContain(
+  //       `${SearchParams.SearchedIndicator}=${searchTerm}`
+  //     );
+  //     await homePage.checkSearchFieldIsPrePopulatedWith();
+  //     await homePage.checkSummaryValidation(
+  //       `There is a problemAt least one of the following fields must be populated:Search subjectSearch area`
+  //     );
+  //   });
+  // });
 
   test('check available area types when no areas are selected when coming onto the results pages', async ({
     homePage,
