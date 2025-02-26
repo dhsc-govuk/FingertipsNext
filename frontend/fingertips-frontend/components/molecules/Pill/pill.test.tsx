b120c22a9ec22f63eed623a935236a37c5a3bf4a
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-02-26 13:55:08 +0000

    fix text

--- frontend/fingertips-frontend/components/molecules/Pill/pill.test.tsx
+++ frontend/fingertips-frontend/components/molecules/Pill/pill.test.tsx
@@ -24,11 +24,10 @@ describe('Pill Suite', () => {
     expect(screen.getByTestId('pill-container')).toBeInTheDocument();
     expect(screen.getByTestId('filter-name')).toBeInTheDocument();
     expect(screen.getByTestId('remove-icon-div')).toBeInTheDocument();
-    expect(screen.getByRole('paragraph')).toBeInTheDocument();
     expect(screen.getByTestId('x-icon')).toBeInTheDocument();
   });
 
-  it('should render text passed in as prop', () => {
+  it('should render child text passed in as prop', () => {
     render(
       <Pill
         selectedFilterId={selectedFilterId}
@@ -37,8 +36,7 @@ describe('Pill Suite', () => {
         {selectedFilterName}
       </Pill>
     );
-
-    expect(screen.getByRole('paragraph')).toHaveTextContent('Dementia');
+    expect(screen.getByText('Dementia')).toBeInTheDocument()
   });
 
   it('should call the passed in handle function when clicking the remove icon', async () => {
