5a6cd80467cf3176ee7adf3f9c9c15a9a60793c7
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-02-27 13:02:08 +0000

    DHSCFT-408 add url capture to logs for test fails (#194)

--- frontend/fingertips-frontend/playwright/tests/fully_integrated_e2e_tests/core_journeys.spec.ts
+++ frontend/fingertips-frontend/playwright/tests/fully_integrated_e2e_tests/core_journeys.spec.ts
@@ -89,3 +89,18 @@ test.describe(`Search via search term ${searchTerm}`, () => {
     });
   });
 });
+
+// log out current url when a test fails
+test.afterEach(async ({ page }, testInfo) => {
+  if (testInfo.status !== testInfo.expectedStatus) {
+    // Test failed - capture the URL
+    const url = page.url();
+    console.log(`Test failed! Current URL: ${url}`);
+
+    // You can also attach it to the test report
+    await testInfo.attach('failed-url', {
+      body: url,
+      contentType: 'text/plain',
+    });
+  }
+});
