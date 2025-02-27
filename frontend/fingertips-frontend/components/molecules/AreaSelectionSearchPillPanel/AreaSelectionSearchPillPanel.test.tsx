d879e93d1ef3d502b03cb2131e9785d325abcc84
obarojohnson <obaro.johnson@bjss.com>
2025-02-27 10:51:28 +0000

    Feature/dhscft 121 area search component (#160)
    Co-authored-by: Howard May <howard.may@bjss.com>
    Co-authored-by: Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
    Co-authored-by: Obaro I. Johnson <obaro.johnson1@nhs.net>

---
+++ frontend/fingertips-frontend/components/molecules/AreaSelectionSearchPillPanel/AreaSelectionSearchPillPanel.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import { AreaAutoCompletePillPanel } from './index';
+import { AreaDocument } from '@/lib/search/searchTypes';
+import { userEvent } from '@testing-library/user-event';
+
+describe('AreaSelectionSearchPillPanel', () => {
+  const areas: AreaDocument[] = [
+    { areaCode: '001', areaName: 'London', areaType: 'GPs' },
+    { areaCode: '002', areaName: 'Manchester', areaType: 'GPs' },
+  ];
+  const mockOnRemovePill = jest.fn();
+
+  it('take a snapshot of component and it renders correctly', () => {
+    const container = render(
+      <AreaAutoCompletePillPanel
+        areas={[
+          {
+            areaCode: 'GPs',
+            areaName: 'Some gp practice here',
+            areaType: 'some types information',
+          },
+        ]}
+        onRemovePill={jest.fn()}
+      />
+    );
+    expect(container.asFragment()).toMatchSnapshot();
+  });
+
+  it('calls onRemovePill when a pill button is clicked', async () => {
+    render(
+      <AreaAutoCompletePillPanel
+        areas={areas}
+        onRemovePill={mockOnRemovePill}
+      />
+    );
+    const user = userEvent.setup();
+    mockOnRemovePill.mockClear();
+    const removeButtons = screen.getAllByTestId('remove-icon-div');
+    for (let i = 0; i < areas.length; i++) {
+      mockOnRemovePill.mockClear();
+      await user.click(removeButtons[i]);
+      expect(mockOnRemovePill).toHaveBeenCalledWith(areas[i]);
+    }
+  });
+});
