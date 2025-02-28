eeee67e059d9eb65f03e7dd658966a5970e46a82
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2025-02-28 17:36:04 +0000

    Update frontend/fingertips-frontend/components/views/ViewsContext.tsx
    Co-authored-by: Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
    Signed-off-by: David Wolfson <145220120+WolfieKnee@users.noreply.github.com>

--- frontend/fingertips-frontend/components/views/ViewsContext.tsx
+++ frontend/fingertips-frontend/components/views/ViewsContext.tsx
@@ -19,18 +19,23 @@ function viewSelector(
   indicators: string[],
   searchState: SearchStateParams
 ): JSX.Element | undefined {
-  if (indicators.length === 1) {
-    if (areaCodes.length === 1) {
-      return <OneIndicatorOneAreaView searchState={searchState} />;
-    } else if (areaCodes.length >= 2) {
-      return <OneIndicatorTwoOrMoreAreasView searchState={searchState} />;
-    }
-  } else if (indicators.length >= 2) {
-    if (areaCodes.length === 1 && areaCodes[0] === areaCodeForEngland) {
-      return <TwoOrMoreIndicatorsEnglandView searchState={searchState} />;
-    }
-    return <TwoOrMoreIndicatorsAreasView searchState={searchState} />;
+  if (indicators.length === 1 && areaCodes.length === 1) {
+    return <OneIndicatorOneAreaView searchState={searchState} />;
   }
+
+  if (indicators.length === 1 && areaCodes.length >= 2) {
+    return <OneIndicatorTwoOrMoreAreasView searchState={searchState} />;
+  }
+
+  if (
+    indicators.length >= 2 &&
+    areaCodes.length === 1 &&
+    areaCodes[0] === areaCodeForEngland
+  ) {
+    return <TwoOrMoreIndicatorsEnglandView searchState={searchState} />;
+  }
+
+  return <TwoOrMoreIndicatorsAreasView searchState={searchState} />;
 }
 
 export function ViewsContext({ searchState }: Readonly<ViewsContextProps>) {
