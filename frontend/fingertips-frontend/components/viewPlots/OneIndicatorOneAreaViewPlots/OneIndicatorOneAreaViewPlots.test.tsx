9f038bf139bd2db25c72cc18ad3c6b5293f19155
david.wolfson@bjss.com <david.wolfson@bjss.com>
2025-03-04 13:35:10 +0000

    don't show line chart when only one year of data are available

--- frontend/fingertips-frontend/components/viewPlots/OneIndicatorOneAreaViewPlots/OneIndicatorOneAreaViewPlots.test.tsx
+++ frontend/fingertips-frontend/components/viewPlots/OneIndicatorOneAreaViewPlots/OneIndicatorOneAreaViewPlots.test.tsx
@@ -13,7 +13,7 @@ jest.mock('next/navigation', () => {
 });
 
 describe('OneIndicatorOneAreaViewPlots', () => {
-  beforeEach(() => {
+  it('should render the view with the title with correct text', () => {
     const searchState: SearchStateParams = {
       [SearchParams.IndicatorsSelected]: ['108'],
       [SearchParams.AreasSelected]: ['E12000001'],
@@ -24,10 +24,12 @@ describe('OneIndicatorOneAreaViewPlots', () => {
         searchState={searchState}
       />
     );
-  });
-  it('should render the view title with correct text', () => {
+
     const heading = screen.getByRole('heading', { level: 2 });
 
+    expect(
+      screen.getByTestId('oneIndicatorOneAreaViewPlot-component')
+    ).toBeInTheDocument();
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent(
       'View data for selected indicators and areas'
@@ -35,9 +37,16 @@ describe('OneIndicatorOneAreaViewPlots', () => {
   });
 
   it('should render the LineChart components', async () => {
-    expect(
-      screen.getByTestId('oneIndicatorOneAreaViewPlot-component')
-    ).toBeInTheDocument();
+    const searchState: SearchStateParams = {
+      [SearchParams.IndicatorsSelected]: ['108'],
+      [SearchParams.AreasSelected]: ['E12000001'],
+    };
+    render(
+      <OneIndicatorOneAreaViewPlots
+        healthIndicatorData={[mockHealthData['108'][1]]}
+        searchState={searchState}
+      />
+    );
     expect(
       screen.getByRole('heading', {
         name: 'See how the indicator has changed over time',
@@ -52,5 +61,39 @@ describe('OneIndicatorOneAreaViewPlots', () => {
     expect(
       screen.getByTestId('tabContainer-lineChartAndTable')
     ).toBeInTheDocument();
+  });
+
+  it('should not display line chart and line chart table when there are less than 2 time periods per area selected', () => {
+    const MOCK_DATA = [
+      {
+        areaCode: 'A1',
+        areaName: 'Area 1',
+        healthData: [mockHealthData['1'][0].healthData[0]],
+      },
+    ];
+
+    const state: SearchStateParams = {
+      [SearchParams.IndicatorsSelected]: ['0'],
+    };
+
+    render(
+      <OneIndicatorOneAreaViewPlots
+        healthIndicatorData={MOCK_DATA}
+        searchState={state}
+      />
+    );
+
+    expect(
+      screen.queryByRole('heading', {
+        name: 'See how the indicator has changed over time',
+      })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId('tabContainer-lineChartAndTable')
+    ).not.toBeInTheDocument();
+    expect(screen.queryByTestId('lineChart-component')).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId('tabContainer-lineChartAndTable')
+    ).not.toBeInTheDocument();
   });
 });
--- frontend/fingertips-frontend/components/viewPlots/OneIndicatorOneAreaViewPlots/index.tsx
+++ frontend/fingertips-frontend/components/viewPlots/OneIndicatorOneAreaViewPlots/index.tsx
@@ -44,37 +44,41 @@ export function OneIndicatorOneAreaViewPlots({
   return (
     <section data-testid="oneIndicatorOneAreaViewPlot-component">
       <H2>View data for selected indicators and areas</H2>
-      <H3>See how the indicator has changed over time</H3>
-      <TabContainer
-        id="lineChartAndTable"
-        items={[
-          {
-            id: 'lineChart',
-            title: 'Line chart',
-            content: (
-              <LineChart
-                healthIndicatorData={dataWithoutEngland}
-                benchmarkData={englandBenchmarkData}
-                searchState={searchState}
-                groupIndicatorData={groupData}
-                xAxisTitle="Year"
-                accessibilityLabel="A line chart showing healthcare data"
-              />
-            ),
-          },
-          {
-            id: 'table',
-            title: 'Tabular data',
-            content: (
-              <LineChartTable
-                healthIndicatorData={dataWithoutEngland}
-                englandBenchmarkData={englandBenchmarkData}
-                groupIndicatorData={groupData}
-              />
-            ),
-          },
-        ]}
-      />
+      {dataWithoutEngland[0]?.healthData.length > 1 && (
+        <>
+          <H3>See how the indicator has changed over time</H3>
+          <TabContainer
+            id="lineChartAndTable"
+            items={[
+              {
+                id: 'lineChart',
+                title: 'Line chart',
+                content: (
+                  <LineChart
+                    healthIndicatorData={dataWithoutEngland}
+                    benchmarkData={englandBenchmarkData}
+                    searchState={searchState}
+                    groupIndicatorData={groupData}
+                    xAxisTitle="Year"
+                    accessibilityLabel="A line chart showing healthcare data"
+                  />
+                ),
+              },
+              {
+                id: 'table',
+                title: 'Tabular data',
+                content: (
+                  <LineChartTable
+                    healthIndicatorData={dataWithoutEngland}
+                    englandBenchmarkData={englandBenchmarkData}
+                    groupIndicatorData={groupData}
+                  />
+                ),
+              },
+            ]}
+          />
+        </>
+      )}
 
       <H3>Inequalities Visualisations</H3>
       <H3>Population Visualisations</H3>
