4235e1b19b6d06f2df0279e786978cf0d52a67aa
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-03-04 15:52:36 +0000

    put component back to how it was

--- frontend/fingertips-frontend/components/organisms/LineChart/index.tsx
+++ frontend/fingertips-frontend/components/organisms/LineChart/index.tsx
@@ -77,7 +77,7 @@ export function LineChart({
     credits: {
       enabled: false,
     },
-    chart: { type: 'line', height: '70%', spacingBottom: 75, spacingTop: 4 },
+    chart: { type: 'line', height: '50%', spacingBottom: 50, spacingTop: 20 },
     title: {
       style: {
         display: 'none',
@@ -94,7 +94,7 @@ export function LineChart({
     },
     legend: {
       title: {
-        text: 'BAssaas',
+        text: 'Areas',
       },
       verticalAlign: 'top',
       align: 'left',
