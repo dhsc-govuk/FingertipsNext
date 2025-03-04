17d04278613d09dd22315ba5d16ee67a0b1a92b5
Euan Percy <euan.percy@bjss.com>
2025-03-04 16:31:37 +0000

    Import CSV Data into Fingertips Database (#210)
    Co-authored-by: Andrew Evans <andrew.evans@bjss.com>
    Co-authored-by: williamwillis11 <92571853+williamwillis11@users.noreply.github.com>

--- database/fingertips-db/entrypoint.sh
+++ database/fingertips-db/entrypoint.sh
@@ -19,4 +19,8 @@ sqlpackage \
     /TargetDatabaseName:$DB_NAME \
     /TargetUser:$SA_USERNAME \
     /TargetPassword:$SA_PASSWORD \
+    /v:LocalFilePath="/var/opt/mssql/csv/" \
+    /v:UseAzureBlob=0 \
+    /diagnostics:true \
+    /diagnosticsLevel:"Verbose" \
     /TargetTrustServerCertificate:${TRUST_CERT}
