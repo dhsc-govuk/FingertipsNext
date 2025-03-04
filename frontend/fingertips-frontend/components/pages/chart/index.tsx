645424141f905f18706e7ac35d0ac2796ea6f6a3
david.wolfson@bjss.com <david.wolfson@bjss.com>
2025-03-04 15:53:21 +0000

    remove unused import

--- frontend/fingertips-frontend/components/pages/chart/index.tsx
+++ frontend/fingertips-frontend/components/pages/chart/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BackLink, H2 } from 'govuk-react';
+import { BackLink } from 'govuk-react';
 import { HealthDataForArea } from '@/generated-sources/ft-api-client';
 import {
   SearchParams,
