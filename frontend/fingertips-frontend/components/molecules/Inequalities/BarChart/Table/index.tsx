6398c6a2802ade48dcb1cf66095b44311ff01353
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-03-05 17:05:10 +0000

    rename column

--- frontend/fingertips-frontend/components/molecules/Inequalities/BarChart/Table/index.tsx
+++ frontend/fingertips-frontend/components/molecules/Inequalities/BarChart/Table/index.tsx
@@ -21,7 +21,7 @@ interface InequalitiesBarChartTableProps {
 }
 
 export enum InequalitiesBarChartTableHeaders {
-  GROUPING_TYPES = 'Groups',
+  INEQUALITY_TYPE = 'Inequality type',
   COMPARED_TO = 'Compared to Persons',
   COUNT = 'Count',
   VALUE = 'Value',
@@ -30,9 +30,10 @@ export enum InequalitiesBarChartTableHeaders {
 }
 
 const getCellHeader = (header: InequalitiesBarChartTableHeaders): ReactNode =>
-  header === InequalitiesBarChartTableHeaders.GROUPING_TYPES ? (
+  header === InequalitiesBarChartTableHeaders.INEQUALITY_TYPE ? (
     <StyledAlignLeftHeader
       key={`heading-${header}`}
+      style={{ width: '16%' }}
       data-testid={`heading-${header}`}
     >
       {header}
