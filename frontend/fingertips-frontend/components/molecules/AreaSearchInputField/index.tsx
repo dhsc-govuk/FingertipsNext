ba188873802b10423dc04f8c6d5f60f33fd37e9c
Fawad Ali <alibombaye@users.noreply.github.com>
2025-03-07 10:12:20 +0000

    DHSCFT-417: tidy up area suggestions (#219)

--- frontend/fingertips-frontend/components/molecules/AreaSearchInputField/index.tsx
+++ frontend/fingertips-frontend/components/molecules/AreaSearchInputField/index.tsx
@@ -7,28 +7,25 @@ const StyleAreaSearchInputField = styled('div')({
 });
 
 interface AreaSearchInputFieldProps {
-  onTextChange?: (criteria: string) => void;
+  onTextChange: (criteria: string) => void;
   disabled?: boolean;
-  touched?: boolean;
+  hasError?: boolean;
   value: string | undefined;
 }
 
 export const AreaSearchInputField = ({
   onTextChange,
-  touched,
+  hasError,
   disabled,
   value,
 }: AreaSearchInputFieldProps) => {
   return (
-    <StyleAreaSearchInputField>
+    <StyleAreaSearchInputField data-testid="area-search-input-field">
       <InputField
         input={{
           value: value ?? '',
           onChange: (e) => {
-            const val = e.target.value;
-            if (onTextChange) {
-              onTextChange(val);
-            }
+            onTextChange(e.target.value);
           },
           disabled: disabled,
         }}
@@ -39,10 +36,9 @@ export const AreaSearchInputField = ({
           </div>
         }
         meta={{
-          touched: touched,
-          error: touched ? 'Enter an area you want to search for' : '',
+          touched: hasError,
+          error: hasError ? 'Enter an area you want to search for' : '',
         }}
-        data-testid="search-form-input-area"
       >
         Search for an area
       </InputField>
