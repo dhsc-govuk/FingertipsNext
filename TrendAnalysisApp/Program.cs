d67c6978420b1a65037ddabb215e0df41db2ad13
Daniel Yip <64152559+dlzhry2@users.noreply.github.com>
2025-03-07 12:22:07 +0000

    DHSCFT-366: Created trend analysis skeleton application (#199)

---
+++ TrendAnalysisApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrendAnalysisApp.Repository;
+namespace TrendAnalysisApp;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        CreateServices().GetRequiredService<TrendDataProcessor>().Process().Wait();
+    }
+
+    private static ServiceProvider CreateServices()
+    {
+        var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        var connString = configuration.GetConnectionString(Constants.Database.FingertipsDbName);
+        
+        return new ServiceCollection()
+            .AddSingleton<IConfiguration>(configuration)
+            .AddDbContext<HealthMeasureDbContext>(options => options.UseSqlServer(connString))
+            .AddSingleton<TrendDataProcessor>()
+            .AddSingleton<HealthMeasureRepository>()
+            .BuildServiceProvider();
+    }
+}
