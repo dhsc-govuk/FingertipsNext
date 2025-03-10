520e0ab6d691fb48088567f41aca4d749503c007
Stuart <stuart.craigen@bjss.com>
2025-03-10 15:28:12 +0000

    DHSCFT-391: Separated BOB and RAG colours in Benchmark

--- frontend/fingertips-frontend/components/organisms/BenchmarkLegend/index.tsx
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/index.tsx
@@ -2,6 +2,7 @@
 
 import { BenchmarkLabel } from '@/components/organisms/BenchmarkLabel';
 import styled from 'styled-components';
+import { Fragment } from 'react';
 
 const DefaultBenchmarkLegendGroupPanelStyle = styled('div')({
   alignItems: 'center',
@@ -15,61 +16,79 @@ const DefaultBenchmarkLegendHeaderStyle = styled('h4')({
   fontWeight: 300,
 });
 
-const StyledLengendLabel = styled('span')({
+const StyledLegendLabel = styled('span')({
   fontFamily: 'nta,Arial,sans-serif',
   fontWeight: 300,
 });
 
 export interface BenchmarkData {
+  key: string;
   title?: string;
-  group: string;
-  types: string[];
+  types: Record<string, string[]>;
   suffix?: string;
 }
 
 export const BenchmarkLegend = () => {
   const model: BenchmarkData[] = [
     {
-      group: 'rag',
+      key: 'rag',
       title: 'Compared to England',
-      types: ['Better', 'Similar', 'Worse', 'not_compared', 'Lower', 'Higher'],
+      types: {
+        rag: ['Better', 'Similar', 'Worse', 'not_compared'],
+        bob: ['Lower', 'Higher'],
+      },
       suffix: '(95% confidence)',
     },
     {
-      group: 'rag_99',
-      types: ['Better', 'Similar', 'Worse', 'not_compared', 'Lower', 'Higher'],
+      key: 'rag_99',
+      types: {
+        rag_99: ['Better', 'Similar', 'Worse', 'not_compared'],
+        bob_99: ['Lower', 'Higher'],
+      },
       suffix: '(99.8% confidence)',
     },
     {
+      key: 'quintiles',
       title: ' Quintiles',
-      group: 'quintiles',
-      types: ['Lowest', 'Low', 'Middle', 'High', 'Highest'],
+      types: { quintiles: ['Lowest', 'Low', 'Middle', 'High', 'Highest'] },
     },
     {
-      group: 'quintiles_with_judgement',
-      types: ['Worst', 'Worse', 'Middle', 'Better', 'Best'],
+      key: 'quintiles_with_judgement',
+      types: {
+        quintiles_with_judgement: [
+          'Worst',
+          'Worse',
+          'Middle',
+          'Better',
+          'Best',
+        ],
+      },
     },
   ];
 
   return (
     <div>
       {model?.map((item, index) => (
-        <DefaultBenchmarkLegendGroupPanelStyle key={item.group + '_' + index}>
+        <DefaultBenchmarkLegendGroupPanelStyle key={item.key}>
           {item.title ? (
             <DefaultBenchmarkLegendHeaderStyle>
               {item.title}
             </DefaultBenchmarkLegendHeaderStyle>
           ) : null}
           <div>
-            {item.types.map((type, label_key) => (
-              <BenchmarkLabel
-                group={item.group}
-                type={type}
-                key={item.group + '_' + type + '_' + index + '-' + label_key}
-              />
+            {Object.keys(item.types).map((group) => (
+              <Fragment key={group}>
+                {Object.values(item.types[group]).map((type) => (
+                  <BenchmarkLabel
+                    group={group}
+                    type={type}
+                    key={group + '_' + type + '_' + index}
+                  />
+                ))}
+              </Fragment>
             ))}
             {item.suffix ? (
-              <StyledLengendLabel>{item.suffix}</StyledLengendLabel>
+              <StyledLegendLabel>{item.suffix}</StyledLegendLabel>
             ) : null}
           </div>
         </DefaultBenchmarkLegendGroupPanelStyle>
