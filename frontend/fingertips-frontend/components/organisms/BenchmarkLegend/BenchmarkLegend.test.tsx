1db3ac8a49ebc1211ad57523a4f94143b22b7dfe
Stuart <stuart.craigen@bjss.com>
2025-03-10 11:50:47 +0000

    DHSCFT-391: Removed test and just left the snapshot test

--- frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
@@ -1,44 +1,8 @@
-import { render, screen, within } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BenchmarkLegend } from '.';
 import '@testing-library/jest-dom';
 
 describe('Testing the benchmark component', () => {
-  it('renders correctly with provided benchmark data', () => {
-    render(<BenchmarkLegend />);
-
-    const txtEngland = screen.getByText('Compared to England');
-    expect(screen.queryByText('legend_panel_header')).not.toBeInTheDocument();
-    expect(txtEngland).toBeInTheDocument();
-
-    const divRag = txtEngland.parentElement as HTMLElement;
-    expect(divRag).not.toBeNull();
-    expect(within(divRag).getByText('Better')).toBeInTheDocument();
-    expect(within(divRag).getByText('Similar')).toBeInTheDocument();
-    expect(within(divRag).getByText('Not compared')).toBeInTheDocument();
-    expect(within(divRag).getByText('Lower')).toBeInTheDocument();
-    expect(within(divRag).getByText('Higher')).toBeInTheDocument();
-
-    const txtQuintile = screen.getByText('Quintiles');
-    expect(txtQuintile).toBeInTheDocument();
-
-    const divQuintile = txtQuintile.parentElement as HTMLElement;
-    expect(divQuintile).not.toBeNull();
-    expect(within(divQuintile).getByText('Lowest')).toBeInTheDocument();
-    expect(within(divQuintile).getByText('Low')).toBeInTheDocument();
-    expect(within(divQuintile).getByText('Middle')).toBeInTheDocument();
-    expect(within(divQuintile).getByText('High')).toBeInTheDocument();
-    expect(within(divQuintile).getByText('Highest')).toBeInTheDocument();
-
-    // No Labels
-    const divQuintileWV = divQuintile.nextSibling as HTMLElement;
-    expect(divQuintileWV).not.toBeNull();
-    expect(within(divQuintileWV).getByText('Worst')).toBeInTheDocument();
-    expect(within(divQuintileWV).getByText('Worse')).toBeInTheDocument();
-    expect(within(divQuintileWV).getByText('Middle')).toBeInTheDocument();
-    expect(within(divQuintileWV).getByText('Better')).toBeInTheDocument();
-    expect(within(divQuintileWV).getByText('Best')).toBeInTheDocument();
-  });
-
   it('Snapshot testing of the UI', () => {
     const container = render(<BenchmarkLegend />);
     expect(container.asFragment()).toMatchSnapshot();
