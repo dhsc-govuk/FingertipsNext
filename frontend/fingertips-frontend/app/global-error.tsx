e05c9b0bb0cf4425ec8b39c360353a7ce71eae42
Gary.Stanley <gary.stanley@bjss.com>
2025-03-11 17:38:58 +0000

    comment

--- frontend/fingertips-frontend/app/global-error.tsx
+++ frontend/fingertips-frontend/app/global-error.tsx
@@ -6,6 +6,10 @@ import { FTContainer } from '@/components/layouts/container';
 import { FTFooter } from '@/components/molecules/Footer';
 import { H1, Paragraph } from 'govuk-react';
 
+// next.js will only use this page in production builds, meaning that
+// in dev builds you will see the developer oriented unhandled exception
+// page that is useful for debugging
+//
 export default function GlobalError() {
   return (
     // global-error must include html and body tags
