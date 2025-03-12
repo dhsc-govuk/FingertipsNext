521ed904d677b0bea59e2d10d67d03cd2e66997c
andyevans-bjss <andrew.evans@bjss.com>
2025-03-12 18:36:04 +0000

    Feature/dhscft 460 limit indicator numbers and remove UseProportionsForTrend (#257)

--- frontend/fingertips-frontend/playwright/tests/fully_integrated_e2e_tests/core_journeys.spec.ts
+++ frontend/fingertips-frontend/playwright/tests/fully_integrated_e2e_tests/core_journeys.spec.ts
@@ -7,7 +7,7 @@ import {
 } from '../../testHelpers';
 import indicators from '../../../../../search-setup/assets/indicators.json';
 import { IndicatorDocument } from '@/lib/search/searchTypes';
-
+//@ts-expect-error don't care about type checking this json file
 const indicatorData = indicators as IndicatorDocument[];
 const subjectSearchTerm = 'hospital';
 const areaSearchTerm = 'north west region';
