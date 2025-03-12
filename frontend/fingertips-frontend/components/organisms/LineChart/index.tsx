a7aaba1979b184a5456b84fc05942d07706929c4
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-03-12 15:01:18 +0000

    switch back

--- frontend/fingertips-frontend/components/organisms/LineChart/index.tsx
+++ frontend/fingertips-frontend/components/organisms/LineChart/index.tsx
@@ -81,7 +81,7 @@ export function LineChart({
     credits: {
       enabled: false,
     },
-    chart: { type: 'line', height: '75%', spacingBottom: 75, spacingTop: 4 },
+    chart: { type: 'line', height: '50%', spacingBottom: 50, spacingTop: 20 },
     title: {
       style: {
         display: 'none',
@@ -98,7 +98,7 @@ export function LineChart({
     },
     legend: {
       title: {
-        text: 'BAAAAA',
+        text: 'Areas',
       },
       verticalAlign: 'top',
       align: 'left',
