09887f93b69e0d96e8b5de2fb4d3a2aab47e2ca9
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-03-14 19:08:03 +0000

    undo later

--- frontend/fingertips-frontend/app/layout.tsx
+++ frontend/fingertips-frontend/app/layout.tsx
@@ -1,15 +1,19 @@
 import { FTContainer } from '@/components/layouts/container';
 import { FTFooter } from '@/components/molecules/Footer';
 import { FTHeader } from '@/components/molecules/Header';
-import { PopulationPyramid } from '@/components/organisms/PopulationPyramid';
+import { PyramidPopulationChartView } from '@/components/viewPlots/PyramidPopulationChartView';
 import StyledComponentsRegistry from '@/lib/registry';
 import type { Metadata } from 'next';
+import { mockHealthData } from '@/mock/data/healthdata';
 
 export const metadata: Metadata = {
   title: 'FingertipsNext',
   description:
     'Fingertips is a rich source of indicators across a range of health and wellbeing themes designed to support JSNA and commissioning to improve health and wellbeing, and reduce inequalities.',
 };
+
+const benchmarkEnglandData = mockHealthData['337'][0];
+const selectedData = mockHealthData['337'][1];
 
 export default function RootLayout({
   children,
@@ -24,14 +28,10 @@ export default function RootLayout({
           <FTContainer>
             {/* {children} */}
 
-            <PopulationPyramid
-              dataForSelectedArea={{
-                areaName: 'London',
-                ageCategories: ['0 to 10', '11 to 30', '31  to 40', '41 to 75'],
-                femaleSeries: [89, 10, 10, 92, 50],
-                maleSeries: [19, 20, 40, 22, 11, 50],
-              }}
-              xAxisTitle="Ages"
+            <PyramidPopulationChartView
+              populationHealthDataForAreas={mockHealthData['337']}
+              selectedAreaCode={selectedData.areaCode}
+              xAxisTitle="Age"
               yAxisTitle="Percentage"
             />
           </FTContainer>
