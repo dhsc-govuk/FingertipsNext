ecbb046b50f0bfeb6117e8825d23412819525d61
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-03-14 18:55:43 +0000

    Housing the pyramid at its base

--- frontend/fingertips-frontend/components/viewPlots/PyramidPopulationChartView.tsx
+++ frontend/fingertips-frontend/components/viewPlots/PyramidPopulationChartView.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { HealthDataForArea } from '@/generated-sources/ft-api-client';
 import { PopulationPyramid } from '@/components/organisms/PopulationPyramid';
 import { useEffect, useState } from 'react';
