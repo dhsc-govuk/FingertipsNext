f8e4f8db84f9af2a4a7634092f0666c57c60d0c9
stuc-bjss <stuart.craigen@bjss.com>
2025-03-17 15:21:54 +0000

    DHSCFT-223: Linting fixes

--- frontend/fingertips-frontend/app/chart/page.test.tsx
+++ frontend/fingertips-frontend/app/chart/page.test.tsx
@@ -15,7 +15,11 @@ import {
   API_CACHE_CONFIG,
   ApiClientFactory,
 } from '@/lib/apiClient/apiClientFactory';
-import { IndicatorsApi, GetHealthDataForAnIndicatorComparisonMethodEnum, AreasApi } from '@/generated-sources/ft-api-client';
+import {
+  IndicatorsApi,
+  GetHealthDataForAnIndicatorComparisonMethodEnum,
+  AreasApi,
+} from '@/generated-sources/ft-api-client';
 import { getMapData } from '@/lib/thematicMapUtils/getMapData';
 import NHSRegionsMap from '@/assets/maps/NHS_England_Regions_January_2024_EN_BSC_7500404208533377417.geo.json';
 import { getAreaFilterData } from '@/lib/areaFilterHelpers/getAreaFilterData';
