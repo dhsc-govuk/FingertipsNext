2654826be809cc69aeaaa5456a34b7d0ab49a1af
Daniel Yip <daniel.yip@bjss.com>
2025-03-17 17:43:42 +0000

    Refactored trend analysis application to work in parallel

--- trend-analysis/TrendAnalysisApp/TrendDataProcessor.cs
+++ trend-analysis/TrendAnalysisApp/TrendDataProcessor.cs
@@ -1,57 +1,81 @@
+using Microsoft.Extensions.DependencyInjection;
 using TrendAnalysisApp.Calculator;
+using TrendAnalysisApp.Calculator.Legacy;
+using TrendAnalysisApp.Mapper;
 using TrendAnalysisApp.Repository;
+using TrendAnalysisApp.Repository.Models;
 
 namespace TrendAnalysisApp;
 
 /// <summary>
 /// The Trend Data Processor Class.
 /// </summary>
-/// <param name="healthMeasureRepo">Repository for health measure data</param>
-/// <param name="trendCalculator"></param
+/// <param name="indicatorRepo"></param
 public class TrendDataProcessor(
-    HealthMeasureRepository healthMeasureRepo,
-    IndicatorRepository indicatorRepo,
-    TrendCalculator trendCalculator
+    IndicatorRepository indicatorRepo
 )
 {
-    private readonly HealthMeasureRepository _healthMeasureRepo = healthMeasureRepo;
     private readonly IndicatorRepository _indicatorRepo = indicatorRepo;
-    private readonly TrendCalculator _trendCalculator = trendCalculator;
 
+    /// <summary>
+    /// Static helper method which can be used in a parallel context.
+    /// It will calculate and save all trends relating to a given indicator
+    /// across multiple dimensions e.g. age, deprivation, sex, area.
+    /// </summary>
+    /// <param name="indicator"></param>
+    /// <param name="healthMeasureRepository"></param>
+    /// <param name="trendCalculator"></param>
+    private static async Task ProcessOne(
+        IndicatorDimensionModel indicator,
+        HealthMeasureRepository healthMeasureRepository,
+        TrendCalculator trendCalculator
+    )
+    {
+        var healthMeasures = await healthMeasureRepository.GetByIndicator(indicator.IndicatorKey);
+        var groupedHealthMeasures = healthMeasures
+            .GroupBy(hm => new {
+                hm.AgeKey,
+                hm.AreaKey,
+                hm.DeprivationKey,
+                hm.SexKey
+            });
+
+        foreach (var hmGroup in groupedHealthMeasures) {
+            var mostRecentDataPoints = hmGroup
+                .OrderByDescending(hm => hm.Year)
+                .Take(TrendCalculator.RequiredNumberOfDataPoints);
+            
+            if (
+                !mostRecentDataPoints.Any() ||
+                mostRecentDataPoints.First().TrendDimension.Name != Constants.Trend.NotYetCalculatedDbString
+            ) { continue; }
+
+            var trend = trendCalculator.CalculateTrend(indicator, mostRecentDataPoints);
+            healthMeasureRepository.UpdateTrendKey(mostRecentDataPoints.First(), (byte) trend);
+        }
+
+        await healthMeasureRepository.SaveChanges();
+        Console.WriteLine($"Processed indicator: ({indicator.IndicatorKey}) {indicator.Name}");
+    }
 
     /// <summary>
     /// Entrypoint for processing the trend data.
     /// </summary>
-    public async Task Process()
+    public async Task Process(ServiceProvider serviceProvider)
     {
         var indicators = await _indicatorRepo.GetAll();
 
-        foreach (var indicator in indicators) {
-            var healthMeasures = await _healthMeasureRepo.GetByIndicator(indicator.IndicatorKey);
-            var groupedHealthMeasures = healthMeasures
-                .GroupBy(hm => new {
-                    hm.AgeKey,
-                    hm.AreaKey,
-                    hm.DeprivationKey,
-                    hm.SexKey
-                });
+        Parallel.ForEach(indicators, indicator =>
+        {
+            using var scope = serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<HealthMeasureDbContext>();
+            var healthMeasureRepository = new HealthMeasureRepository(dbContext);
 
-            foreach (var hmGroup in groupedHealthMeasures) {
-                var mostRecentDataPoints = hmGroup
-                    .OrderByDescending(hm => hm.Year)
-                    .Take(TrendCalculator.RequiredNumberOfDataPoints);
-                
-                if (
-                    !mostRecentDataPoints.Any() ||
-                    mostRecentDataPoints.First().TrendDimension.Name != Constants.Trend.NotYetCalculatedDbString
-                ) { continue; }
+            var legacyMapper = serviceProvider.GetRequiredService<LegacyMapper>();
+            var scopedLegacyCalculator = scope.ServiceProvider.GetRequiredService<TrendMarkerCalculator>();
+            var trendCalculator = new TrendCalculator(scopedLegacyCalculator, legacyMapper);
 
-                var trend = _trendCalculator.CalculateTrend(indicator, mostRecentDataPoints);
-                _healthMeasureRepo.UpdateTrendKey(mostRecentDataPoints.First(), (byte) trend);
-            }
-
-            await _healthMeasureRepo.SaveChanges();
-            Console.WriteLine($"Processed indicator: ({indicator.IndicatorKey}) {indicator.Name}");
-        }
+            ProcessOne(indicator, healthMeasureRepository, trendCalculator).Wait();
+        });
     }
 }
