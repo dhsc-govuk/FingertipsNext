5dc2380241f249adfb122b12ca29870f41024e5a
stuc-bjss <stuart.craigen@bjss.com>
2025-03-18 18:00:21 +0000

    DHSCFT-223: Updated swagger and the generated client

--- frontend/fingertips-frontend/app/chart/page.tsx
+++ frontend/fingertips-frontend/app/chart/page.tsx
@@ -74,7 +74,8 @@ export default async function ChartPage(
             )
               ? [GetHealthDataForAnIndicatorInequalitiesEnum.Sex]
               : [],
-            comparisonMethod: GetHealthDataForAnIndicatorComparisonMethodEnum.Rag,
+            comparisonMethod:
+              GetHealthDataForAnIndicatorComparisonMethodEnum.Rag,
           },
           API_CACHE_CONFIG
         )
