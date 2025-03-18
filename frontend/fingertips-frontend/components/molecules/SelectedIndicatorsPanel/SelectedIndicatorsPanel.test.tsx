8b95353a36487e6b54259d3737eb78ad66fb73c8
Fawad Ali <alibombaye@users.noreply.github.com>
2025-03-18 14:40:41 +0000

    show add or change indicators button

--- frontend/fingertips-frontend/components/molecules/SelectedIndicatorsPanel/SelectedIndicatorsPanel.test.tsx
+++ frontend/fingertips-frontend/components/molecules/SelectedIndicatorsPanel/SelectedIndicatorsPanel.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { SelectedIndicatorsPanel } from '.';
 import { generateIndicatorDocument } from '@/lib/search/mockDataHelper';
+import userEvent from '@testing-library/user-event';
+import { SearchParams } from '@/lib/searchStateManager';
 
 const mockPath = 'some-mock-path';
 const mockReplace = jest.fn();
@@ -52,5 +54,41 @@ describe('SelectedIndicatorsPanel', () => {
     );
 
     expect(screen.getAllByTestId('pill-container')).toHaveLength(2);
+  });
+
+  it('should render a button to Add or change indicators', () => {
+    render(
+      <SelectedIndicatorsPanel
+        selectedIndicatorsData={mockSelectedIndicatorsData}
+      />
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent(
+      'Add or change indicators'
+    );
+  });
+
+  it('should return to the results page with the provided search state when the Add or change button is clicked', async () => {
+    const expectedPath = [
+      `/results`,
+      `?${SearchParams.IndicatorsSelected}=1&${SearchParams.IndicatorsSelected}=2`,
+      `&${SearchParams.AreasSelected}=E40000012`,
+    ].join('');
+
+    const user = userEvent.setup();
+
+    render(
+      <SelectedIndicatorsPanel
+        selectedIndicatorsData={mockSelectedIndicatorsData}
+        searchState={{
+          [SearchParams.IndicatorsSelected]: ['1', '2'],
+          [SearchParams.AreasSelected]: ['E40000012'],
+        }}
+      />
+    );
+
+    await user.click(screen.getByRole('button'));
+
+    expect(mockReplace).toHaveBeenCalledWith(expectedPath);
   });
 });
