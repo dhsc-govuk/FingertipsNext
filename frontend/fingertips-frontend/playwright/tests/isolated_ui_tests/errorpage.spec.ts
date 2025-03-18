4ca868cc8ef26d3374cd2a5579efb0484c89ca89
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-03-18 13:56:14 +0000

    a11y added

--- frontend/fingertips-frontend/playwright/tests/isolated_ui_tests/errorpage.spec.ts
+++ frontend/fingertips-frontend/playwright/tests/isolated_ui_tests/errorpage.spec.ts
@@ -8,22 +8,27 @@ test.use({ failOnUnhandledError: false });
 test.describe('Error page tests', () => {
   test('Home page displays ErrorPage when API returns unexpected error', async ({
     homePage,
+    axeBuilder,
   }) => {
     await homePage.navigateToHomePage(`?as=${ErrorAreaCode}`);
 
     await test
       .expect(homePage.errorPageTitle())
       .toContainText('Sorry, there is a problem with the service');
+    await homePage.expectNoAccessibilityViolations(axeBuilder);
   });
 
   test('Chart page displays ErrorPage when API returns unexpected error', async ({
     chartPage,
+    // axeBuilder,
   }) => {
-    await chartPage.page.goto(`/chart?is=${ErrorIndicatorId}`);
+    await chartPage.page.goto(`/chart?is=`);
 
     await test
       .expect(chartPage.errorPageTitle())
       .toContainText('Sorry, there is a problem with the service');
+    // readd once DHSCFT-495 is actioned as the next error overlay is current causing the test to fail
+    // await chartPage.expectNoAccessibilityViolations(axeBuilder);
   });
 
   test('Indicator page displays ErrorPage when API returns unexpected error', async ({
@@ -38,6 +43,7 @@ test.describe('Error page tests', () => {
 
   test('Results page displays ErrorPage when API returns unexpected error', async ({
     resultsPage,
+    axeBuilder,
   }) => {
     await resultsPage.navigateToResults(`${ErrorIndicatorId}`, [
       `${ErrorAreaCode}`,
@@ -46,16 +52,19 @@ test.describe('Error page tests', () => {
     await test
       .expect(resultsPage.errorPageTitle())
       .toContainText('Sorry, there is a problem with the service');
+    await resultsPage.expectNoAccessibilityViolations(axeBuilder);
   });
 
   test('Navigating to an unsupported url should show the not found page', async ({
     homePage,
+    axeBuilder,
   }) => {
     await homePage.page.goto('cabbage');
 
     await test
       .expect(homePage.errorPageTitle())
       .toContainText('Page not found');
+    await homePage.expectNoAccessibilityViolations(axeBuilder);
   });
 });
 
