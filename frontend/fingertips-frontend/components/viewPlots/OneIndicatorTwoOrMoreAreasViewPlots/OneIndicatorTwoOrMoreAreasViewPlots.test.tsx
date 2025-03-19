35e69d9d0753892a90edf93ae636484e4dc447db
Priya Thorpe <priya.thorpe@bjss.com>
2025-03-19 10:34:46 +0000

    fix type error

--- frontend/fingertips-frontend/components/viewPlots/OneIndicatorTwoOrMoreAreasViewPlots/OneIndicatorTwoOrMoreAreasViewPlots.test.tsx
+++ frontend/fingertips-frontend/components/viewPlots/OneIndicatorTwoOrMoreAreasViewPlots/OneIndicatorTwoOrMoreAreasViewPlots.test.tsx
@@ -195,6 +195,7 @@ describe('OneIndicatorTwoOrMoreAreasViewPlots', () => {
       <OneIndicatorTwoOrMoreAreasViewPlots
         healthIndicatorData={testHealthData}
         searchState={searchState}
+        areaCodes={mockAreas}
       />
     );
 
