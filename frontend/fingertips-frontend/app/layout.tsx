6f8f0c4577a781bb51d9ca8fdcbe59993e86e4d1
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-03-19 13:13:35 +0000

    Data from master

--- frontend/fingertips-frontend/app/layout.tsx
+++ frontend/fingertips-frontend/app/layout.tsx
@@ -1,8 +1,10 @@
 import { FTContainer } from '@/components/layouts/container';
 import { FTFooter } from '@/components/molecules/Footer';
 import { FTHeader } from '@/components/molecules/Header';
+import { PyramidPopulationChartView } from '@/components/viewPlots/PyramidPopulationChartView';
 import StyledComponentsRegistry from '@/lib/registry';
 import type { Metadata } from 'next';
+import { mockHealthData } from '@/mock/data/healthdata';
 
 export const metadata: Metadata = {
   title: 'FingertipsNext',
@@ -20,7 +22,11 @@ export default function RootLayout({
       <body>
         <StyledComponentsRegistry>
           <FTHeader />
-          <FTContainer>{children}</FTContainer>
+          {/* <FTContainer>{children}</FTContainer> */}
+          <PyramidPopulationChartView
+            healthDataForAreas={mockHealthData['337']}
+            selectedGroupAreaCode={mockHealthData['337'][2].areaCode}
+          />
           <FTFooter />
         </StyledComponentsRegistry>
       </body>
