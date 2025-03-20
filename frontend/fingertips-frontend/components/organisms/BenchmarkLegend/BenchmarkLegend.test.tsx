93b3b85f4b8f29c1344a3f321f9e0e2d54c55367
Stuart <stuart.craigen@bjss.com>
2025-03-20 13:58:44 +0000

    DHSCFT-388: added test for benchmark legend

--- frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
+++ frontend/fingertips-frontend/components/organisms/BenchmarkLegend/BenchmarkLegend.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BenchmarkLegend } from '.';
 import '@testing-library/jest-dom';
 
@@ -6,5 +6,19 @@ describe('Testing the benchmark component', () => {
   it('Snapshot testing of the UI', () => {
     const container = render(<BenchmarkLegend rag quintiles />);
     expect(container.asFragment()).toMatchSnapshot();
+  });
+
+  it('Should only render RAG points', () => {
+    render(<BenchmarkLegend rag />);
+    expect(screen.getAllByText('Better')).toHaveLength(2);
+    expect(screen.queryByText('Worst')).not.toBeInTheDocument();
+    expect(screen.queryByText('Quintiles')).not.toBeInTheDocument();
+  });
+
+  it('Should only render quintile points', () => {
+    render(<BenchmarkLegend quintiles />);
+    expect(screen.getAllByText('Worst')).toHaveLength(1);
+    expect(screen.getAllByText('Lowest')).toHaveLength(1);
+    expect(screen.queryByText('Not compared')).not.toBeInTheDocument();
   });
 });
