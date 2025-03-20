911c4770f33dbc235052aa71d60cae7c54adbac1
Zoe Nolan <zoe.nolan@bjss.com>
2025-03-20 12:56:14 +0000

    DHSCFT-421: Added hardcoded version of the spine chart table

--- frontend/fingertips-frontend/components/viewPlots/TwoOrMoreIndicatorsAreasViewPlots/index.tsx
+++ frontend/fingertips-frontend/components/viewPlots/TwoOrMoreIndicatorsAreasViewPlots/index.tsx
@@ -2,13 +2,120 @@
 
 import { SearchParams, SearchStateManager } from '@/lib/searchStateManager';
 import { MultiIndicatorViewPlotProps } from '@/components/viewPlots/ViewPlotProps';
-import { seriesDataWithoutEnglandOrGroup } from '@/lib/chartHelpers/chartHelpers';
-import { areaCodeForEngland } from '@/lib/chartHelpers/constants';
+import {
+  SpineChartTableProps,
+  SpineChartTable,
+} from '@/components/organisms/SpineChartTable';
+import { HealthDataPointTrendEnum } from '@/generated-sources/ft-api-client';
+import { MOCK_HEALTH_DATA } from '@/lib/tableHelpers/mocks';
 import { H2 } from 'govuk-react';
 
+export const mapToSpineChartTableProps = (): SpineChartTableProps => {
+  const mockIndicatorData = [
+    {
+      indicatorId: 2,
+      title: 'Test indicator 1',
+      definition: '',
+    },
+    {
+      indicatorId: 1,
+      title: 'Test indicator 2',
+      definition: '',
+    },
+  ];
+
+  const mockUnits = ['kg', 'per 1000'];
+
+  const mockHealthData = [
+    {
+      areaCode: 'A1425',
+      areaName: 'Greater Manchester ICB - 00T',
+      healthData: [
+        {
+          year: 2008,
+          count: 222,
+          value: 890.305692,
+          lowerCi: 441.69151,
+          upperCi: 578.32766,
+          ageBand: 'All',
+          sex: 'All',
+          trend: HealthDataPointTrendEnum.NotYetCalculated,
+        },
+      ],
+    },
+    {
+      areaCode: 'A1425',
+      areaName: 'Greater Manchester ICB - 00T',
+      healthData: [
+        {
+          year: 2024,
+          count: 111,
+          value: 690.305692,
+          lowerCi: 341.69151,
+          upperCi: 478.32766,
+          ageBand: 'All',
+          sex: 'All',
+          trend: HealthDataPointTrendEnum.NotYetCalculated,
+        },
+      ],
+    },
+  ];
+
+  const mockGroup = [
+    {
+      areaCode: '90210',
+      areaName: 'Manchester',
+      healthData: [
+        {
+          year: 2008,
+          count: 111,
+          value: 980.305692,
+          lowerCi: 441.69151,
+          upperCi: 578.32766,
+          ageBand: 'All',
+          sex: 'All',
+          trend: HealthDataPointTrendEnum.NotYetCalculated,
+        },
+      ],
+    },
+    {
+      areaCode: '90210',
+      areaName: 'Manchester',
+      healthData: [
+        {
+          year: 2024,
+          count: 3333,
+          value: 690.305692,
+          lowerCi: 341.69151,
+          upperCi: 478.32766,
+          ageBand: 'All',
+          sex: 'All',
+          trend: HealthDataPointTrendEnum.NotYetCalculated,
+        },
+      ],
+    },
+  ];
+
+  const mockBest = [1666, 22];
+
+  const mockWorst = [959, 100];
+
+  const data: SpineChartTableProps = {
+    indicators: mockIndicatorData,
+    measurementUnits: mockUnits,
+    indicatorHealthData: mockHealthData,
+    groupIndicatorData: mockGroup,
+    englandBenchmarkData: MOCK_HEALTH_DATA,
+    worst: mockWorst,
+    best: mockBest,
+  };
+
+  return data;
+};
+
 export function TwoOrMoreIndicatorsAreasViewPlot({
-  healthIndicatorData,
   searchState,
+  healthIndicatorData,
   indicatorMetadata,
 }: Readonly<MultiIndicatorViewPlotProps>) {
   const stateManager = SearchStateManager.initialise(searchState);
@@ -17,20 +124,20 @@ export function TwoOrMoreIndicatorsAreasViewPlot({
     [SearchParams.GroupSelected]: selectedGroupCode,
   } = stateManager.getSearchState();
 
-  const dataWithoutEngland = seriesDataWithoutEnglandOrGroup(
-    healthIndicatorData[0],
-    selectedGroupCode
-  );
-  const englandBenchmarkData = healthIndicatorData[0].find(
-    (areaData) => areaData.areaCode === areaCodeForEngland
-  );
+  const spineTableData = mapToSpineChartTableProps();
 
-  const groupData =
-    selectedGroupCode && selectedGroupCode != areaCodeForEngland
-      ? healthIndicatorData[0].find(
-          (areaData) => areaData.areaCode === selectedGroupCode
-        )
-      : undefined;
-
-  return <H2>View data for selected indicators and areas</H2>;
+  return (
+    <section data-testid="twoOrMoreIndicatorsAreasViewPlot-component">
+      <H2>View data for selected indicators and areas</H2>
+      <SpineChartTable
+        indicators={spineTableData.indicators}
+        measurementUnits={spineTableData.measurementUnits}
+        indicatorHealthData={spineTableData.indicatorHealthData}
+        groupIndicatorData={spineTableData.groupIndicatorData}
+        englandBenchmarkData={spineTableData.englandBenchmarkData}
+        best={spineTableData.best}
+        worst={spineTableData.worst}
+      />
+    </section>
+  );
 }
