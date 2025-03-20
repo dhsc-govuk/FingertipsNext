c4625e7cc407c236e6713a605f7b0358cb22d477
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-03-20 13:10:56 +0000

    update chart tests

--- frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
+++ frontend/fingertips-frontend/components/pages/chart/chart.test.tsx
@@ -1,9 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Chart } from '@/components/pages/chart/index';
 import { expect } from '@jest/globals';
-import { mockHealthData } from '@/mock/data/healthdata';
 import { PopulationDataForArea } from '@/lib/chartHelpers/preparePopulationData';
-import { SearchParams, SearchStateParams } from '@/lib/searchStateManager';
 
 const mockPopulationData: PopulationDataForArea = {
   ageCategories: [],
@@ -11,58 +9,19 @@ const mockPopulationData: PopulationDataForArea = {
   maleSeries: [],
 };
 
-const state: SearchStateParams = {
-  [SearchParams.SearchedIndicator]: 'test',
-  [SearchParams.IndicatorsSelected]: ['1', '2'],
-};
-
-jest.mock('@/components/organisms/ThematicMap/', () => {
-  return {
-    ThematicMap: function ThematicMap() {
-      return <div data-testid="thematicMap-component"></div>;
-    },
-  };
-});
-
-jest.mock('next/navigation', () => {
-  const originalModule = jest.requireActual('next/navigation');
-
-  return {
-    ...originalModule,
-    usePathname: jest.fn(),
-    useRouter: jest.fn().mockImplementation(() => ({
-      replace: jest.fn(),
-    })),
-  };
-});
-
 describe('Page structure', () => {
   it('should render the PopulationPyramid component when Population data are provided', () => {
     render(
       <Chart
-        healthIndicatorData={[mockHealthData[1]]}
         populationData={{
           dataForSelectedArea: mockPopulationData,
           dataForEngland: undefined,
           dataForBaseline: undefined,
         }}
-        searchState={state}
       />
     );
 
     const populationPyramid = screen.getByTestId('populationPyramid-component');
     expect(populationPyramid).toBeInTheDocument();
-  });
-
-  it('should _not_ render the ThematicMap component when map props are _not_ provided', () => {
-    render(
-      <Chart
-        healthIndicatorData={[mockHealthData['92420']]}
-        searchState={state}
-      />
-    );
-    const thematicMap = screen.queryByTestId('thematicMap-component');
-
-    expect(thematicMap).not.toBeInTheDocument();
   });
 });
