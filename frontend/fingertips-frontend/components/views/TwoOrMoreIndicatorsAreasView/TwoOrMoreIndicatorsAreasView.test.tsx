fbbd7e72dfe73baa11d45b2e8fb4833c0f41b951
Zoe Nolan <zoe.nolan@bjss.com>
2025-03-26 09:05:20 +0000

    DHSCFT-424: Update test for TwoOrMoreIndicatorsAreasView

--- frontend/fingertips-frontend/components/views/TwoOrMoreIndicatorsAreasView/TwoOrMoreIndicatorsAreasView.test.tsx
+++ frontend/fingertips-frontend/components/views/TwoOrMoreIndicatorsAreasView/TwoOrMoreIndicatorsAreasView.test.tsx
@@ -6,9 +6,13 @@ import { IndicatorsApi } from '@/generated-sources/ft-api-client';
 import { SearchParams, SearchStateParams } from '@/lib/searchStateManager';
 import { mockDeep } from 'jest-mock-extended';
 import TwoOrMoreIndicatorsAreasView from '.';
-import { ApiClientFactory } from '@/lib/apiClient/apiClientFactory';
+import { areaCodeForEngland } from '@/lib/chartHelpers/constants';
+import {
+  ApiClientFactory,
+} from '@/lib/apiClient/apiClientFactory';
 import { SearchServiceFactory } from '@/lib/search/searchServiceFactory';
 import { IIndicatorSearchService } from '@/lib/search/searchTypes';
+import { HealthDataForArea } from '@/generated-sources/ft-api-client';
 
 const mockIndicatorsApi = mockDeep<IndicatorsApi>();
 ApiClientFactory.getIndicatorsApiClient = () => mockIndicatorsApi;
@@ -17,117 +21,30 @@ const mockIndicatorSearchService = mockDeep<IIndicatorSearchService>();
 SearchServiceFactory.getIndicatorSearchService = () =>
   mockIndicatorSearchService;
 
+const mockAreaCode = 'A001';
+const mockGroupCode = 'G001';
+
+const mockAreaData: HealthDataForArea = {
+  areaCode: mockAreaCode,
+  areaName: 'area',
+  healthData: [],
+};
+
+const mockGroupData: HealthDataForArea = {
+  areaCode: mockGroupCode,
+  areaName: 'group',
+  healthData: [],
+};
+
+const mockEnglandData: HealthDataForArea = {
+  areaCode: areaCodeForEngland,
+  areaName: 'england',
+  healthData: [],
+};
+
 describe('TwoOrMoreIndicatorsAreasView', () => {
   afterEach(() => {
     jest.resetAllMocks();
-  });
-  it.each([
-    [
-      ['1', '2'],
-      ['A001', 'A002', 'A003'],
-    ],
-    [['1'], ['A001']],
-  ])(
-    'should return an error if given invalid parameters',
-    async (testIndicators, testAreas) => {
-      const searchState: SearchStateParams = {
-        [SearchParams.IndicatorsSelected]: testIndicators,
-        [SearchParams.AreasSelected]: testAreas,
-      };
-
-      await expect(async () => {
-        await TwoOrMoreIndicatorsAreasView({ searchState: searchState });
-      }).rejects.toThrow('Invalid parameters provided to view');
-    }
-  );
-
-  it.each([
-    [['1', '2'], ['A001'], 'G001', ['A001', areaCodeForEngland, 'G001']],
-    [['1', '2'], ['A001'], areaCodeForEngland, ['A001', areaCodeForEngland]],
-  ])(
-    'should make 2 calls to the healthIndicatorApi with the expected parameters',
-    async (testIndicators, testAreas, testGroup, expectedAreaCodes) => {
-      const searchState: SearchStateParams = {
-        [SearchParams.IndicatorsSelected]: testIndicators,
-        [SearchParams.AreasSelected]: testAreas,
-        [SearchParams.GroupSelected]: testGroup,
-      };
-      mockIndicatorsApi.getHealthDataForAnIndicator.mockResolvedValueOnce([]);
-
-      await TwoOrMoreIndicatorsAreasView({ searchState: searchState });
-
-      expect(
-        mockIndicatorsApi.getHealthDataForAnIndicator
-      ).toHaveBeenNthCalledWith(
-        1,
-        {
-          areaCodes: expectedAreaCodes,
-          indicatorId: 1,
-        },
-        2,
-        {
-          areaCodes: expectedAreaCodes,
-          indicatorId: 2,
-        },
-        API_CACHE_CONFIG
-      );
-    }
-  );
-
-  it.only('should call get indicator endpoint and pass indicator metadata', async () => {
-    const indicatorIds = ['2', '1'];
-
-    const searchParams: SearchStateParams = {
-      [SearchParams.SearchedIndicator]: 'testing',
-      [SearchParams.IndicatorsSelected]: indicatorIds,
-      [SearchParams.GroupSelected]: 'E12000002',
-      [SearchParams.AreasSelected]: ['E08000001'],
-    };
-
-    const mockResponses = [
-      {
-        indicatorID: indicatorIds[0],
-        indicatorName: 'pancakes eaten',
-        indicatorDefinition: 'number of pancakes consumed',
-        dataSource: 'BJSS Leeds',
-        earliestDataPeriod: '2025',
-        latestDataPeriod: '2025',
-        lastUpdatedDate: new Date('March 4, 2025'),
-        associatedAreaCodes: ['E06000047'],
-        unitLabel: 'pancakes',
-        hasInequalities: true,
-        usedInPoc: false,
-      },
-      {
-        indicatorID: indicatorIds[1],
-        indicatorName: 'pizzas eaten',
-        indicatorDefinition: 'number of pizzas consumed',
-        dataSource: 'BJSS Leeds',
-        earliestDataPeriod: '2023',
-        latestDataPeriod: '2023',
-        lastUpdatedDate: new Date('March 4, 2023'),
-        associatedAreaCodes: ['E06000047'],
-        unitLabel: 'pizzas',
-        hasInequalities: true,
-        usedInPoc: false,
-      },
-    ];
-
-    mockIndicatorSearchService.getIndicator
-      .mockResolvedValueOnce(mockResponses[0])
-      .mockResolvedValueOnce(mockResponses[1]);
-
-    const _ = await TwoOrMoreIndicatorsAreasView({
-      searchState: searchParams,
-    });
-
-    indicatorIds.forEach((indicator) => {
-      expect(mockIndicatorSearchService.getIndicator).toHaveBeenCalledWith(
-        indicator
-      );
-    });
-
-    expect(page.props.indicatorMetadata).toStrictEqual(mockResponses);
   });
 
   it('should call TwoOrMoreIndicatorsAreasViewPlots with the correct props', async () => {
@@ -137,18 +54,9 @@ describe('TwoOrMoreIndicatorsAreasView', () => {
       [SearchParams.GroupSelected]: 'G001',
     };
 
-    const mockResponses = [
-      mockHealthData['108'],
-      mockHealthData['107'],
-      mockHealthData['106'],
-      mockHealthData['105'],
-    ];
-
     mockIndicatorsApi.getHealthDataForAnIndicator
-      .mockResolvedValueOnce(mockResponses[0])
-      .mockResolvedValueOnce(mockResponses[1])
-      .mockResolvedValueOnce(mockResponses[2])
-      .mockResolvedValueOnce(mockResponses[3]);
+      .mockResolvedValueOnce([mockAreaData, mockGroupData, mockEnglandData])
+      .mockResolvedValueOnce([mockAreaData, mockGroupData, mockEnglandData]);
 
     const page = await TwoOrMoreIndicatorsAreasView({
       searchState: searchState,
