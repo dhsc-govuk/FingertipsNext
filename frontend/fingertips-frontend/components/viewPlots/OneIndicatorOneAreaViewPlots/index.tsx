fe26e1141db6b60bf669d33d1d9dcb93aec09dde
Zoe Nolan <zoe.nolan@bjss.com>
2025-03-26 17:40:10 +0000

    DHSCFT-421: Repir rebase errors

--- frontend/fingertips-frontend/components/viewPlots/OneIndicatorOneAreaViewPlots/index.tsx
+++ frontend/fingertips-frontend/components/viewPlots/OneIndicatorOneAreaViewPlots/index.tsx
@@ -12,7 +12,7 @@ import { SearchParams, SearchStateManager } from '@/lib/searchStateManager';
 import { H2, H3, Paragraph } from 'govuk-react';
 import styled from 'styled-components';
 import { typography } from '@govuk-react/lib';
-import { ViewPlotProps } from '../ViewPlotProps';
+import { OneIndicatorViewPlotProps} from '../ViewPlotProps';
 import {
   BenchmarkComparisonMethod,
   HealthDataForArea,
--- frontend/fingertips-frontend/components/viewPlots/ViewPlotProps.ts
+++ frontend/fingertips-frontend/components/viewPlots/ViewPlotProps.ts
@@ -1,4 +1,4 @@
-import { HealthDataForArea, IndicatorWithHealthDataForArea } from '@/generated-sources/ft-api-client';
+import { IndicatorWithHealthDataForArea } from '@/generated-sources/ft-api-client';
 import { IndicatorDocument } from '@/lib/search/searchTypes';
 import { SearchStateParams } from '@/lib/searchStateManager';
 
@@ -7,4 +7,10 @@ export type OneIndicatorViewPlotProps = {
   searchState: SearchStateParams;
   indicatorMetadata?: IndicatorDocument;
   populationHealthDataForArea?: HealthDataForArea[];
+};
+
+export type TwoOrMoreIndicatorsAreasViewPlot = {
+  indicatorData: IndicatorWithHealthDataForArea[];
+  searchState: SearchStateParams;
+  indicatorMetadata?: IndicatorDocument[];
 };
