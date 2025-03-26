f8fdd86d82c577a2c4e29bdf57f021a2d5252742
Zoe Nolan <zoe.nolan@bjss.com>
2025-03-26 13:23:37 +0000

    DHSCFT-421: Run prettier

--- frontend/fingertips-frontend/components/views/TwoOrMoreIndicatorsAreasView/TwoOrMoreIndicatorsAreasView.test.tsx
+++ frontend/fingertips-frontend/components/views/TwoOrMoreIndicatorsAreasView/TwoOrMoreIndicatorsAreasView.test.tsx
@@ -6,9 +6,7 @@ import { IndicatorsApi } from '@/generated-sources/ft-api-client';
 import { SearchParams, SearchStateParams } from '@/lib/searchStateManager';
 import { mockDeep } from 'jest-mock-extended';
 import TwoOrMoreIndicatorsAreasView from '.';
-import {
-  ApiClientFactory,
-} from '@/lib/apiClient/apiClientFactory';
+import { ApiClientFactory } from '@/lib/apiClient/apiClientFactory';
 import { SearchServiceFactory } from '@/lib/search/searchServiceFactory';
 import { IIndicatorSearchService } from '@/lib/search/searchTypes';
 
