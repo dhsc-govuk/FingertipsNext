af1e3182fe5654bd991939c0e5d5df490a0c8d13
david.wolfson@bjss.com <david.wolfson@bjss.com>
2025-03-27 11:47:25 +0000

    handle useEffect in child to address test warning

--- frontend/fingertips-frontend/components/viewPlots/OneIndicatorTwoOrMoreAreasViewPlots/OneIndicatorTwoOrMoreAreasViewPlots.test.tsx
+++ frontend/fingertips-frontend/components/viewPlots/OneIndicatorTwoOrMoreAreasViewPlots/OneIndicatorTwoOrMoreAreasViewPlots.test.tsx
@@ -173,7 +173,7 @@ describe('OneIndicatorTwoOrMoreAreasViewPlots', () => {
         />
       );
 
-      await assertLineChartAndTableNotInDocument();
+      await waitFor(() => assertLineChartAndTableNotInDocument());
     });
   });
 
