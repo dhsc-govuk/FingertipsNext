4d81a3d90af587ce20c3371150ffbcd0e751c4eb
Priya Thorpe <priya.thorpe@bjss.com>
2025-03-27 11:18:11 +0000

    WIP

--- frontend/fingertips-frontend/components/views/TwoOrMoreIndicatorsEnglandView/index.tsx
+++ frontend/fingertips-frontend/components/views/TwoOrMoreIndicatorsEnglandView/index.tsx
@@ -3,7 +3,8 @@ import { areaCodeForEngland } from '@/lib/chartHelpers/constants';
 import { SearchParams, SearchStateManager } from '@/lib/searchStateManager';
 import { connection } from 'next/server';
 import { ViewProps } from '../ViewsContext';
-import { ApiClientFactory } from '@/lib/apiClient/apiClientFactory';
+import { API_CACHE_CONFIG, ApiClientFactory } from '@/lib/apiClient/apiClientFactory';
+import { HealthDataForArea } from '@/generated-sources/ft-api-client';
 
 export default async function TwoOrMoreIndicatorsEnglandView({
   searchState,
@@ -21,10 +22,59 @@ export default async function TwoOrMoreIndicatorsEnglandView({
     throw new Error('Invalid parameters provided to view');
   }
 
-  const areaCodesToRequest = areaCodeForEngland;
+  const areaCodesToRequest = [areaCodeForEngland];
 
   await connection();
   const indicatorApi = ApiClientFactory.getIndicatorsApiClient();
+
+  const combinedIndicatorData: HealthDataForArea[][] = [];
+  try {
+    const promises = indicatorsSelected.map((indicator) => {
+      return indicatorApi.getHealthDataForAnIndicator(
+        {
+          indicatorId: Number(indicator),
+          areaCodes: areaCodesToRequest,
+        },
+        API_CACHE_CONFIG
+      );
+    });
+
+    const results = await Promise.all(promises);
+
+    results.forEach((row) => {
+      if (row.areaHealthData) {
+        combinedIndicatorData.push(row.areaHealthData);
+      }
+    });
+  } catch (error) {
+    console.error('error getting health indicator data for areas', error);
+    throw new Error('error getting health indicator data for areas');
+  }
+
+  // const { healthIndicatorData, groupIndicatorData, englandIndicatorData } =
+  //   extractingCombinedHealthData(
+  //     combinedIndicatorData,
+  //     areasSelected,
+  //     selectedGroupCode
+  //   );
+  //
+  // let indicatorMetadata: (IndicatorDocument | undefined)[];
+  // try {
+  //   const promises = indicatorsSelected.map((indicator) => {
+  //     return SearchServiceFactory.getIndicatorSearchService().getIndicator(
+  //       indicator
+  //     );
+  //   });
+  //
+  //   const results = await Promise.all(promises);
+  //
+  //   indicatorMetadata = results;
+  // } catch (error) {
+  //   console.error('error getting health indicator data for areas', error);
+  //   throw new Error('error getting health indicator data for areas');
+  // }
+  
+  
 
   console.log('TODO: fetch health data with inequalites');
   console.log(`TODO: fetch population data for areas: [${areaCodesToRequest}]`);
