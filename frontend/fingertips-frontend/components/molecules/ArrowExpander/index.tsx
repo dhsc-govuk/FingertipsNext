18b441e7f9f00cc5c35f9c0f03668cce9f0a2333
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-03-28 19:33:40 +0000

    2lint

--- frontend/fingertips-frontend/components/molecules/ArrowExpander/index.tsx
+++ frontend/fingertips-frontend/components/molecules/ArrowExpander/index.tsx
@@ -22,14 +22,14 @@ const StyleTextSpanExpander = styled('div')<{ fill?: string }>(({ fill }) => ({
 interface ExpanderProps {
   children: React.ReactNode;
   fill?: string;
-  openTitle?: string
-  closeTitle?: string
+  openTitle?: string;
+  closeTitle?: string;
 }
 export const ArrowExpander = ({
   children,
   fill = '#1D70B8',
-  openTitle = "Show",
-  closeTitle = "Hide"
+  openTitle = 'Show',
+  closeTitle = 'Hide',
 }: ExpanderProps) => {
   const [isOpen, setIsOpen] = useState<boolean>();
   return (
