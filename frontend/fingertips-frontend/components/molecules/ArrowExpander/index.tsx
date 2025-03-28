e84ecebd7388fadb91be2295e29ede7bb26ff652
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-03-28 20:39:42 +0000

    Changed the way it works to make it reuaable

--- frontend/fingertips-frontend/components/molecules/ArrowExpander/index.tsx
+++ frontend/fingertips-frontend/components/molecules/ArrowExpander/index.tsx
@@ -2,22 +2,38 @@
 import React, { useState } from 'react';
 import { ArrowToggleButton } from '@/components/molecules/ArrowToggleButton';
 import styled from 'styled-components';
+import { Label } from 'govuk-react';
+import { GovukColours } from '@/lib/styleHelpers/colours';
 
-const StyleButtonExpander = styled('div')({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'left',
-  alignItems: 'center',
+const StyleButtonExpander = styled('span')({
+  'display': 'flex',
+  'flexDirection': 'row',
+  'justifyContent': 'left',
+  'width': 'fit-content',
+  'flexWrap': 'wrap',
+  'cursor': 'pointer',
+  'padding': '5px',
+  'alignItems': 'center',
+
+  ':hover svg circle': {
+    fill: GovukColours.Black,
+    stroke: GovukColours.Black,
+  },
+
+  ':hover svg path': {
+    stroke: GovukColours.White,
+  },
 });
 
-const StyleTextSpanExpander = styled('div')<{ fill?: string }>(({ fill }) => ({
-  fontWeight: '300',
-  display: 'inline-block',
-  fontSize: '19px',
-  color: fill,
-  textAlign: 'center',
-  padding: '2px 1px 1px 1px',
-}));
+const StyleLabelTextForExpander = styled(Label)<{ fill?: string }>(
+  ({ fill }) => ({
+    fontWeight: '300',
+    fontSize: '19px',
+    color: fill,
+    padding: '2px 1px 1px 1px',
+    cursor: 'pointer',
+  })
+);
 
 interface ExpanderProps {
   children: React.ReactNode;
@@ -28,25 +44,17 @@ interface ExpanderProps {
 export const ArrowExpander = ({
   children,
   fill = '#1D70B8',
-  openTitle = 'Show',
-  closeTitle = 'Hide',
+  openTitle,
+  closeTitle,
 }: ExpanderProps) => {
   const [isOpen, setIsOpen] = useState<boolean>();
   return (
     <>
-      <StyleButtonExpander>
-        <ArrowToggleButton
-          fill={fill}
-          width={30}
-          height={30}
-          onToggle={(isOpen: boolean) => {
-            setIsOpen(isOpen);
-            return isOpen;
-          }}
-        />
-        <StyleTextSpanExpander fill={fill}>
-          {isOpen ? openTitle : closeTitle}
-        </StyleTextSpanExpander>
+      <StyleButtonExpander onClick={() => setIsOpen(!isOpen)}>
+        <ArrowToggleButton fill={fill} width={30} height={30} isOpen={isOpen} />
+        <StyleLabelTextForExpander fill={fill}>
+          {isOpen ? closeTitle : openTitle}
+        </StyleLabelTextForExpander>
       </StyleButtonExpander>
       {isOpen ? children : null}
     </>
--- frontend/fingertips-frontend/components/molecules/ArrowToggleButton/index.tsx
+++ frontend/fingertips-frontend/components/molecules/ArrowToggleButton/index.tsx
@@ -1,59 +1,50 @@
-import { useCallback, useState } from 'react';
+import styled from 'styled-components';
+
+const StyleSVGElement = styled('svg')({
+  'display': 'flex',
+  'flexDirection': 'row',
+  'justifyContent': 'left',
+  'alignItems': 'center',
+  'cursor': 'pointer',
+  '& circle': {
+    outline: 'none',
+    fill: 'none',
+  },
+});
 
 interface ToggleButtonProps {
   width?: number;
   height?: number;
   fill?: string;
-  onToggle?: (isClosed: boolean) => boolean;
+  isOpen?: boolean;
 }
 
 export const ArrowToggleButton = ({
   width,
   height,
   fill,
-  onToggle,
+  isOpen,
 }: ToggleButtonProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleArrow = useCallback(() => {
-    let status = !isOpen;
-    if (onToggle) {
-      status = onToggle(status);
-    }
-    setIsOpen(status);
-  }, [onToggle, isOpen]);
-
-  width = width ?? 20;
-  height = height ?? 20;
+  width = width ?? 30;
+  height = height ?? 30;
   fill = fill ?? '#EEEEEE';
   return (
-    <svg
+    <StyleSVGElement
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 25 25"
       data-testid="arrow-toggle-button"
       width={width}
       height={height}
-      fill="currentColor"
-      style={{ cursor: 'pointer', padding: '0px' }}
-      onClick={toggleArrow}
-      className="w-6 h-6 transition-transform duration-300"
     >
-      <circle
-        cx="12"
-        cy="12"
-        r="10"
-        stroke={fill}
-        strokeWidth="0.5"
-        fill="none"
-      />
+      <circle cx="12" cy="12" r="10" stroke={fill} strokeWidth="0.5" />
       <path
-        d={isOpen ? 'M8 10l4 4 4-4' : 'M8 14l4-4 4 4'}
+        d={isOpen ? 'M8 14l4-4 4 4' : 'M8 10l4 4 4-4'}
         stroke={fill}
         strokeWidth="2"
         fill="none"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </StyleSVGElement>
   );
 };
