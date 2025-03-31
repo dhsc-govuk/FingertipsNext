4aac34f66bf6deb9cb690ad290fe220c546e2560
stuc-bjss <stuart.craigen@bjss.com>
2025-03-31 12:40:53 +0100

    DHSCFT-292: scroll table horizontally and have sticky columns

--- frontend/fingertips-frontend/components/organisms/LineChartTable/LineChartTable.test.tsx
+++ frontend/fingertips-frontend/components/organisms/LineChartTable/LineChartTable.test.tsx
@@ -254,6 +254,7 @@ describe('Line chart table suite', () => {
           }
         />
       );
+      screen.debug();
 
       expect(screen.getByRole('table')).toBeInTheDocument();
       expect(screen.getAllByRole('columnheader')[1]).toHaveTextContent(
