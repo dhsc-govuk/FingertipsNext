ff197057a5cdc2592606c0fd063b83637aabe998
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-04-01 12:51:07 +0100

    pyramidTable coming in

--- frontend/fingertips-frontend/components/organisms/PopulationPyramidTable/PyramidTable.tsx
+++ frontend/fingertips-frontend/components/organisms/PopulationPyramidTable/PyramidTable.tsx
@@ -1,7 +1,32 @@
+"use client"
+
+import { Table } from "govuk-react";
+
 interface PyramidTableProps {
-  headers: string[] | undefined;
+  headers: string[];
+  renderHeader?: (header: string) => React.ReactNode | string
 }
 
-export const PyramidTable = ({ headers }: PyramidTableProps) => {
-  return <div></div>;
+
+
+export const PyramidTable = ({ headers, renderHeader }: PyramidTableProps) => {
+  return (
+    <Table head={
+      <Table.Row>
+        {headers.map((header) => {
+          return (
+            <Table.CellHeader key={header}>
+              {
+                renderHeader ? renderHeader(header) : header
+              }
+            </Table.CellHeader>
+          )
+        })
+        }
+      </Table.Row>
+    }>
+
+    </Table>
+  )
+
 };
