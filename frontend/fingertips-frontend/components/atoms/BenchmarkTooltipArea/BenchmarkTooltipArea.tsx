b3fc9cdec7330ea36e7b8f4b11d4efe4796fa628
WolfieKnee <david.wolfson@bjss.com>
2025-04-03 16:32:32 +0100

    restructure files

---
+++ frontend/fingertips-frontend/components/atoms/BenchmarkTooltipArea/BenchmarkTooltipArea.tsx
@@ -0,0 +1,107 @@
+import {
+  HealthDataForArea,
+  BenchmarkComparisonMethod,
+  IndicatorPolarity,
+  BenchmarkOutcome,
+} from '@/generated-sources/ft-api-client';
+import {
+  getBenchmarkColour,
+  getConfidenceLimitNumber,
+  getIndicatorDataForAreasForMostRecentYearOnly,
+} from '@/lib/chartHelpers/chartHelpers';
+import { symbolEncoder } from '@/lib/chartHelpers/pointFormatterHelper';
+
+interface BenchmarkTooltipArea {
+  indicatorDataForArea: HealthDataForArea;
+  benchmarkComparisonMethod: BenchmarkComparisonMethod;
+  polarity: IndicatorPolarity;
+  measurementUnit: string | undefined;
+  benchmarkArea: string;
+}
+
+export function BenchmarkTooltipArea({
+  indicatorDataForArea,
+  benchmarkComparisonMethod,
+  polarity,
+  measurementUnit,
+  benchmarkArea,
+}: Readonly<BenchmarkTooltipArea>) {
+  const areaMarkerSymbol =
+    indicatorDataForArea.healthData[0].benchmarkComparison?.outcome ===
+    BenchmarkOutcome.NotCompared
+      ? symbolEncoder.multiplicationX
+      : symbolEncoder.circle;
+
+  const indicatorDataForAreaForMostRecentYear =
+    getIndicatorDataForAreasForMostRecentYearOnly([indicatorDataForArea]);
+
+  const benchmarkColour = getBenchmarkColour(
+    benchmarkComparisonMethod,
+    indicatorDataForAreaForMostRecentYear[0].healthData[0].benchmarkComparison
+      ?.outcome ?? BenchmarkOutcome.NotCompared,
+    polarity
+  );
+
+  const benchmarkConfidenceLimit = getConfidenceLimitNumber(
+    benchmarkComparisonMethod
+  );
+
+  return (
+    <div>
+      <div>
+        <b>{indicatorDataForArea.areaName}</b>
+        <p>{indicatorDataForArea.healthData[0].year}</p>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '0.25em' }}>
+          <div
+            style={{
+              color: benchmarkColour,
+              display: 'flex',
+              gap: '0.25em',
+              fontSize: 'large',
+            }}
+          >
+            {areaMarkerSymbol}
+          </div>
+          <div>
+            <span style={{ display: 'block' }}>
+              {indicatorDataForAreaForMostRecentYear[0].healthData[0].value}{' '}
+              {measurementUnit}
+            </span>
+            {getComparisionText(
+              benchmarkArea,
+              indicatorDataForAreaForMostRecentYear[0].healthData[0]
+                .benchmarkComparison?.outcome
+            )}
+            {benchmarkConfidenceLimit ? (
+              <span style={{ display: 'block' }}>
+                ({benchmarkConfidenceLimit}%)
+              </span>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function getComparisionText(
+  benchmarkArea: string,
+  benchmarkOutcome?: BenchmarkOutcome
+) {
+  // TODO: DHSCFT-518 to handle no data
+  if (
+    benchmarkOutcome === BenchmarkOutcome.NotCompared ||
+    benchmarkOutcome === BenchmarkOutcome.Similar
+  ) {
+    return (
+      <span style={{ display: 'block' }}>
+        {benchmarkOutcome} to {benchmarkArea}
+      </span>
+    );
+  }
+  return (
+    <span style={{ display: 'block' }}>
+      {benchmarkOutcome} than {benchmarkArea}
+    </span>
+  );
+}
--- frontend/fingertips-frontend/components/molecules/BenchmarkTooltip/BenchmarkTooltip.tsx
+++ frontend/fingertips-frontend/components/molecules/BenchmarkTooltip/BenchmarkTooltip.tsx
@@ -1,15 +1,9 @@
+import { BenchmarkTooltipArea } from '@/components/atoms/BenchmarkTooltipArea/BenchmarkTooltipArea';
 import {
   BenchmarkComparisonMethod,
-  BenchmarkOutcome,
   HealthDataForArea,
   IndicatorPolarity,
 } from '@/generated-sources/ft-api-client';
-import {
-  getBenchmarkColour,
-  getConfidenceLimitNumber,
-  getIndicatorDataForAreasForMostRecentYearOnly,
-} from '@/lib/chartHelpers/chartHelpers';
-import { symbolEncoder } from '@/lib/chartHelpers/pointFormatterHelper';
 
 interface BenchmarkTooltipProps {
   indicatorDataForArea: HealthDataForArea;
@@ -17,7 +11,8 @@ interface BenchmarkTooltipProps {
   polarity: IndicatorPolarity;
   measurementUnit: string | undefined;
   benchmarkArea: string;
-  // Indicator?: TODO: add this to support heatmap tooltip?
+  // indicatorDataForBenchmark: HealthDataForArea
+  // indicatorDataForGroup: HealthDataForArea
 }
 
 export function BenchmarkTooltip({
@@ -27,80 +22,17 @@ export function BenchmarkTooltip({
   measurementUnit,
   benchmarkArea,
 }: Readonly<BenchmarkTooltipProps>) {
-  const benchmarkConfidenceLimit = getConfidenceLimitNumber(
-    benchmarkComparisonMethod
-  );
-  const areaMarkerSymbol =
-    indicatorDataForArea.healthData[0].benchmarkComparison?.outcome ===
-    BenchmarkOutcome.NotCompared
-      ? symbolEncoder.multiplicationX
-      : symbolEncoder.circle;
-  const indicatorDataForAreaForMostRecentYear =
-    getIndicatorDataForAreasForMostRecentYearOnly([indicatorDataForArea]);
-
-  const benchmarkColour = getBenchmarkColour(
-    benchmarkComparisonMethod,
-    indicatorDataForAreaForMostRecentYear[0].healthData[0].benchmarkComparison
-      ?.outcome ?? BenchmarkOutcome.NotCompared,
-    polarity
-  );
-
   return (
-    <div>
-      <div>
-        <b>{indicatorDataForArea.areaName}</b>
-        <p>{indicatorDataForArea.healthData[0].year}</p>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '0.25em' }}>
-          <div
-            style={{
-              color: benchmarkColour,
-              display: 'flex',
-              gap: '0.25em',
-              fontSize: 'large',
-            }}
-          >
-            {areaMarkerSymbol}
-          </div>
-          <div>
-            <span style={{ display: 'block' }}>
-              {indicatorDataForAreaForMostRecentYear[0].healthData[0].value}{' '}
-              {measurementUnit}
-            </span>
-            {getComparisionText(
-              benchmarkArea,
-              indicatorDataForAreaForMostRecentYear[0].healthData[0]
-                .benchmarkComparison?.outcome
-            )}
-            {benchmarkConfidenceLimit ? (
-              <span style={{ display: 'block' }}>
-                ({benchmarkConfidenceLimit}%)
-              </span>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function getComparisionText(
-  benchmarkArea: string,
-  benchmarkOutcome?: BenchmarkOutcome
-) {
-  // TODO: DHSCFT-518 to handle no data
-  if (
-    benchmarkOutcome === BenchmarkOutcome.NotCompared ||
-    benchmarkOutcome === BenchmarkOutcome.Similar
-  ) {
-    return (
-      <span style={{ display: 'block' }}>
-        {benchmarkOutcome} to {benchmarkArea}
-      </span>
-    );
-  }
-  return (
-    <span style={{ display: 'block' }}>
-      {benchmarkOutcome} than {benchmarkArea}
-    </span>
+    <>
+      {/* Add Benchmark if required */}
+      {/* Add Group if required */}
+      <BenchmarkTooltipArea
+        indicatorDataForArea={indicatorDataForArea}
+        benchmarkComparisonMethod={benchmarkComparisonMethod}
+        polarity={polarity}
+        measurementUnit={measurementUnit}
+        benchmarkArea={benchmarkArea}
+      />
+    </>
   );
 }
