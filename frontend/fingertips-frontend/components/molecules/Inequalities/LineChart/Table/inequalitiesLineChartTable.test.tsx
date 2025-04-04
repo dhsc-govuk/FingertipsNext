e40f36891f8f1d5f00c7c692db64fb8848415c11
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-04-04 18:15:57 +0100

    Make inequalities table header span all columns

--- frontend/fingertips-frontend/components/molecules/Inequalities/LineChart/Table/inequalitiesLineChartTable.test.tsx
+++ frontend/fingertips-frontend/components/molecules/Inequalities/LineChart/Table/inequalitiesLineChartTable.test.tsx
@@ -5,9 +5,73 @@ import { MOCK_HEALTH_DATA } from '@/lib/tableHelpers/mocks';
 import { InequalitiesChartData } from '@/components/organisms/Inequalities/inequalitiesHelpers';
 
 describe('Inequalities table suite', () => {
-  describe('Sex inequality', () => {
-    const CELLS_PER_ROW = 4;
+  const CELLS_PER_ROW = 4;
 
+  const tableData: InequalitiesChartData = {
+    areaName: MOCK_HEALTH_DATA[1].areaName,
+    rowData: [
+      {
+        period: 2004,
+        inequalities: {
+          Persons: { value: 890.3432 },
+          Male: { value: 703.420759 },
+          Female: { value: 703.420759 },
+        },
+      },
+      {
+        period: 2008,
+        inequalities: {
+          Persons: { value: 135.149304 },
+          Male: { value: 890.328253 },
+          Female: { value: 890.328253 },
+        },
+      },
+    ],
+  };
+
+  it('should render the InequalitiesTable component', () => {
+    render(
+      <InequalitiesLineChartTable
+        tableData={tableData}
+        dynamicKeys={['Persons', 'Male', 'Female']}
+      />
+    );
+
+    expect(
+      screen.getByTestId('inequalitiesLineChartTable-component')
+    ).toBeInTheDocument();
+  });
+
+  it('should render expected elements', () => {
+    const headings = ['Period', 'Persons', 'Male', 'Female'];
+
+    render(
+      <InequalitiesLineChartTable
+        tableData={tableData}
+        dynamicKeys={['Persons', 'Male', 'Female']}
+      />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(screen.getAllByRole('columnheader')[0]).toHaveTextContent(
+      MOCK_HEALTH_DATA[1].areaName
+    );
+    expect(screen.getAllByRole('columnheader')[0]).toHaveAttribute(
+      'colspan',
+      headings.length.toString()
+    );
+    expect(screen.getAllByRole('cell')).toHaveLength(
+      tableData.rowData.length * CELLS_PER_ROW
+    );
+    headings.forEach((heading, index) =>
+      expect(
+        screen.getByTestId(`header-${heading}-${index}`)
+      ).toBeInTheDocument()
+    );
+  });
+
+  it('should display x if data point is not available', () => {
+    const expectedNumberOfRows = 2;
     const tableData: InequalitiesChartData = {
       areaName: MOCK_HEALTH_DATA[1].areaName,
       rowData: [
@@ -21,137 +85,76 @@ describe('Inequalities table suite', () => {
         },
         {
           period: 2008,
-          inequalities: {
-            Persons: { value: 135.149304 },
-            Male: { value: 890.328253 },
-            Female: { value: 890.328253 },
-          },
+          inequalities: { Persons: { value: 135.149304 } },
         },
       ],
     };
 
-    it('should render the InequalitiesTable component', () => {
-      render(
-        <InequalitiesLineChartTable
-          tableData={tableData}
-          dynamicKeys={['Persons', 'Male', 'Female']}
-        />
-      );
+    render(
+      <InequalitiesLineChartTable
+        tableData={tableData}
+        dynamicKeys={['Persons', 'Male', 'Female']}
+      />
+    );
+    expect(screen.getAllByRole('cell')).toHaveLength(
+      expectedNumberOfRows * CELLS_PER_ROW
+    );
+    screen
+      .getAllByTestId('not-available')
+      .forEach((id) => expect(id).toHaveTextContent('X'));
+  });
 
+  it('should render only period heading when empty dynamic keys are passed', () => {
+    const absentHeadings = ['Persons', 'Male', 'Female'];
+    const cellsPerRow = 1;
+
+    render(
+      <InequalitiesLineChartTable tableData={tableData} dynamicKeys={[]} />
+    );
+
+    expect(screen.getByTestId('header-Period-0')).toBeInTheDocument();
+    absentHeadings.forEach((heading, index) =>
       expect(
-        screen.getByTestId('inequalitiesLineChartTable-component')
-      ).toBeInTheDocument();
-    });
+        screen.queryByTestId(`header-${heading}-${index + 1}`)
+      ).not.toBeInTheDocument()
+    );
+    expect(screen.getAllByRole('cell')).toHaveLength(
+      tableData.rowData.length * cellsPerRow
+    );
+  });
 
-    it('should render expected elements', () => {
-      const headings = ['Period', 'Persons', 'Male', 'Female'];
+  it('When the UI is rendered with measurement unit it should render correctly', () => {
+    render(
+      <InequalitiesLineChartTable
+        tableData={tableData}
+        dynamicKeys={['Persons', 'Male', 'Female']}
+        measurementUnit="per 100,000"
+      />
+    );
+    expect(screen.getByText('Value: per 100,000')).toBeInTheDocument();
+  });
 
-      render(
-        <InequalitiesLineChartTable
-          tableData={tableData}
-          dynamicKeys={['Persons', 'Male', 'Female']}
-        />
-      );
+  it("Value: 'unit measurement' should not be in document when the unit measurement is not provided", () => {
+    render(
+      <InequalitiesLineChartTable
+        tableData={tableData}
+        dynamicKeys={['Persons', 'Male', 'Female']}
+      />
+    );
 
-      expect(screen.getByRole('table')).toBeInTheDocument();
-      expect(screen.getAllByRole('columnheader')[0]).toHaveTextContent(
-        MOCK_HEALTH_DATA[1].areaName
-      );
-      expect(screen.getAllByRole('cell')).toHaveLength(
-        tableData.rowData.length * CELLS_PER_ROW
-      );
-      headings.forEach((heading, index) =>
-        expect(
-          screen.getByTestId(`header-${heading}-${index}`)
-        ).toBeInTheDocument()
-      );
-    });
+    expect(
+      screen.queryByTestId('inequalitiesLineChartTable-measurementUnit')
+    ).not.toBeInTheDocument();
+  });
 
-    it('should display x if data point is not available', () => {
-      const expectedNumberOfRows = 2;
-      const tableData: InequalitiesChartData = {
-        areaName: MOCK_HEALTH_DATA[1].areaName,
-        rowData: [
-          {
-            period: 2004,
-            inequalities: {
-              Persons: { value: 890.3432 },
-              Male: { value: 703.420759 },
-              Female: { value: 703.420759 },
-            },
-          },
-          {
-            period: 2008,
-            inequalities: { Persons: { value: 135.149304 } },
-          },
-        ],
-      };
-
-      render(
-        <InequalitiesLineChartTable
-          tableData={tableData}
-          dynamicKeys={['Persons', 'Male', 'Female']}
-        />
-      );
-      expect(screen.getAllByRole('cell')).toHaveLength(
-        expectedNumberOfRows * CELLS_PER_ROW
-      );
-      screen
-        .getAllByTestId('not-available')
-        .forEach((id) => expect(id).toHaveTextContent('X'));
-    });
-
-    it('should render only period heading when empty dynamic keys are passed', () => {
-      const absentHeadings = ['Persons', 'Male', 'Female'];
-      const cellsPerRow = 1;
-
-      render(
-        <InequalitiesLineChartTable tableData={tableData} dynamicKeys={[]} />
-      );
-
-      expect(screen.getByTestId('header-Period-0')).toBeInTheDocument();
-      absentHeadings.forEach((heading, index) =>
-        expect(
-          screen.queryByTestId(`header-${heading}-${index + 1}`)
-        ).not.toBeInTheDocument()
-      );
-      expect(screen.getAllByRole('cell')).toHaveLength(
-        tableData.rowData.length * cellsPerRow
-      );
-    });
-
-    it('When the UI is rendered with measurement unit it should render correctly', () => {
-      render(
-        <InequalitiesLineChartTable
-          tableData={tableData}
-          dynamicKeys={['Persons', 'Male', 'Female']}
-          measurementUnit="per 100,000"
-        />
-      );
-      expect(screen.getByText('Value: per 100,000')).toBeInTheDocument();
-    });
-
-    it("Value: 'unit measurement' should not be in document when the unit measurement is not provided", () => {
-      render(
-        <InequalitiesLineChartTable
-          tableData={tableData}
-          dynamicKeys={['Persons', 'Male', 'Female']}
-        />
-      );
-
-      expect(
-        screen.queryByTestId('inequalitiesLineChartTable-measurementUnit')
-      ).not.toBeInTheDocument();
-    });
-    it('snapshot test - should match snapshot', () => {
-      const container = render(
-        <InequalitiesLineChartTable
-          tableData={tableData}
-          dynamicKeys={['Persons', 'Male', 'Female']}
-          measurementUnit="per 100,000"
-        />
-      );
-      expect(container.asFragment()).toMatchSnapshot();
-    });
+  it('snapshot test - should match snapshot', () => {
+    const container = render(
+      <InequalitiesLineChartTable
+        tableData={tableData}
+        dynamicKeys={['Persons', 'Male', 'Female']}
+        measurementUnit="per 100,000"
+      />
+    );
+    expect(container.asFragment()).toMatchSnapshot();
   });
 });
