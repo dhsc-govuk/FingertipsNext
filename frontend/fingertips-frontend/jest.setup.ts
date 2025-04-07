1227c11873dfc85d431b2b71ed2c51f3c5525f52
Gary.Stanley <gary.stanley@bjss.com>
2025-04-08 00:35:29 +0100

    remove dead code

--- frontend/fingertips-frontend/jest.setup.ts
+++ frontend/fingertips-frontend/jest.setup.ts
@@ -3,14 +3,6 @@ import 'jest-styled-components';
 
 process.env.FINGERTIPS_API_URL = 'ft-api-url/';
 
-jest.mock('@/lib/logging', () => {
-  const original = jest.requireActual('@/lib/logging');
-  return {
-    ...original,
-    logUsingMockAiSearchService: jest.fn(),
-  };
-});
-
 const windowMock = {
   scrollTo: jest.fn(),
 };
--- frontend/fingertips-frontend/lib/logging.ts
+++
@@ -1,3 +0,0 @@
-export function logUsingMockAiSearchService(user: string): void {
-  console.warn(`${user}: using Mock AI Search Service.`);
-}
