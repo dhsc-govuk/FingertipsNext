4c6a2de041041c2a68246e83d92fdff0134fb206
Zoe Nolan <zoe.nolan@bjss.com>
2025-04-07 10:52:05 +0100

    DHSCFT-293: Tidy up Header and Row test naming

--- frontend/fingertips-frontend/components/organisms/SpineChartTable/SpineChartTableHeader.test.tsx
+++ frontend/fingertips-frontend/components/organisms/SpineChartTable/SpineChartTableHeader.test.tsx
@@ -6,87 +6,85 @@ import {
 } from './SpineChartTableHeader';
 import { GovukColours } from '@/lib/styleHelpers/colours';
 
-describe('Spine chart table suite', () => {
+describe('Spine chart table header', () => {
   const mockHeaderData = {
     area: 'testArea',
     group: 'testGroup',
   };
 
-  describe('Spine chart table header', () => {
-    it('should contain the expected elements', () => {
-      render(
-        <table>
-          <thead>
-            <SpineChartTableHeader
-              areaName={mockHeaderData.area}
-              groupName={mockHeaderData.group}
-            />
-          </thead>
-        </table>
-      );
+  it('should contain the expected elements', () => {
+    render(
+      <table>
+        <thead>
+          <SpineChartTableHeader
+            areaName={mockHeaderData.area}
+            groupName={mockHeaderData.group}
+          />
+        </thead>
+      </table>
+    );
 
-      expect(screen.getByTestId('empty-header')).toHaveTextContent('');
-      expect(screen.getByTestId('area-header')).toHaveTextContent(
-        `${mockHeaderData.area}`
-      );
-      expect(screen.getByTestId('group-header')).toHaveTextContent(
-        `${mockHeaderData.group}`
-      );
-      expect(screen.getByTestId('england-header')).toHaveTextContent(
-        `Benchmark: England`
-      );
+    expect(screen.getByTestId('empty-header')).toHaveTextContent('');
+    expect(screen.getByTestId('area-header')).toHaveTextContent(
+      `${mockHeaderData.area}`
+    );
+    expect(screen.getByTestId('group-header')).toHaveTextContent(
+      `${mockHeaderData.group}`
+    );
+    expect(screen.getByTestId('england-header')).toHaveTextContent(
+      `Benchmark: England`
+    );
 
-      Object.values(SpineChartTableHeadingEnum).forEach((heading) =>
-        expect(screen.getByTestId(`${heading}-header`)).toBeInTheDocument()
-      );
-    });
+    Object.values(SpineChartTableHeadingEnum).forEach((heading) =>
+      expect(screen.getByTestId(`${heading}-header`)).toBeInTheDocument()
+    );
+  });
 
-    it('should have grey cell color for benchmark column', () => {
-      render(
-        <table>
-          <thead>
-            <SpineChartTableHeader
-              areaName={mockHeaderData.area}
-              groupName={mockHeaderData.group}
-            />
-          </thead>
-        </table>
-      );
+  it('should have grey cell color for benchmark column', () => {
+    render(
+      <table>
+        <thead>
+          <SpineChartTableHeader
+            areaName={mockHeaderData.area}
+            groupName={mockHeaderData.group}
+          />
+        </thead>
+      </table>
+    );
 
-      expect(screen.getByTestId('england-header')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
+    expect(screen.getByTestId('england-header')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
 
-      expect(screen.getByTestId('Value-header')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
-      expect(screen.getByTestId('Worst-header')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
-      expect(screen.getByTestId('Best-header')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
-    });
+    expect(screen.getByTestId('Value-header')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
+    expect(screen.getByTestId('Worst-header')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
+    expect(screen.getByTestId('Best-header')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
+  });
 
-    it('should have light grey cell color for the group column', () => {
-      render(
-        <table>
-          <thead>
-            <SpineChartTableHeader
-              areaName={mockHeaderData.area}
-              groupName={mockHeaderData.group}
-            />
-          </thead>
-        </table>
-      );
+  it('should have light grey cell color for the group column', () => {
+    render(
+      <table>
+        <thead>
+          <SpineChartTableHeader
+            areaName={mockHeaderData.area}
+            groupName={mockHeaderData.group}
+          />
+        </thead>
+      </table>
+    );
 
-      expect(screen.getByTestId('group-header')).toHaveStyle(
-        `background-color: ${GovukColours.LightGrey}`
-      );
+    expect(screen.getByTestId('group-header')).toHaveStyle(
+      `background-color: ${GovukColours.LightGrey}`
+    );
 
-      expect(screen.getByTestId('GroupValue-header')).toHaveStyle(
-        `background-color: ${GovukColours.LightGrey}`
-      );
-    });
+    expect(screen.getByTestId('GroupValue-header')).toHaveStyle(
+      `background-color: ${GovukColours.LightGrey}`
+    );
   });
 });
--- frontend/fingertips-frontend/components/organisms/SpineChartTable/SpineChartTableRow.test.tsx
+++ frontend/fingertips-frontend/components/organisms/SpineChartTable/SpineChartTableRow.test.tsx
@@ -4,7 +4,7 @@ import { SpineChartTableRow } from './SpineChartTableRow';
 import { GovukColours } from '@/lib/styleHelpers/colours';
 import { HealthDataPointTrendEnum } from '@/generated-sources/ft-api-client';
 
-describe('Spine chart table suite', () => {
+describe('Spine chart table row', () => {
   const mockRowData = {
     indicatorId: 1,
     indicator: 'indicator',
@@ -23,90 +23,88 @@ describe('Spine chart table suite', () => {
     },
   };
 
-  describe('Spine chart table row', () => {
-    it('should have grey cell color for benchmark column', () => {
-      render(
-        <table>
-          <tbody>
-            <SpineChartTableRow
-              indicatorId={mockRowData.indicatorId}
-              indicator={mockRowData.indicator}
-              unit={mockRowData.unit}
-              period={mockRowData.period}
-              trend={mockRowData.trend}
-              count={mockRowData.count}
-              value={mockRowData.value}
-              groupValue={mockRowData.groupValue}
-              benchmarkValue={mockRowData.benchmarkValue}
-              benchmarkStatistics={mockRowData.benchmarkStatistics}
-            />
-          </tbody>
-        </table>
-      );
+  it('should have grey cell color for benchmark column', () => {
+    render(
+      <table>
+        <tbody>
+          <SpineChartTableRow
+            indicatorId={mockRowData.indicatorId}
+            indicator={mockRowData.indicator}
+            unit={mockRowData.unit}
+            period={mockRowData.period}
+            trend={mockRowData.trend}
+            count={mockRowData.count}
+            value={mockRowData.value}
+            groupValue={mockRowData.groupValue}
+            benchmarkValue={mockRowData.benchmarkValue}
+            benchmarkStatistics={mockRowData.benchmarkStatistics}
+          />
+        </tbody>
+      </table>
+    );
 
-      expect(screen.getByTestId('benchmark-value-cell')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
-      expect(screen.getByTestId('benchmark-worst-cell')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
-      expect(screen.getByTestId('benchmark-best-cell')).toHaveStyle(
-        `background-color: ${GovukColours.MidGrey}`
-      );
-    });
+    expect(screen.getByTestId('benchmark-value-cell')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
+    expect(screen.getByTestId('benchmark-worst-cell')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
+    expect(screen.getByTestId('benchmark-best-cell')).toHaveStyle(
+      `background-color: ${GovukColours.MidGrey}`
+    );
+  });
 
-    it('should have light grey cell color for benchmark column', () => {
-      render(
-        <table>
-          <tbody>
-            <SpineChartTableRow
-              indicatorId={mockRowData.indicatorId}
-              indicator={mockRowData.indicator}
-              unit={mockRowData.unit}
-              period={mockRowData.period}
-              trend={mockRowData.trend}
-              count={mockRowData.count}
-              value={mockRowData.value}
-              groupValue={mockRowData.groupValue}
-              benchmarkValue={mockRowData.benchmarkValue}
-              benchmarkStatistics={mockRowData.benchmarkStatistics}
-            />
-          </tbody>
-        </table>
-      );
+  it('should have light grey cell color for benchmark column', () => {
+    render(
+      <table>
+        <tbody>
+          <SpineChartTableRow
+            indicatorId={mockRowData.indicatorId}
+            indicator={mockRowData.indicator}
+            unit={mockRowData.unit}
+            period={mockRowData.period}
+            trend={mockRowData.trend}
+            count={mockRowData.count}
+            value={mockRowData.value}
+            groupValue={mockRowData.groupValue}
+            benchmarkValue={mockRowData.benchmarkValue}
+            benchmarkStatistics={mockRowData.benchmarkStatistics}
+          />
+        </tbody>
+      </table>
+    );
 
-      expect(screen.getByTestId('group-value-cell')).toHaveStyle(
-        `background-color: ${GovukColours.LightGrey}`
-      );
-    });
+    expect(screen.getByTestId('group-value-cell')).toHaveStyle(
+      `background-color: ${GovukColours.LightGrey}`
+    );
+  });
 
-    it('should have X for missing data', () => {
-      render(
-        <table>
-          <tbody>
-            <SpineChartTableRow
-              indicatorId={mockRowData.indicatorId}
-              indicator={mockRowData.indicator}
-              unit={mockRowData.unit}
-              period={mockRowData.period}
-              trend={mockRowData.trend}
-              count={undefined}
-              value={undefined}
-              groupValue={undefined}
-              benchmarkValue={undefined}
-              benchmarkStatistics={mockRowData.benchmarkStatistics}
-            />
-          </tbody>
-        </table>
-      );
+  it('should have X for missing data', () => {
+    render(
+      <table>
+        <tbody>
+          <SpineChartTableRow
+            indicatorId={mockRowData.indicatorId}
+            indicator={mockRowData.indicator}
+            unit={mockRowData.unit}
+            period={mockRowData.period}
+            trend={mockRowData.trend}
+            count={undefined}
+            value={undefined}
+            groupValue={undefined}
+            benchmarkValue={undefined}
+            benchmarkStatistics={mockRowData.benchmarkStatistics}
+          />
+        </tbody>
+      </table>
+    );
 
-      expect(screen.getByTestId('count-cell')).toHaveTextContent(`X`);
+    expect(screen.getByTestId('count-cell')).toHaveTextContent(`X`);
 
-      expect(screen.getByTestId('value-cell')).toHaveTextContent(`X`);
+    expect(screen.getByTestId('value-cell')).toHaveTextContent(`X`);
 
-      expect(screen.getByTestId('group-value-cell')).toHaveTextContent(`X`);
+    expect(screen.getByTestId('group-value-cell')).toHaveTextContent(`X`);
 
-      expect(screen.getByTestId('benchmark-value-cell')).toHaveTextContent(`X`);
-    });
+    expect(screen.getByTestId('benchmark-value-cell')).toHaveTextContent(`X`);
   });
 });
