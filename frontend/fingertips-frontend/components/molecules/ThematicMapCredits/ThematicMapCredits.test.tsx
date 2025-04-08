6e5e25fc4581cba116ec1f6d7f4bec78daa02c8c
WolfieKnee <david.wolfson@bjss.com>
2025-04-08 08:10:55 +0100

    testing

--- frontend/fingertips-frontend/components/molecules/ThematicMapCredits/ThematicMapCredits.test.tsx
+++ frontend/fingertips-frontend/components/molecules/ThematicMapCredits/ThematicMapCredits.test.tsx
@@ -1,3 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { ThematicMapCredits } from '.';
+
+const mockDataSource = 'BJSS Leeds';
+const mockAreaType = 'regions';
+
 describe('ThematicMapCredits', () => {
-  it.todo('should have some tests');
+  it('should display the map source and data source', async () => {
+    render(
+      <ThematicMapCredits areaType={mockAreaType} dataSource={mockDataSource} />
+    );
+
+    expect(
+      screen.getByText(`Map source: `, { exact: false })
+    ).toBeInTheDocument();
+    expect(screen.queryByText(RegExp(mockDataSource))).toBeInTheDocument();
+    expect(
+      screen.getByText(`Data source: ${mockDataSource}`)
+    ).toBeInTheDocument();
+  });
+
+  it('should not display data source when metadata does not exist', async () => {
+    render(<ThematicMapCredits areaType={mockAreaType} />);
+    screen.debug();
+
+    expect(screen.queryByText(/Data source: /)).not.toBeInTheDocument();
+  });
 });
--- frontend/fingertips-frontend/components/organisms/ThematicMap/ThematicMap.test.tsx
+++ frontend/fingertips-frontend/components/organisms/ThematicMap/ThematicMap.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { mockHealthData } from '@/mock/data/healthdata';
 import { ThematicMap } from '.';
 import { getMapGeographyData } from '@/components/organisms/ThematicMap/thematicMapHelpers';
@@ -8,17 +8,6 @@ import { SearchParams } from '@/lib/searchStateManager';
 const mockAreaType = 'regions';
 const mockAreaCodes = ['E12000001', 'E12000002'];
 const mockMapGeographyData = getMapGeographyData(mockAreaType, mockAreaCodes);
-const mockMetaData = {
-  indicatorID: '108',
-  indicatorName: 'pancakes eaten',
-  indicatorDefinition: 'number of pancakes consumed',
-  dataSource: 'BJSS Leeds',
-  earliestDataPeriod: '2025',
-  latestDataPeriod: '2025',
-  lastUpdatedDate: new Date('March 4, 2025'),
-  unitLabel: 'pancakes',
-  hasInequalities: true,
-};
 
 const mockGetSearchState = jest.fn();
 const mockSearchStateContext: SearchStateContext = {
@@ -45,58 +34,26 @@ describe('ThematicMap', () => {
         mapGeographyData={await mockMapGeographyData}
         benchmarkComparisonMethod={'Unknown'}
         polarity={'Unknown'}
-        indicatorMetadata={mockMetaData}
       />
     );
 
-    const actual = await screen.findByTestId('benchmarkLegend-component');
-    expect(actual).toBeInTheDocument();
+    expect(
+      await screen.findByTestId('benchmarkLegend-component')
+    ).toBeInTheDocument();
   });
 
-  it('should display data source when metadata exists', async () => {
+  it('should render the credits', async () => {
     render(
       <ThematicMap
         healthIndicatorData={mockHealthData['92420']}
         mapGeographyData={await mockMapGeographyData}
         benchmarkComparisonMethod={'Unknown'}
         polarity={'Unknown'}
-        indicatorMetadata={mockMetaData}
       />
     );
 
-    const actual = await screen.findAllByText('Data source:', { exact: false });
-
-    expect(actual[0]).toBeVisible();
-  });
-
-  // it('should not display data source when metadata does not exist', async () => {
-  //   render(
-  //     <ThematicMap
-  //       healthIndicatorData={mockHealthData['92420']}
-  //       mapGeographyData={await mockMapGeographyData}
-  //       benchmarkComparisonMethod={'Unknown'}
-  //       polarity={'Unknown'}
-  //     />
-  //   );
-  //   // TODO: ASSERTION GIVING FALSE NEGATIVE
-  //   expect(
-  //     await waitFor(() => screen.findByText(/Data source: /))
-  //   ).not.toBeVisible();
-  // });
-
-  it('should display maps source when it exists', async () => {
-    render(
-      <ThematicMap
-        healthIndicatorData={mockHealthData['92420']}
-        mapGeographyData={await mockMapGeographyData}
-        benchmarkComparisonMethod={'Unknown'}
-        polarity={'Unknown'}
-        indicatorMetadata={mockMetaData}
-      />
-    );
-
-    const actual = await screen.findAllByText('Map source:', { exact: false });
-
-    expect(actual[0]).toBeVisible();
+    expect(
+      await screen.findByTestId('thematic-map-credits')
+    ).toBeInTheDocument();
   });
 });
