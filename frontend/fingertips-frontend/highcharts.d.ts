aa58cb56986537e6e0e4692a962f716c4f841398
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-04-09 10:31:37 +0100

    remove this highchart configuration base request

--- frontend/fingertips-frontend/highcharts.d.ts
+++
@@ -1,9 +0,0 @@
-import _Highcharts from 'highcharts';
-
-declare module 'highcharts' {
-  export * from 'highcharts';
-
-  export const Templating: {
-    helpers: Record<string, (...args: number[]) => unknown>;
-  };
-}
