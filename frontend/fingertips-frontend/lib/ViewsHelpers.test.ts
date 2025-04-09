102c518fe3bebfa3c33b9e93960d27af935089b0
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-04-09 14:32:34 +0100

    remove unused imports

--- frontend/fingertips-frontend/lib/ViewsHelpers.test.ts
+++ frontend/fingertips-frontend/lib/ViewsHelpers.test.ts
@@ -1,8 +1,4 @@
-import {
-  chunkArray,
-  getHealthDataForIndicator,
-  HealthDataRequestAreas,
-} from './ViewsHelpers';
+import { chunkArray, getHealthDataForIndicator } from './ViewsHelpers';
 import {
   IndicatorsApi,
   IndicatorWithHealthDataForArea,
