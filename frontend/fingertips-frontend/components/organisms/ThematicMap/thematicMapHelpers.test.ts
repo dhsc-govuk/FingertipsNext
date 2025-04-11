b7c4eb82cd002d81bf35359bb21fb5c82f40d85b
WolfieKnee <david.wolfson@bjss.com>
2025-04-11 12:21:54 +0100

    add test for no data

--- frontend/fingertips-frontend/components/organisms/ThematicMap/thematicMapHelpers.test.ts
+++ frontend/fingertips-frontend/components/organisms/ThematicMap/thematicMapHelpers.test.ts
@@ -19,6 +19,7 @@ import {
 } from '@/mock/data/mapGroupBoundaries';
 import {
   BenchmarkComparisonMethod,
+  BenchmarkOutcome,
   HealthDataForArea,
   HealthDataPointTrendEnum,
   IndicatorPolarity,
@@ -87,112 +88,134 @@ describe('getMapGeographyData', () => {
 });
 
 describe('prepareThematicMapSeriesData', () => {
-  const mockHealthData: HealthDataForArea[] = [
-    {
-      areaCode: 'E92000001',
-      areaName: 'England',
-      healthData: [
-        {
-          year: 2004,
-          value: 978.34,
-          ageBand: allAgesAge,
-          sex: personsSex,
-          trend: HealthDataPointTrendEnum.CannotBeCalculated,
-          benchmarkComparison: { outcome: 'Better' },
-          deprivation: noDeprivation,
-        },
-        {
-          year: 2008,
-          value: 800.232,
-          ageBand: allAgesAge,
-          sex: personsSex,
-          trend: HealthDataPointTrendEnum.CannotBeCalculated,
-          benchmarkComparison: { outcome: 'Higher' },
-          deprivation: noDeprivation,
-        },
-      ],
-    },
-    {
-      areaCode: 'E12000001',
-      areaName: 'North East region (statistical)',
-      healthData: [
-        {
-          year: 2004,
-          value: 856.344,
-          ageBand: allAgesAge,
-          sex: personsSex,
-          trend: HealthDataPointTrendEnum.CannotBeCalculated,
-          benchmarkComparison: { outcome: 'Lower' },
-          deprivation: noDeprivation,
-        },
-        {
-          year: 2008,
-          value: 767.343,
-          ageBand: allAgesAge,
-          sex: personsSex,
-          trend: HealthDataPointTrendEnum.CannotBeCalculated,
-          benchmarkComparison: { outcome: 'NotCompared' },
-          deprivation: noDeprivation,
-        },
-      ],
-    },
-    {
-      areaCode: 'E12000003',
-      areaName: 'Yorkshire and the Humber region (statistical)',
-      healthData: [
-        {
-          year: 2004,
-          value: 674.434,
-          ageBand: allAgesAge,
-          sex: personsSex,
-          trend: HealthDataPointTrendEnum.CannotBeCalculated,
-          benchmarkComparison: { outcome: 'Similar' },
-          deprivation: noDeprivation,
-        },
-        {
-          year: 2008,
-          value: 643.434,
-          ageBand: allAgesAge,
-          sex: personsSex,
-          trend: HealthDataPointTrendEnum.CannotBeCalculated,
-          benchmarkComparison: { outcome: 'Worse' },
-          deprivation: noDeprivation,
-        },
-      ],
-    },
-  ];
-
-  const expected = [
-    {
-      areaName: mockHealthData[0].areaName,
-      areaCode: mockHealthData[0].areaCode,
-      value: mockHealthData[0].healthData[1].value,
-      year: mockHealthData[0].healthData[1].year,
-      benchmarkComparisonOutcome:
-        mockHealthData[0].healthData[1].benchmarkComparison?.outcome,
-      benchmarkColourCode: 55,
-    },
-    {
-      areaName: mockHealthData[1].areaName,
-      areaCode: mockHealthData[1].areaCode,
-      value: mockHealthData[1].healthData[1].value,
-      year: mockHealthData[1].healthData[1].year,
-      benchmarkComparisonOutcome:
-        mockHealthData[1].healthData[1].benchmarkComparison?.outcome,
-      benchmarkColourCode: 5,
-    },
-    {
-      areaName: mockHealthData[2].areaName,
-      areaCode: mockHealthData[2].areaCode,
-      value: mockHealthData[2].healthData[1].value,
-      year: mockHealthData[2].healthData[1].year,
-      benchmarkComparisonOutcome:
-        mockHealthData[2].healthData[1].benchmarkComparison?.outcome,
-      benchmarkColourCode: 35,
-    },
-  ];
   it('should return the expected series data, for the most recent year', () => {
+    const mockHealthData: HealthDataForArea[] = [
+      {
+        areaCode: 'E92000001',
+        areaName: 'England',
+        healthData: [
+          {
+            year: 2004,
+            value: 978.34,
+            ageBand: allAgesAge,
+            sex: personsSex,
+            trend: HealthDataPointTrendEnum.CannotBeCalculated,
+            benchmarkComparison: { outcome: 'Better' },
+            deprivation: noDeprivation,
+          },
+          {
+            year: 2008,
+            value: 800.232,
+            ageBand: allAgesAge,
+            sex: personsSex,
+            trend: HealthDataPointTrendEnum.CannotBeCalculated,
+            benchmarkComparison: { outcome: 'Higher' },
+            deprivation: noDeprivation,
+          },
+        ],
+      },
+      {
+        areaCode: 'E12000001',
+        areaName: 'North East region (statistical)',
+        healthData: [
+          {
+            year: 2004,
+            value: 856.344,
+            ageBand: allAgesAge,
+            sex: personsSex,
+            trend: HealthDataPointTrendEnum.CannotBeCalculated,
+            benchmarkComparison: { outcome: 'Lower' },
+            deprivation: noDeprivation,
+          },
+          {
+            year: 2008,
+            value: 767.343,
+            ageBand: allAgesAge,
+            sex: personsSex,
+            trend: HealthDataPointTrendEnum.CannotBeCalculated,
+            benchmarkComparison: { outcome: 'NotCompared' },
+            deprivation: noDeprivation,
+          },
+        ],
+      },
+      {
+        areaCode: 'E12000003',
+        areaName: 'Yorkshire and the Humber region (statistical)',
+        healthData: [
+          {
+            year: 2004,
+            value: 674.434,
+            ageBand: allAgesAge,
+            sex: personsSex,
+            trend: HealthDataPointTrendEnum.CannotBeCalculated,
+            benchmarkComparison: { outcome: 'Similar' },
+            deprivation: noDeprivation,
+          },
+          {
+            year: 2008,
+            value: 643.434,
+            ageBand: allAgesAge,
+            sex: personsSex,
+            trend: HealthDataPointTrendEnum.CannotBeCalculated,
+            benchmarkComparison: { outcome: 'Worse' },
+            deprivation: noDeprivation,
+          },
+        ],
+      },
+    ];
+
+    const expected = [
+      {
+        areaName: mockHealthData[0].areaName,
+        areaCode: mockHealthData[0].areaCode,
+        value: mockHealthData[0].healthData[1].value,
+        year: mockHealthData[0].healthData[1].year,
+        benchmarkComparisonOutcome:
+          mockHealthData[0].healthData[1].benchmarkComparison?.outcome,
+        benchmarkColourCode: 55,
+      },
+      {
+        areaName: mockHealthData[1].areaName,
+        areaCode: mockHealthData[1].areaCode,
+        value: mockHealthData[1].healthData[1].value,
+        year: mockHealthData[1].healthData[1].year,
+        benchmarkComparisonOutcome:
+          mockHealthData[1].healthData[1].benchmarkComparison?.outcome,
+        benchmarkColourCode: 5,
+      },
+      {
+        areaName: mockHealthData[2].areaName,
+        areaCode: mockHealthData[2].areaCode,
+        value: mockHealthData[2].healthData[1].value,
+        year: mockHealthData[2].healthData[1].year,
+        benchmarkComparisonOutcome:
+          mockHealthData[2].healthData[1].benchmarkComparison?.outcome,
+        benchmarkColourCode: 35,
+      },
+    ];
+
     const actual = prepareThematicMapSeriesData(mockHealthData);
+    expect(actual).toEqual(expected);
+  });
+  it('should return the expected series data, for areas with no data', () => {
+    const actual = prepareThematicMapSeriesData([
+      {
+        areaCode: 'Code1',
+        areaName: 'Area 1',
+        healthData: [],
+      },
+    ]);
+
+    const expected = [
+      {
+        areaName: 'Area 1',
+        areaCode: 'Code1',
+        value: undefined,
+        year: undefined,
+        benchmarkComparisonOutcome: BenchmarkOutcome.NotCompared,
+        benchmarkColourCode: 5,
+      },
+    ];
 
     expect(actual).toEqual(expected);
   });
