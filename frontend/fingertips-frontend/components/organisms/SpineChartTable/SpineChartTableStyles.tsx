18d65377c6d7f13106caca36a0a5b45dd134228e
Daniel Yip <daniel.yip@bjss.com>
2025-04-16 13:49:06 +0100

    Change spine chart table stickiness

--- frontend/fingertips-frontend/components/organisms/SpineChartTable/SpineChartTableStyles.tsx
+++ frontend/fingertips-frontend/components/organisms/SpineChartTable/SpineChartTableStyles.tsx
@@ -9,14 +9,9 @@ import {
   StyledIndicatorTitleCell,
 } from '@/lib/tableHelpers';
 import { Table } from 'govuk-react';
-import {
-  paddingSize,
-  spineChartIndicatorTitleColumnMinWidth,
-  spineChartPeriodColumnMinWidth,
-} from './spineChartTableHelpers';
 
 export const StyledTable = styled(Table)({
-  borderCollapse: 'collapse',
+  borderCollapse: 'separate',
 });
 
 export const StyledDivTableContainer = styled.div({
@@ -59,19 +54,9 @@ export const StyledAlignRightBorderHeader = styled(StyledAlignRightHeader)({
 });
 
 export const StyledAlignLeftStickyLeftHeader = styled(StyledAlignLeftHeader)(
-  stickyLeft as unknown as TemplateStringsArray
+  {...stickyLeft as unknown as TemplateStringsArray,
+  borderRight: 'solid #bfc1c3 1px'}
 );
-
-export const StickyValueUnitHeader = styled(StyledAlignRightBorderHeader)({
-  ...(stickyLeft as unknown as TemplateStringsArray),
-  left: `${spineChartIndicatorTitleColumnMinWidth + spineChartPeriodColumnMinWidth + paddingSize * 2}px`,
-});
-
-export const StickyPeriodHeader = styled(StyledAlignCentreHeader)({
-  ...(stickyLeft as unknown as TemplateStringsArray),
-  minWidth: `${spineChartPeriodColumnMinWidth}px}`,
-  left: `${spineChartIndicatorTitleColumnMinWidth + paddingSize}px`,
-});
 
 export const StyledAlignCentreTableCell = styled(StyledAlignLeftTableCell)({
   textAlign: 'center',
@@ -107,22 +92,12 @@ export const StyledAlignRightCellPadLeft = styled(StyledAlignRightTableCell)({
   fontSize: 19,
 });
 
-export const StyledValueUnitStickyCell = styled(
-  StyledAlignLeftBorderRightTableCell
-)({
-  ...(stickyLeft as unknown as TemplateStringsArray),
-  left: `${spineChartIndicatorTitleColumnMinWidth + spineChartPeriodColumnMinWidth + paddingSize * 2}px`,
-});
-
-export const StyledPeriodStickyCell = styled(StyledAlignCentreTableCell)({
-  ...(stickyLeft as unknown as TemplateStringsArray),
-  width: `${spineChartPeriodColumnMinWidth}px}`,
-  left: `${spineChartIndicatorTitleColumnMinWidth + paddingSize}px`,
-});
-
 export const StyledIndicatorTitleStickyLeftCell = styled(
   StyledIndicatorTitleCell
-)(stickyLeft as unknown as TemplateStringsArray);
+)({...stickyLeft as unknown as TemplateStringsArray,
+  borderRight: 'solid #bfc1c3 1px',
+  paddingRight: '0.5em',
+});
 
 export const StyledGroupHeader = styled(StyledGreyHeader)({
   backgroundColor: GovukColours.LightGrey,
