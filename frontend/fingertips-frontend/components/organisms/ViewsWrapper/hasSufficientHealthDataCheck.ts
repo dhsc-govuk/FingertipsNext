a1102ba9e662cbcf703d51ae1b83440f7ea54117
Fawad Ali <alibombaye@users.noreply.github.com>
2025-04-22 13:20:43 +0100

    Improve naming

--- frontend/fingertips-frontend/components/organisms/ViewsWrapper/hasSufficientHealthDataCheck.ts
+++ frontend/fingertips-frontend/components/organisms/ViewsWrapper/hasSufficientHealthDataCheck.ts
@@ -1,6 +1,6 @@
 import { IndicatorWithHealthDataForArea } from '@/generated-sources/ft-api-client';
 
-const hasSufficientAreaHealthData = (
+const checkAreaHasSufficientHealthData = (
   indicatorDataForArea: IndicatorWithHealthDataForArea,
   areasSelected: string[],
   indicatorsRequested: number
@@ -8,16 +8,16 @@ const hasSufficientAreaHealthData = (
   if (indicatorDataForArea.areaHealthData) {
     return indicatorDataForArea.areaHealthData.reduce<boolean>(
       (hasSufficientAreaHealthData, areaHealthData) => {
-        const hasAreaSelectedHealthData = areasSelected?.find(
+        const healthDataForSelectedArea = areasSelected?.find(
           (areaSelected) => areaSelected === areaHealthData.areaCode
         );
 
-        if (hasAreaSelectedHealthData) {
-          const hasSufficientAreaHealthData =
+        if (healthDataForSelectedArea) {
+          const hasSufficientHealthData =
             indicatorsRequested === 1 && areasSelected.length <= 2
               ? areaHealthData.healthData.length > 1
               : true;
-          if (hasSufficientAreaHealthData) return true;
+          if (hasSufficientHealthData) return true;
         }
         return hasSufficientAreaHealthData;
       },
@@ -33,7 +33,7 @@ export const hasSufficientHealthDataCheck = (
 ): boolean => {
   const hasHealthDataForAllSelectedAreasAndIndicators = indicatorsDataForAreas
     ?.map((indicatorData) => {
-      return hasSufficientAreaHealthData(
+      return checkAreaHasSufficientHealthData(
         indicatorData,
         areasSelected,
         indicatorsDataForAreas.length
