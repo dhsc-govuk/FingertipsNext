9dff92c93293aadfa6c6f7cce2fea5dd03cf5781
stuc-bjss <stuart.craigen@bjss.com>
2025-04-23 15:42:59 +0100

    DHSCFT-671: variable naming

--- frontend/fingertips-frontend/components/organisms/SpineChart/SpineChartQuartilesInfo.tsx
+++ frontend/fingertips-frontend/components/organisms/SpineChart/SpineChartQuartilesInfo.tsx
@@ -6,14 +6,16 @@ const DivContainer = styled.div({
   margin: '1rem 0',
 });
 
+const spineChartLegendWidth = 360;
+const spineChartLegendFontSize = 16;
+
 export const SpineChartQuartilesInfoContainer = () => {
-  const w = 360;
-  const q1 = w / 3;
+  const q1 = spineChartLegendWidth / 3;
   const q2 = q1 * 2;
-  const fontSize = 16;
+
   return (
     <DivContainer>
-      <svg width={w} height={90}>
+      <svg width={spineChartLegendWidth} height={90}>
         <g transform={`translate(0, 25)`}>
           <rect x={0} y={0} width={2} height={25} fill={GovukColours.MidGrey} />
           <rect
@@ -31,14 +33,20 @@ export const SpineChartQuartilesInfoContainer = () => {
             fill={GovukColours.MidGrey}
           />
           <rect
-            x={w - 2}
+            x={spineChartLegendWidth - 2}
             y={0}
             width={2}
             height={25}
             fill={GovukColours.MidGrey}
           />
 
-          <rect x={0} y={0} width={w} height={20} fill={GovukColours.MidGrey} />
+          <rect
+            x={0}
+            y={0}
+            width={spineChartLegendWidth}
+            height={20}
+            fill={GovukColours.MidGrey}
+          />
           <rect
             x={q1}
             y={0}
@@ -47,44 +55,52 @@ export const SpineChartQuartilesInfoContainer = () => {
             fill={GovukColours.LightGrey}
           />
           <rect
-            x={w / 2 - 1}
+            x={spineChartLegendWidth / 2 - 1}
             y={-5}
             width={2}
             height={30}
             fill={GovukColours.Black}
           />
         </g>
-        <g fontSize={fontSize}>
+        <g fontSize={spineChartLegendFontSize}>
           <g transform={'translate(0, 68)'}>
             <text x={0} textAnchor={'start'}>
               Worst/
             </text>
-            <text x={0} y={fontSize} textAnchor={'start'}>
+            <text x={0} y={spineChartLegendFontSize} textAnchor={'start'}>
               lowest
             </text>
 
             <text x={q1} textAnchor={'middle'}>
               25th
             </text>
-            <text x={q1} y={fontSize} textAnchor={'middle'}>
+            <text x={q1} y={spineChartLegendFontSize} textAnchor={'middle'}>
               percentile
             </text>
 
             <text x={q2} textAnchor={'middle'}>
               75th
             </text>
-            <text x={q2} y={fontSize} textAnchor={'middle'}>
+            <text x={q2} y={spineChartLegendFontSize} textAnchor={'middle'}>
               percentile
             </text>
 
-            <text x={w} textAnchor={'end'}>
+            <text x={spineChartLegendWidth} textAnchor={'end'}>
               Best/
             </text>
-            <text x={w} y={fontSize} textAnchor={'end'}>
+            <text
+              x={spineChartLegendWidth}
+              y={spineChartLegendFontSize}
+              textAnchor={'end'}
+            >
               highest
             </text>
           </g>
-          <text x={w / 2} y={fontSize} textAnchor={'middle'}>
+          <text
+            x={spineChartLegendWidth / 2}
+            y={spineChartLegendFontSize}
+            textAnchor={'middle'}
+          >
             Benchmark value
           </text>
         </g>
