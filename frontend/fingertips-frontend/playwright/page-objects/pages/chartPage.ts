71461648e3f4dee3d059d4bccbc4ad7b5839f94b
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-04-23 15:57:44 +0100

    add new base screenshots

--- frontend/fingertips-frontend/playwright/page-objects/pages/chartPage.ts
+++ frontend/fingertips-frontend/playwright/page-objects/pages/chartPage.ts
@@ -191,6 +191,8 @@ export default class ChartPage extends AreaFilter {
       await this.page.waitForLoadState();
       await expect(this.page.getByText('Loading')).toHaveCount(0);
       await this.page.waitForLoadState();
+      await this.page.evaluate(() => window.scrollTo(0, 0));
+      await this.page.waitForFunction('window.scrollY === 0');
       await this.page.waitForTimeout(1000);
 
       // for now just warn if visual comparisons do not match
