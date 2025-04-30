5085ce390b94a3fa5ddbd35924767ab269e38409
WolfieKnee <david.wolfson@bjss.com>
2025-04-30 14:54:09 +0100

    framework for testing of TrendDataProcessorTests

--- trend-analysis/TrendAnalysisApp.UnitTests/TrendDataProcessorTests.cs
+++ trend-analysis/TrendAnalysisApp.UnitTests/TrendDataProcessorTests.cs
@@ -1,24 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using TrendAnalysisApp.Repository;
 using TrendAnalysisApp.SearchData;
 using NSubstitute;
 using Shouldly;
 using Newtonsoft.Json.Linq;
+using TrendAnalysisApp.Calculator.Legacy;
+using TrendAnalysisApp.Mapper;
+using TrendAnalysisApp.Repository.Models;
+using TrendAnalysisApp.UnitTests.Helpers;
 
 namespace TrendAnalysisApp.UnitTests;
 
 public class TrendDataProcessorTests
 {
-    private readonly IIndicatorRepository _mockRepository;
     private readonly IIndicatorJsonFileHelper _mockFileHelper;
     private readonly TrendDataProcessor _trendDataProcessor;
+    private readonly ServiceProvider _serviceProvider;
+    private readonly HealthMeasureDbContext _dbContext;
+    private readonly HealthMeasureRepository _healthDataRepository;
 
     public TrendDataProcessorTests()
     {
-        _mockRepository = Substitute.For<IIndicatorRepository>();
         _mockFileHelper = Substitute.For<IIndicatorJsonFileHelper>();
 
-        // If time, consider adding tests for the full Process method
-        _trendDataProcessor = new TrendDataProcessor(_mockRepository, _mockFileHelper);
+        // use inMemoryDb for testing rather than mocking the context based on HealthMeasureRepository tests in API 
+        var inMemoryDbGuid = Guid.NewGuid().ToString();
+        var dbOptions = new DbContextOptionsBuilder().UseInMemoryDatabase(
+             inMemoryDbGuid
+        );
+        _dbContext = new HealthMeasureDbContext(dbOptions.Options);
+        _healthDataRepository = new HealthMeasureRepository(_dbContext);
+        
+        var indicatorRepository = new IndicatorRepository(_dbContext);
+        
+        // init TrendProcessor with repository that uses inMemoryDb 
+        _trendDataProcessor = new TrendDataProcessor(indicatorRepository, _mockFileHelper);
+        // initialise the serviceProvider using inMemoryDb (based on Program.cs) with minium subset of services
+        _serviceProvider = new ServiceCollection()
+            .AddDbContext<HealthMeasureDbContext>(
+            options => options.UseInMemoryDatabase(inMemoryDbGuid) // TODO: refactor to use _dbContext
+            )
+           .AddTransient<TrendMarkerCalculator>()
+            .AddSingleton<LegacyMapper>()
+            .BuildServiceProvider();
     }
 
     [Fact]
@@ -55,4 +80,70 @@ public class TrendDataProcessorTests
             Arg.Is<JArray>(ja => JToken.DeepEquals(ja, expectedUpdatedFileContents))
         );
     }
+    
+    // DHSCFT-559: Test 1 - trends are not calculated for population indicators (ones to skip)
+    [Fact]
+    public async Task TestTrendDataProcessor_DoesNotReturnTrendsForIndicatorsToSkip()
+    {
+        // arrange
+        // TODO: populate inMemoryDb with mock data for testing
+        // TODO: PopulateDatabase helper?
+
+        var healthMeasureModel0 = new HealthMeasureModelHelper(key: 100)
+            .WithIndicatorDimension(indicatorId: Constants.Indicator.GpRegisteredPopulationId)
+            .Build();
+        var healthMeasureModel1 = new HealthMeasureModelHelper(key: 110)
+            .WithIndicatorDimension(indicatorId: Constants.Indicator.ResidentPopulationId)
+            .Build();
+        // var healthMeasureModelX = new HealthMeasureModelHelper(key: 120, IndicatorTestData.DiabetesPrevelanceHealthMeasurePoints[0])
+        //     .WithIndicatorDimension(IndicatorTestData.DiabetesPrevalence)
+        //     .Build();
+
+        
+        
+        _dbContext.HealthMeasure.Add(healthMeasureModel0);
+        _dbContext.HealthMeasure.Add(healthMeasureModel1);
+        _dbContext.SaveChanges();
+        
+        
+        const string expectedFilePath = "SearchData/assets/indicators.json";
+       
+        // act
+        await _trendDataProcessor.Process(_serviceProvider);
+        var actual0 = _dbContext.HealthMeasure.Find(100)?.TrendDimension;
+        var actual1 = _dbContext.HealthMeasure.Find(110)?.TrendDimension;
+        
+        // actual = DB query for mock indicators
+        
+        // assert
+        // indicators should have default trends?
+        actual0?.Name.ShouldBe(Constants.Trend.NotYetCalculated);
+        actual1?.Name.ShouldBe(Constants.Trend.NotYetCalculated);
+        
+          
+        
+        // mockJSONwritter is (not?)called with expected (or it's the same)
+        // _mockFileHelper.ReceivedCalls().ShouldBeEmpty();
+        _mockFileHelper.Received().Write(
+            expectedFilePath,null);
+    }
+    
+    // DHSCFT-559: Test 2 - healthdata (for one indicator) is correctly grouped 
+    // mockRepos is called with correctly grouped data (with correct trends ~[from mock calculator?]~)
+    // mockJSONwritter is called with expected
+    
+    // DHSCFT-559: Test 3 - if no healthdata for a group
+    // mockRepos is (not?)called with expected default
+    // expected JSON is written 
+    
+    // DHSCFT-559: Test 4 - if one of two groups is aggregate
+    // mockRepos is called with expected default
+    // expected JSON is written with only the aggregate
+    
+    // IF time - test repository behaviours 
+    
 }
+
+
+// TODO: do we need to reset the Model?
+
