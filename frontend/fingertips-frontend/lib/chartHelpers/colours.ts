431bfa2d2eb7a44a4953c25c45120f299a5cd9f6
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-04-30 17:58:01 +0100

    revert this commit - just to test out screenshots

--- frontend/fingertips-frontend/lib/chartHelpers/colours.ts
+++ frontend/fingertips-frontend/lib/chartHelpers/colours.ts
@@ -10,12 +10,6 @@ export enum UniqueChartColours {
 // The ordering of this array determines the order in which the colours are
 // used by Highcharts, so it should be preserved.
 export const chartColours: ChartColours[] = [
-  GovukColours.Orange,
-  GovukColours.LightPurple,
-  GovukColours.DarkPink,
-  GovukColours.Green,
-  GovukColours.Pink,
-  GovukColours.Purple,
   GovukColours.Yellow,
   GovukColours.Red,
   GovukColours.Blue,
@@ -24,4 +18,10 @@ export const chartColours: ChartColours[] = [
   UniqueChartColours.ChartDarkBlue,
   GovukColours.LightBlue,
   GovukColours.OtherLightPurple,
+  GovukColours.Orange,
+  GovukColours.LightPurple,
+  GovukColours.DarkPink,
+  GovukColours.Green,
+  GovukColours.Pink,
+  GovukColours.Purple,
 ];
