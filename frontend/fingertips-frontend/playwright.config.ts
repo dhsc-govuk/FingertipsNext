527278ffa4e4bfd99dc37bf742e015234c9a728e
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-05-01 10:56:42 +0100

    feedback

--- frontend/fingertips-frontend/playwright.config.ts
+++ frontend/fingertips-frontend/playwright.config.ts
@@ -20,7 +20,7 @@ const config: PlaywrightTestConfig = {
   expect: {
     timeout: 10_000,
     toHaveScreenshot: {
-      maxDiffPixelRatio: 0.025,
+      maxDiffPixelRatio: 0.001,
       scale: 'device',
       pathTemplate: '.test/spec/snaps/{projectName}/{testFilePath}/{arg}{ext}',
     },
