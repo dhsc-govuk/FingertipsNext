ee29879926b16d4ed892fb129b8e98c679f86bf8
Daniel Yip <64152559+dlzhry2@users.noreply.github.com>
2025-05-07 14:46:13 +0100

    DHSCFT-727: Support postcode search (text-only) (#543)

---
+++ frontend/fingertips-frontend/playwright/tests/fully_integrated_e2e_tests/ai_search_tests/area_search.spec.ts
@@ -0,0 +1,63 @@
+import { AreaDocument } from '@/lib/search/searchTypes';
+import { test } from '../../../page-objects/pageFactory';
+import { TestTag } from '@/playwright/testHelpers';
+
+test.describe(
+  'Azure AI Search: Area search suggestions',
+  {
+    tag: TestTag.CD,
+  },
+  () => {
+    const testPostcode = 'LE12 8PY';
+    const fullMatchingGp = 'Barrow Health Centre';
+    const partialMatchingGp = 'Quorn Medical Centre';
+    const testAreaSearchTerm: AreaDocument = {
+      areaCode: 'E12000002',
+      areaType: 'Regions',
+      areaName: 'North West Region',
+    };
+
+    test.beforeEach(async ({ homePage }) => {
+      await test.step('Navigate to home page', async () => {
+        await homePage.navigateToHomePage();
+        await homePage.checkOnHomePage();
+      });
+    });
+
+    test(
+      'suggestion panel should return only expected area when there is a full match for area code',
+      {
+        tag: TestTag.CI,
+      },
+      async ({ homePage }) => {
+        await test.step('Fill in full area code and check results', async () => {
+          await homePage.searchForArea(testAreaSearchTerm.areaCode);
+          await homePage.checkAreaSuggestionPanelContainsAreas([
+            testAreaSearchTerm.areaName,
+          ]);
+        });
+      }
+    );
+
+    test('returns multiple relevant results for a partial postcode', async ({
+      homePage,
+    }) => {
+      await test.step('Fill in partial postcode and check results', async () => {
+        await homePage.searchForArea(testPostcode.substring(0, 6));
+        await homePage.checkAreaSuggestionPanelContainsAreas([
+          fullMatchingGp,
+          partialMatchingGp,
+        ]);
+      });
+    });
+
+    test('returns matching result by postcode for a full postcode search', async ({
+      homePage,
+    }) => {
+      await test.step('Fill in full postcode and check results', async () => {
+        await homePage.searchForArea(testPostcode);
+        await homePage.checkAreaSuggestionPanelContainsAreas([fullMatchingGp]);
+      });
+    });
+  }
+);
