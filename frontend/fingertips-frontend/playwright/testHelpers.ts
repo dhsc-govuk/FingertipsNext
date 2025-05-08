da1a7679d4c4d9f12f2da10d7f6f1aaab8ecb630
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-05-08 12:49:13 +0100

    Feature/dhscft 739 e2e test refactor how we select indicators to allow testing scope to widen for core journyes (#546)

--- frontend/fingertips-frontend/playwright/testHelpers.ts
+++ frontend/fingertips-frontend/playwright/testHelpers.ts
@@ -2,6 +2,21 @@ import { AreaDocument, RawIndicatorDocument } from '@/lib/search/searchTypes';
 import ChartPage from './page-objects/pages/chartPage';
 import { AreaTypeKeys } from '@/lib/areaFilterHelpers/areaType';
 
+export type SimpleIndicatorDocument = {
+  indicatorID: string;
+  indicatorName: string;
+  associatedAreaCodes: string[];
+  dataSource: string;
+};
+
+export interface TestParams {
+  indicatorMode: IndicatorMode;
+  areaMode: AreaMode;
+  searchMode: SearchMode;
+  expectedIndicatorIDsToSelect: string[];
+  subjectSearchTerm?: string;
+}
+
 export enum SearchMode {
   ONLY_SUBJECT = 'ONLY_SUBJECT',
   ONLY_AREA = 'ONLY_AREA',
@@ -220,15 +235,30 @@ const indicatorsMatchingSearchTerm = (
   indicator.indicatorName.toLowerCase().includes(normalizedSearchTerm) ||
   indicator.indicatorDefinition.toLowerCase().includes(normalizedSearchTerm);
 
-export function getAllIndicatorIds(
+const indicatorMatchingIndicatorID = (
+  indicator: RawIndicatorDocument,
+  indicatorID: string
+): boolean => {
+  // First convert to string
+  const indicatorIDString = String(indicator.indicatorID);
+
+  return indicatorIDString.toLowerCase() === indicatorID.toLowerCase();
+};
+
+export function getAllIndicators(
   indicators: RawIndicatorDocument[]
-): string[] {
+): SimpleIndicatorDocument[] {
   return indicators
     .filter((indicator) => indicatorsUsedInPOC(indicator))
-    .map((indicator) => indicator.indicatorID);
+    .map((indicator) => ({
+      indicatorName: indicator.indicatorName,
+      indicatorID: indicator.indicatorID,
+      associatedAreaCodes: indicator.associatedAreaCodes,
+      dataSource: indicator.dataSource,
+    }));
 }
 
-export function getAllIndicatorIdsForSearchTerm(
+export function getAllIndicatorIDsForSearchTerm(
   indicators: RawIndicatorDocument[],
   searchTerm: string
 ): string[] {
@@ -244,12 +274,49 @@ export function getAllIndicatorIdsForSearchTerm(
     .map((indicator) => indicator.indicatorID);
 }
 
+export function getAllIndicatorsForSearchTerm(
+  indicators: RawIndicatorDocument[],
+  searchTerm: string
+): SimpleIndicatorDocument[] {
+  if (!searchTerm) return [];
+
+  const lowerCasedSearchTerm = searchTerm.toLowerCase();
+  return indicators
+    .filter(
+      (indicator) =>
+        indicatorsUsedInPOC(indicator) &&
+        indicatorsMatchingSearchTerm(indicator, lowerCasedSearchTerm)
+    )
+    .map((indicator) => ({
+      indicatorName: indicator.indicatorName,
+      indicatorID: indicator.indicatorID,
+      associatedAreaCodes: indicator.associatedAreaCodes,
+      dataSource: indicator.dataSource,
+    }));
+}
+
+export function getIndicatorDataByIndicatorID(
+  indicators: RawIndicatorDocument[],
+  indicatorID: string
+): SimpleIndicatorDocument[] {
+  if (!indicatorID) return [];
+
+  return indicators
+    .filter((indicator) => indicatorMatchingIndicatorID(indicator, indicatorID))
+    .map((indicator) => ({
+      indicatorName: indicator.indicatorName,
+      indicatorID: indicator.indicatorID,
+      associatedAreaCodes: indicator.associatedAreaCodes,
+      dataSource: indicator.dataSource,
+    }));
+}
+
 export function getAllAreasByAreaType(
   areas: AreaDocument[],
   areaType: AreaTypeKeys
 ): AreaDocument[] {
   const sanitisedAreaType = areaType.toLowerCase().replaceAll('-', ' ');
-  console.log(sanitisedAreaType);
+
   return areas.filter((area) =>
     area.areaType.toLowerCase().includes(sanitisedAreaType)
   );
