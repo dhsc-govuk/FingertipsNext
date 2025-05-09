f152f091402cc25519a35bcbf1d4bf2bc39af400
Nayira Sanchez <196929267+Nayira-BJSS@users.noreply.github.com>
2025-05-09 09:29:15 +0100

    Added check for Pop Pyramid table and edited README

--- frontend/fingertips-frontend/playwright/testHelpers.ts
+++ frontend/fingertips-frontend/playwright/testHelpers.ts
@@ -126,6 +126,12 @@ export function getScenarioConfig(
       },
     },
     {
+      componentLocator: ChartPage.populationPyramidTableComponent,
+      componentProps: {
+        isTabTable: true,
+      },
+    },
+    {
       componentLocator: ChartPage.thematicMapComponent,
       componentProps: {},
     },
@@ -166,6 +172,7 @@ export function getScenarioConfig(
       ChartPage.inequalitiesForSingleTimePeriodComponent,
       ChartPage.inequalitiesTrendComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.ONE_INDICATOR}-${AreaMode.ENGLAND_AREA}`]: [
       ChartPage.lineChartComponent,
@@ -177,31 +184,38 @@ export function getScenarioConfig(
       ChartPage.inequalitiesForSingleTimePeriodComponent,
       ChartPage.inequalitiesTrendComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.ONE_INDICATOR}-${AreaMode.THREE_PLUS_AREAS}`]: [
       ChartPage.barChartEmbeddedTableComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.ONE_INDICATOR}-${AreaMode.ALL_AREAS_IN_A_GROUP}`]: [
       ChartPage.thematicMapComponent,
       ChartPage.barChartEmbeddedTableComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.TWO_INDICATORS}-${AreaMode.ENGLAND_AREA}`]: [
       ChartPage.basicTableComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.TWO_INDICATORS}-${AreaMode.THREE_PLUS_AREAS}`]: [
       ChartPage.heatMapComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.TWO_INDICATORS}-${AreaMode.ALL_AREAS_IN_A_GROUP}`]: [
       ChartPage.heatMapComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
     ],
     [`${IndicatorMode.THREE_PLUS_INDICATORS}-${AreaMode.TWO_AREAS}`]: [
       ChartPage.heatMapComponent,
       ChartPage.populationPyramidComponent,
+      ChartPage.populationPyramidTableComponent,
       ChartPage.spineChartTableComponent, // needs to be last so scroll right doesn't impact other component screenshots
     ],
   };
