5545c6d381d0692ed0b9887d62d33db8da315ae9
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-05-13 13:06:03 +0100

    add doc string

--- search-setup/src/types.ts
+++ search-setup/src/types.ts
@@ -42,6 +42,16 @@ export interface SuggestionResult {
   value: { '@search.text': string }[];
 }
 
+/**
+ * The synonym data is found in the assets/indicator-synonyms.json directory
+ * "keyword": <string>            # The main keyword to be mapped
+ * "synonyms": <list of strings>  # A list of synonyms related to the keyword
+ * "isExplicit": <boolean>        # A flag that indicates whether the keyword should be one-way mapped or equivalently mapped to its synonyms
+ *
+ *  Azure synonyms allow explicit (one-way) mapping and equivalent mapping from a keyword to its synonyms
+ *  Explicit mapping ensures that a search for the keyword matches results containing its synonyms, but the reverse is not the case
+ *  Equivalent mapping ensures that a search for the keyword or its synonyms matches results containing the keyword or its synonyms
+ */
 export type SynonymData = Array<{
   keyword: string;
   synonyms: string[];
