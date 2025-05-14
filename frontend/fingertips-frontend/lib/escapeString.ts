a3445e47b180c4862946315c8367123e98c7d592
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-05-14 07:42:22 +0100

    JavaScript regular expression during some clever stuff I don't need to escapse open bracket

--- frontend/fingertips-frontend/lib/escapeString.ts
+++ frontend/fingertips-frontend/lib/escapeString.ts
@@ -4,5 +4,5 @@
    reference: https://learn.microsoft.com/en-us/azure/search/query-lucene-syntax#escaping-special-characters
 */
 export const escapeString = (text: string): string => {
-  return text.replace(/[+\-&|!(){}\[\]^"~*?:\\\/]/g, '\\$&');
+  return text.replace(/[+\-&|!(){}[\]^"~*?:\\/]/g, '\\$&');
 };
