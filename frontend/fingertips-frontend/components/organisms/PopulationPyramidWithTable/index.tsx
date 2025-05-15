fcfecf6dfcf174250f76a2e29a15513f7d314165
Fawad Ali <alibombaye@users.noreply.github.com>
2025-05-15 11:41:58 +0100

    DHSCFT-784: fix for default state (#578)
    Co-authored-by: GitHub Actions Bot <actions@github.com>

--- frontend/fingertips-frontend/components/organisms/PopulationPyramidWithTable/index.tsx
+++ frontend/fingertips-frontend/components/organisms/PopulationPyramidWithTable/index.tsx
@@ -11,50 +11,18 @@ import { H3, H5 } from 'govuk-react';
 import {
   determineHealthDataForArea,
   seriesDataWithoutGroup,
-  sortHealthDataPointsByDescendingYear,
 } from '@/lib/chartHelpers/chartHelpers';
 import { SearchParams, SearchStateParams } from '@/lib/searchStateManager';
 import { ArrowExpander } from '@/components/molecules/ArrowExpander';
 import { PopulationPyramidChartTable } from '../PopulationPyramidChartTable';
 import { ChartSelectArea } from '@/components/molecules/ChartSelectArea';
 import { AreaWithoutAreaType } from '../Inequalities/inequalitiesHelpers';
-import {
-  allAreaTypes,
-  HierarchyNameTypes,
-} from '@/lib/areaFilterHelpers/areaType';
 import { areaCodeForEngland } from '@/lib/chartHelpers/constants';
 import { StyleChartWrapper } from '@/components/styles/viewPlotStyles/styleChartWrapper';
-
-const determineHeaderTitle = (
-  healthDataForAreaSelected?: HealthDataForArea,
-  areaTypeSelected?: string
-): string => {
-  if (!healthDataForAreaSelected) return '';
-
-  const hierarchyName = allAreaTypes.find(
-    (areaType) => areaType.key === areaTypeSelected
-  );
-
-  const titleTypeText =
-    hierarchyName?.hierarchyName === HierarchyNameTypes.NHS
-      ? 'Registered'
-      : 'Resident';
-
-  const year = sortHealthDataPointsByDescendingYear(
-    healthDataForAreaSelected.healthData
-  )[0].year;
-
-  return `${titleTypeText} population profile for ${healthDataForAreaSelected?.areaName} ${year}`;
-};
-
-export function determinePopulationDataForArea(
-  populationDataForAllAreas: PopulationDataForArea[],
-  areaToFind?: string
-) {
-  if (!areaToFind) return populationDataForAllAreas[0];
-
-  return populationDataForAllAreas.find((data) => data.areaCode === areaToFind);
-}
+import {
+  determineHeaderTitle,
+  determinePopulationDataForArea,
+} from './populationPyramidHelpers';
 
 interface PyramidPopulationChartViewProps {
   healthDataForAreas: HealthDataForArea[];
@@ -108,6 +76,7 @@ export const PopulationPyramidWithTable = ({
 
   const populationDataForSelectedArea = determinePopulationDataForArea(
     areasForPopulationData,
+    availableAreas,
     populationAreaSelected
   );
 
