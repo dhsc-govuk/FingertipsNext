cd0ea1b2a3df1922adb54afbca546655e727a706
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-05-15 14:13:32 +0100

    update soak

--- frontend/fingertips-frontend/k6_performance_tests/soak_script.ts
+++ frontend/fingertips-frontend/k6_performance_tests/soak_script.ts
@@ -26,8 +26,8 @@ const ENDPOINTS = [
 
 export const options = {
   stages: [
-    { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes
-    { duration: '8h', target: 100 }, // stay at 100 users for 8 hours
+    { duration: '5m', target: 20 }, // traffic ramp-up from 1 to 20 users over 5 minutes - anything over 20 causes threshold errors even at short duration
+    { duration: '8h', target: 20 }, // stay at 20 users for 8 hours - anything over 20 causes threshold errors even at short duration
     { duration: '5m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
