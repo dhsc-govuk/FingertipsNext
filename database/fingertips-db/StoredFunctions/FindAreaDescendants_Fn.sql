dcb94acbf23a68ed694d4f2b64d39b53168b2645
Obaro I. Johnson <obaro.johnson1@nhs.net>
2025-05-16 18:57:00 +0100

    Changed procedure to a function

---
+++ database/fingertips-db/StoredFunctions/FindAreaDescendants_Fn.sql
@@ -0,0 +1,105 @@
+/********************************************************************************************
+-- Script Name:     FindAreaDescendants_Fn.sql
+-- Description:     This script creates the table function [dbo].[FindAreaDescendants]
+                    that finds descendant areas for a given AreaType and Ancestor AreaCode
+                    using a recursive common table expression (CTE).
+
+-- Parameters:
+    @RequestedAreaType          : The AreaTypeKey of the descendant areas to find.
+    @RequestedAncestorAreaCode  : The AreaCode of the ancestor area to start the search from.
+    @MaxDepth                   : Optional max recursion depth (default = 10 if NULL).
+
+-- Returns:
+    A table of areas that are descendants of the specified ancestor and match the specified AreaType.
+
+-- Usage:
+    SELECT * FROM dbo.FindAreaDescendants('LocalAuthority', 'E09000001', 5);
+
+-- Change Log:
+    Date        Author        Description
+    ----------  ------------  -------------------------------------------------------------------
+    [2025-05-16]    [Obaro I. Johnson]   changed from procedure to function
+*********************************************************************************************/
+-- IF OBJECT_ID(N'dbo.FindAreaDescendants_Fn', N'IF') IS NOT NULL
+--     DROP FUNCTION [dbo].[FindAreaDescendants_Fn];
+-- GO
+
+
+CREATE FUNCTION [dbo].[FindAreaDescendants_Fn]( 
+    @RequestedAreaType VARCHAR(50),
+    @RequestedAncestorAreaCode VARCHAR(20),
+    @MaxDepth INT -- The depth to which the recursion should go
+)
+RETURNS TABLE
+AS
+RETURN (
+    WITH 
+    ParentAreaByCodeWithDifferentAreaType AS (
+        SELECT *
+        FROM Areas.Areas area
+        WHERE area.AreaCode = @RequestedAncestorAreaCode
+          AND area.AreaTypeKey <> @RequestedAreaType
+    ),
+
+    TargetAreaType AS (
+        SELECT *
+        FROM Areas.AreaTypes AT
+        WHERE AT.AreaTypeKey = @RequestedAreaType
+    ),
+
+    AreaRelationshipsHierarchy_CTE (
+        AreaKey,
+        AreaCode,
+        AreaName,
+        AreaTypeKey,
+        AreaLevel,
+        ParentAreaKey,
+        Depth
+    ) AS (
+        -- Anchor part of the recursive CTE
+        SELECT 
+            ar.ChildAreaKey AS AreaKey,
+            a.AreaCode,
+            a.AreaName,
+            a.AreaTypeKey,
+            at2.[Level] AS AreaLevel,
+            ar.ParentAreaKey,
+            0 AS Depth
+        FROM Areas.AreaRelationships ar
+        JOIN Areas.Areas a ON a.AreaKey = ar.ChildAreaKey
+        JOIN ParentAreaByCodeWithDifferentAreaType sa ON ar.ParentAreaKey = sa.AreaKey
+        JOIN Areas.AreaTypes at2 ON at2.AreaTypeKey = a.AreaTypeKey
+
+        UNION ALL
+
+        -- Recursive step to find descendants
+        SELECT 
+            ar.ChildAreaKey AS AreaKey,
+            a.AreaCode,
+            a.AreaName,
+            a.AreaTypeKey,
+            at2.[Level] AS AreaLevel,
+            ar.ParentAreaKey,
+            recursive_cte.Depth + 1 AS Depth
+        FROM Areas.AreaRelationships ar
+        JOIN Areas.Areas a ON a.AreaKey = ar.ChildAreaKey
+        JOIN Areas.AreaTypes at2 ON at2.AreaTypeKey = a.AreaTypeKey
+        INNER JOIN AreaRelationshipsHierarchy_CTE recursive_cte ON recursive_cte.AreaKey = ar.ParentAreaKey
+        CROSS JOIN TargetAreaType tat
+        WHERE at2.[Level] <= tat.[Level]
+          AND at2.HierarchyType = tat.HierarchyType
+          AND recursive_cte.Depth <= ISNULL(@MaxDepth, 10)
+    )
+
+    -- Final result selection
+    SELECT DISTINCT 
+        rc.AreaKey,
+        rc.AreaCode,
+        rc.AreaName,
+        tat.AreaTypeKey,
+        tat.[Level] AS Level,
+        tat.HierarchyType,
+        tat.AreaTypeName
+    FROM AreaRelationshipsHierarchy_CTE rc
+    JOIN TargetAreaType tat ON rc.AreaTypeKey = tat.AreaTypeKey
+);
