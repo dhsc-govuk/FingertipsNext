ed377d6a91be949847362d18a0c4ceee4c36c185
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-05-20 15:43:31 +0100

    implement input field wrapper

---
+++ frontend/fingertips-frontend/components/atoms/InputField/index.tsx
@@ -0,0 +1,53 @@
+import { InputField as GovukInputField } from 'govuk-react';
+import { InputFieldProps as GovukInputFieldProps } from '@govuk-react/input-field';
+import { ChangeEventHandler, useState } from 'react';
+import { spacing } from '@govuk-react/lib';
+import styled from 'styled-components';
+import { CharacterCount } from '../CharacterCount';
+
+interface InputFieldProps extends GovukInputFieldProps {
+  characterLimit?: number;
+  thresholdPercentage?: number;
+}
+
+const StyledInputField = styled(GovukInputField)(
+  spacing.withWhiteSpace({ marginBottom: 1 })
+);
+
+const StyledDiv = styled.div({ paddingBottom: '30px' });
+
+export function InputField(props: Readonly<InputFieldProps>) {
+  const [inputTextLength, setInputTextLength] = useState(
+    props.input?.defaultValue?.toString().length ?? 0
+  );
+  const existingOnChangeHandler = props.input?.onChange;
+  const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (existingOnChangeHandler !== undefined) {
+      existingOnChangeHandler(e);
+    }
+
+    setInputTextLength(e.target.value.length);
+  };
+
+  return (
+    <StyledDiv>
+      <StyledInputField
+        {...props}
+        input={{
+          ...props.input,
+          maxLength: props.characterLimit ?? props.input?.maxLength,
+          onChange: props.characterLimit
+            ? onChangeHandler
+            : existingOnChangeHandler,
+        }}
+      >
+        {props.children}
+      </StyledInputField>
+      <CharacterCount
+        textLength={inputTextLength}
+        characterLimit={props.characterLimit}
+        thresholdPercentage={props.thresholdPercentage}
+      />
+    </StyledDiv>
+  );
+}
