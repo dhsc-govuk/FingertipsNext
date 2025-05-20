54fcfd80840b1a17bc5b2cae740c6e7e52e5b453
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-05-20 15:03:50 +0100

    pr comments

--- frontend/fingertips-frontend/lib/areaFilterHelpers/generateAreaDisplayString.test.ts
+++ frontend/fingertips-frontend/lib/areaFilterHelpers/generateAreaDisplayString.test.ts
@@ -1,9 +1,9 @@
 import { AreaTypeKeys, gpsAreaType, nhsRegionsAreaType } from './areaType';
-import { formatAreaName } from './formatAreaName';
+import { generateAreaDisplayString } from './generateAreaDisplayString';
 
-describe('formatAreaName', () => {
+describe('generateAreaDisplayString', () => {
   it('should return the areaCode and areaName provided when the areaTypeKey is gps', () => {
-    const formattedAreaName = formatAreaName(
+    const formattedAreaName = generateAreaDisplayString(
       'A001',
       'Some name',
       gpsAreaType.key as AreaTypeKeys,
@@ -14,11 +14,21 @@ describe('formatAreaName', () => {
   });
 
   it('should just return the areaName provided when the areaTypeKey is not gps', () => {
-    const formattedAreaName = formatAreaName(
+    const formattedAreaName = generateAreaDisplayString(
       'A001',
       'Some name',
       nhsRegionsAreaType.key as AreaTypeKeys,
       'Some postcode'
+    );
+
+    expect(formattedAreaName).toEqual('Some name Some postcode');
+  });
+
+  it('should return nothing when postcode is not provided', () => {
+    const formattedAreaName = generateAreaDisplayString(
+      'A001',
+      'Some name',
+      nhsRegionsAreaType.key as AreaTypeKeys
     );
 
     expect(formattedAreaName).toEqual('Some name');
--- frontend/fingertips-frontend/lib/areaFilterHelpers/generateAreaDisplayString.ts
+++ frontend/fingertips-frontend/lib/areaFilterHelpers/generateAreaDisplayString.ts
@@ -1,12 +1,12 @@
 import { gpsAreaType } from './areaType';
 
-export function formatAreaName(
+export function generateAreaDisplayString(
   areaCode: string,
   areaName: string,
   areaTypeKey: string,
   postcode?: string
 ): string {
   return areaTypeKey.toLowerCase() === gpsAreaType.key
-    ? `${areaCode} - ${areaName} ${postcode ?? ''}`
-    : areaName;
+    ? `${areaCode} - ${areaName}${postcode ? ' ' + postcode : ''}`
+    : areaName + `${postcode ? ' ' + postcode : ''}`;
 }
