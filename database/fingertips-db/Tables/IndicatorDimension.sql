c21fcee4c459238538b52927223c3faf3cd980fe
WolfieKnee <david.wolfson@bjss.com>
2025-05-22 09:44:04 +0100

    Allow NULL on YearType

--- database/fingertips-db/Tables/IndicatorDimension.sql
+++ database/fingertips-db/Tables/IndicatorDimension.sql
@@ -8,7 +8,7 @@ CREATE TABLE [dbo].[IndicatorDimension](
 	[BenchmarkComparisonMethod] [nvarchar](255) NULL,        --e.g. RAG or Quntiles
 	[StartDate] [datetime2](7) NOT NULL,					--The start data that this row was relevant for, to support slowly changing dimensions if an indicator changes over time
 	[EndDate] [datetime2](7) NOT NULL,
-    [YearType] [nvarchar](255) NOT NULL 
+    [YearType] [nvarchar](255) NULL 
 CONSTRAINT [PK_IndicatorDimension] PRIMARY KEY CLUSTERED 
 (
 	[IndicatorKey] ASC
