27d0de72ed2e3197ea9cde43c2d6b1993d418168
Gary.Stanley <gary.stanley@bjss.com>
2025-05-22 19:26:20 +0100

    no longer a distinction between allAreas and availableAreas

--- frontend/fingertips-frontend/components/views/OneIndicatorTwoOrMoreAreasView/index.tsx
+++ frontend/fingertips-frontend/components/views/OneIndicatorTwoOrMoreAreasView/index.tsx
@@ -8,7 +8,6 @@ import {
   determineBenchmarkRefType,
   getIndicatorData,
 } from '@/lib/ViewsHelpers';
-import { IndicatorWithHealthDataForArea } from '@/generated-sources/ft-api-client';
 
 export default async function OneIndicatorTwoOrMoreAreasView({
   selectedIndicatorsData,
@@ -48,29 +47,23 @@ export default async function OneIndicatorTwoOrMoreAreasView({
 
   const benchmarkRefType = determineBenchmarkRefType(lineChartAreaSelected);
 
-  const indicatorDataAllAreas = await getIndicatorData(
+  const indicatorData = await getIndicatorData(
     indicatorsAndAreas,
     benchmarkRefType,
     areaCodes.length > 2
   );
 
-  const indicatorDataAvailableAreas = {
-    ...indicatorDataAllAreas,
-    areaHealthData: indicatorDataAllAreas.areaHealthData,
-  } satisfies IndicatorWithHealthDataForArea;
-
   const indicatorMetadata = selectedIndicatorsData?.[0];
   return (
     <ViewsWrapper
       areaCodes={areaCodes}
-      indicatorsDataForAreas={[indicatorDataAvailableAreas]}
+      indicatorsDataForAreas={[indicatorData]}
     >
       <OneIndicatorTwoOrMoreAreasViewPlots
         areaCodes={areaCodes}
-        indicatorData={indicatorDataAvailableAreas}
+        indicatorData={indicatorData}
         searchState={searchState}
         indicatorMetadata={indicatorMetadata}
-        indicatorDataAllAreas={indicatorDataAllAreas}
       />
     </ViewsWrapper>
   );
