57020edbb5b02542b0b5323a19d0346a007a0dea
stuc-bjss <stuart.craigen@bjss.com>
2025-05-23 13:22:16 +0100

    DHSCFT-779: export thematic map

--- frontend/fingertips-frontend/lib/chartHelpers/chartHelpers.ts
+++ frontend/fingertips-frontend/lib/chartHelpers/chartHelpers.ts
@@ -150,6 +150,7 @@ export function getMostRecentData(
 
 export async function loadHighchartsModules(callback: () => void) {
   await import('highcharts/modules/exporting');
+  await import('highcharts/modules/map');
   await import('highcharts/highcharts-more').then(callback);
 }
 
