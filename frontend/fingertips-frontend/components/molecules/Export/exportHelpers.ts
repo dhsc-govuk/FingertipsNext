3652c0b8cc65d8b1d4bd767cffab34b53b6bc5d8
stuc-bjss <stuart.craigen@bjss.com>
2025-05-27 18:34:21 +0100

    DHSCFT-779: export thematic map (#598)
    Signed-off-by: stuc-bjss <stuart.craigen@bjss.com>
    Co-authored-by: GitHub Actions Bot <actions@github.com>

--- frontend/fingertips-frontend/components/molecules/Export/exportHelpers.ts
+++ frontend/fingertips-frontend/components/molecules/Export/exportHelpers.ts
@@ -54,7 +54,13 @@ export const getSvgFromOptions = (options: Highcharts.Options): string => {
   container.style.display = 'none';
   document.body.appendChild(container);
 
-  const chart = Highcharts.chart(container, options);
+  // type issue with mapChart existing (or not) on the Highcharts object - it does
+  // maybe because the loading of highcharts modules is done before this code is executed
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const constructor = options.mapView ? Highcharts.mapChart : Highcharts.chart;
+
+  const chart = constructor(container, options);
   const svg = chart.getSVG();
 
   chart.destroy();
