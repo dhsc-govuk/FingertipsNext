df1754016546f887ccb4d5a0f18cdd294a9ea5da
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-06-03 17:15:40 +0100

    Update header link to use a Next Link

--- frontend/fingertips-frontend/components/molecules/Header/index.tsx
+++ frontend/fingertips-frontend/components/molecules/Header/index.tsx
@@ -2,12 +2,35 @@
 
 import { Link } from '@/components/atoms/Link';
 import { contactEmailLink } from '@/lib/links';
+import { GovukColours } from '@/lib/styleHelpers/colours';
 import { Main, PhaseBanner, TopNav } from 'govuk-react';
+import NextLink from 'next/link';
 import styled from 'styled-components';
 
 const ZeroPaddingMain = styled(Main)`
   padding: 0px;
 `;
+
+const TopNavLink = styled(NextLink)({
+  'color': GovukColours.White,
+  'textDecoration': 'none',
+  'textDecorationSkipInk': 'none',
+  'borderBottom': '1px solid transparent',
+  'fontWeight': 700,
+  'lineHeight': 1,
+  ':hover': {
+    borderBottomColor: GovukColours.White,
+  },
+  'display': 'inline-block',
+  ':focus': {
+    'color': GovukColours.Black,
+    'backgroundColor': GovukColours.Yellow,
+    'outline': `3px solid ${GovukColours.Yellow}`,
+    ':hover': {
+      borderBottomColor: GovukColours.Black,
+    },
+  },
+});
 
 const ServiceTitle = styled('span')({
   fontWeight: '700',
@@ -18,9 +41,9 @@ export function FTHeader() {
     <header>
       <TopNav
         serviceTitle={
-          <TopNav.NavLink href="/">
+          <TopNavLink href="/">
             <ServiceTitle>Access public health data</ServiceTitle>
-          </TopNav.NavLink>
+          </TopNavLink>
         }
       />
       <ZeroPaddingMain>
