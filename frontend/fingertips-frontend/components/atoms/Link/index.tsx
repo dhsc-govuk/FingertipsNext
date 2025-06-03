b1bcf7706ce50f4b3b5b2b58728de83015912b7d
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-06-03 17:06:14 +0100

    Simplify Link component

--- frontend/fingertips-frontend/components/atoms/Link/index.tsx
+++ frontend/fingertips-frontend/components/atoms/Link/index.tsx
@@ -2,9 +2,9 @@ import { link } from '@govuk-react/lib';
 import NextLink from 'next/link';
 import styled from 'styled-components';
 
-export const Link = styled(NextLink)({
-  ...link.common()[0],
-  ...link.common()[1],
-  ...link.styleDefault,
-  ...link.printFriendly,
-});
+export const Link = styled(NextLink)(
+  link.common()[0],
+  link.common()[1],
+  link.styleDefault,
+  link.printFriendly
+);
