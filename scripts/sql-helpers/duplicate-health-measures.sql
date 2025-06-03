ea7e8f9398c45a319371a72688da8f7f51f2d203
Howard May <howard.may@bjss.com>
2025-06-03 11:59:58 +0100

    DHSCFT-764: Added sql helper

---
+++ scripts/sql-helpers/duplicate-health-measures.sql
@@ -0,0 +1,48 @@
+WITH 
+  groupedHealthMeasures AS (
+SELECT
+	count(*) as DuplicateCount,
+	id.IndicatorId,
+	ad.Code,
+	hm.Year,
+	ad2.Name as ageName,
+	sd.Name as sexName,
+	dd.Name as imdName
+FROM
+	dbo.HealthMeasure hm
+JOIN 
+	dbo.AreaDimension ad 
+ON
+	ad.AreaKey = hm.AreaKey
+JOIN
+	dbo.IndicatorDimension id 
+ON
+	id.IndicatorKey = hm.IndicatorKey
+JOIN 
+	dbo.SexDimension sd 
+ON
+	sd.SexKey = hm.SexKey
+JOIN
+	dbo.AgeDimension ad2 
+ON
+	ad2.AgeKey = hm.AgeKey
+JOIN
+	dbo.DeprivationDimension dd 
+ON
+	dd.DeprivationKey = hm.DeprivationKey
+GROUP BY 
+	id.IndicatorId ,
+	ad.Code,
+	hm.[Year],
+	ad2.AgeKey,
+	ad2.Name,
+	sd.Name,
+    dd.DeprivationKey,
+	dd.Name
+	)
+SELECT
+	*
+FROM
+	groupedHealthMeasures
+WHERE
+	DuplicateCount > 1
