eb530302721bd762874bf986d4f661bdfaa8a788
HowardMayBjss <howard.may@bjss.com>
2025-06-09 10:01:08 +0100

    DHSCFT-876: WIP

---
+++ api/DHSC.FingertipsNext.Modules.HealthData.Schemas/DatePeriod.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DHSC.FingertipsNext.Modules.HealthData.Schemas;
+
+/// <summary>
+///     Date period and type
+/// </summary>
+public class DatePeriod
+{  
+    [JsonPropertyName("type")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public DatePeriodType PeriodType { get; init; }
+
+    [JsonPropertyName("from")]
+    public DateOnly From { get; init; }
+
+    [JsonPropertyName("to")]
+    public DateOnly To { get; init; }
+}
