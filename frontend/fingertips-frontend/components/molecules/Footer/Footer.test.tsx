264c217a6be3d93328b50c124cbc96adc540378d
Fawad Ali <alibombaye@users.noreply.github.com>
2025-06-11 13:37:07 +0100

    additional tests

--- frontend/fingertips-frontend/components/molecules/Footer/Footer.test.tsx
+++ frontend/fingertips-frontend/components/molecules/Footer/Footer.test.tsx
@@ -14,6 +14,12 @@ describe('FTFooter', () => {
     expect(footer).toHaveClass('chart-page-footer');
   });
 
+  it('should not have className "chart-page-footer" when chartPage is false', () => {
+    render(<FTFooter tag="vXYZ" hash="ABCD" chartPage={false} />);
+    const footer = screen.getByRole('contentinfo');
+    expect(footer).not.toHaveClass('chart-page-footer');
+  });
+
   it('should render the project version', () => {
     render(<FTFooter tag={'vXYZ'} hash={'ABCD'} />);
     const version = screen.getByTestId('project-version');
--- frontend/fingertips-frontend/components/molecules/Header/Header.test.tsx
+++ frontend/fingertips-frontend/components/molecules/Header/Header.test.tsx
@@ -17,6 +17,14 @@ describe('Header', () => {
     expect(header).toHaveClass('chart-page-header');
   });
 
+  it('should not have className "chart-page-header" when chartPage is false', () => {
+    render(<FTHeader chartPage={false} />);
+
+    const header = screen.getByRole('banner');
+
+    expect(header).not.toHaveClass('chart-page-header');
+  });
+
   it('should render Access public health data as a link', async () => {
     render(<FTHeader />);
     const returnToHomePageLink = screen.getByRole('link', {
