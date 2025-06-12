665a90c4881518227433795f3f218308cece3263
WolfieKnee <david.wolfson@bjss.com>
2025-06-12 16:16:38 +0100

    register new module

--- api/DHSC.FingertipsNext.Modules.DataManager/DataManagementModule.cs
+++ api/DHSC.FingertipsNext.Modules.DataManager/DataManagementModule.cs
@@ -1,8 +1,19 @@
-﻿// using DHSC.FingertipsNext.Monolith;
+﻿using DHSC.FingertipsNext.Modules.DataManagement.Repository;
+using DHSC.FingertipsNext.Modules.DataManagement.Service;
+using DHSC.FingertipsNext.Monolith;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DHSC.FingertipsNext.Modules.DataManagement;
 
-public class DataManagementModule 
+public class DataManagementModule : AbstractMonolithModule, IMonolithModule
 {
-    public string ModuleName => "DataManagement";
+    public override string ModuleName => "DataManagement";
+
+    public override void RegisterModule(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDataManagementService, DataManagementService>();
+        services.AddTransient<IDataManagementRepository, DataManagementRepository>();
+    }
+    
 }
