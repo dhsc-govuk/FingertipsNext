358f588defeee50742b683f21af9ba677d7dc6b7
Evan Harris <evan.harris1@nhs.net>
2025-06-12 09:04:36 +0100

    Fixing unit test

--- api/DHSC.FingertipsNext.Modules.Area.UnitTests/Fakers/AreaModelFaker.cs
+++ api/DHSC.FingertipsNext.Modules.Area.UnitTests/Fakers/AreaModelFaker.cs
@@ -12,6 +12,7 @@ internal sealed class AreaModelFaker : Faker<AreaModel>
         RuleFor(a => a.AreaName, f => f.Lorem.Sentence(4));
         RuleFor(a => a.AreaCode, f => f.Random.Guid().ToString());
         RuleFor(a => a.AreaType, f => Fake.AreaTypeModel);
+        RuleFor(a => a.AreaTypeKey, f => Fake.AreaTypeModel.AreaTypeKey);
         RuleFor(a => a.Children, f => new List<AreaModel>());
         RuleFor(a => a.Parents, f => new List<AreaModel>());
     }
