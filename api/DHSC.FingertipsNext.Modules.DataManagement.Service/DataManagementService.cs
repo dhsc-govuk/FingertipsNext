a9226e40eaaa5464ef8e363d606b6c8d727b5b51
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2025-06-13 14:43:44 +0100

    Feature/dhscft 901 create data mangement api module (#664)

---
+++ api/DHSC.FingertipsNext.Modules.DataManagement.Service/DataManagementService.cs
@@ -0,0 +1,12 @@
+﻿using DHSC.FingertipsNext.Modules.DataManagement.Repository;
+
+namespace DHSC.FingertipsNext.Modules.DataManagement.Service;
+
+public class DataManagementService(IDataManagementRepository dataManagementRepository) : IDataManagementService
+{
+    public string SayHelloToRepository()
+    {
+        var repositorySays = dataManagementRepository.SayHello();
+        return "The Repository says: " + repositorySays;
+    }
+}
---
+++ api/DHSC.FingertipsNext.Modules.DataManager.UnitTests/Repository/DataManagementRepositoryTests.cs
@@ -0,0 +1,20 @@
+using DHSC.FingertipsNext.Modules.DataManagement.Repository;
+using Shouldly;
+
+namespace DHSC.FingertipsNext.Modules.DataManagement.UnitTests.Repository;
+
+public class DataManagementRepositoryTests
+{
+    private readonly DataManagementRepository _repository;
+
+    public DataManagementRepositoryTests()
+    {
+        _repository = new DataManagementRepository();
+    }
+    [Fact]
+    public void SayHelloTest()
+    {
+        // assert
+        _repository.SayHello().ShouldBe("I'm a Repository");
+    }
+}
