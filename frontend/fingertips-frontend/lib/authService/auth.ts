c0dc5679a5ee772e9c9a6a45480f5f4ec8a6b6b8
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-06-16 16:27:29 +0100

    add other MS providers

--- frontend/fingertips-frontend/lib/authService/auth.ts
+++ frontend/fingertips-frontend/lib/authService/auth.ts
@@ -1,12 +1,25 @@
 import NextAuth from 'next-auth';
 import MicrosoftEntraID from 'next-auth/providers/microsoft-entra-id';
+import AzureADB2C from 'next-auth/providers/azure-ad-b2c';
+import AzureADProvider from 'next-auth/providers/azure-ad';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
   providers: [
+    AzureADB2C({
+      clientId: process.env.AUTH_AZURE_AD_B2C_CLIENT_ID,
+      clientSecret: process.env.AUTH_AZURE_AD_B2C_CLIENT_SECRET,
+      issuer: process.env.AUTH_AZURE_AD_B2C_ISSUER,
+    }),
+
     MicrosoftEntraID({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
       issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+    }),
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
     }),
   ],
 });
