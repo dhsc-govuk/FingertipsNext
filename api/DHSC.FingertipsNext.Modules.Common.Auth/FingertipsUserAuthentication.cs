22396d49d1e2edc3f680dc15ae99c1d7fa95bf38
Evan Harris <evan.harris1@nhs.net>
2025-06-17 10:41:08 +0100

    Renaming and adding user authentication middleware builder

---
+++ api/DHSC.FingertipsNext.Modules.Common.Auth/FingertipsUserAuthentication.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DHSC.FingertipsNext.Modules.Common.Auth
+{
+    public static class FingertipsUserAuthentication
+    {
+        public static IServiceCollection AddFingertipsUserAuthentication(this IServiceCollection collection)
+        {
+            collection.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(jwtOptions =>
+               {
+                   jwtOptions.Authority = "https://{--your-authority--}";
+                   jwtOptions.Audience = "https://{--your-audience--}";
+               });
+
+            return collection;
+        }
+
+    }
+}
