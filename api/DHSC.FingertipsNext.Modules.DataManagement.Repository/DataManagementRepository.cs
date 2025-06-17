ea05c4ee30ddfd8e27110ce81073a55c2ede740f
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2025-06-17 17:00:50 +0100

    scaffold

--- api/DHSC.FingertipsNext.Modules.DataManagement.Repository/DataManagementRepository.cs
+++ api/DHSC.FingertipsNext.Modules.DataManagement.Repository/DataManagementRepository.cs
@@ -1,9 +1,30 @@
-﻿namespace DHSC.FingertipsNext.Modules.DataManagement.Repository;
+﻿using DHSC.FingertipsNext.Modules.DataManagement.Repository.Models;
 
-public class DataManagementRepository : IDataManagementRepository
+namespace DHSC.FingertipsNext.Modules.DataManagement.Repository;
+
+public class DataManagementRepository(DataManagementDbContext dataManagementDbContext) : IDataManagementRepository
 {
+    
+    
+    private readonly DataManagementDbContext _dbContext =
+        dataManagementDbContext ?? throw new ArgumentNullException(nameof(dataManagementDbContext));
+    
     public string SayHello()
     {
         return "I'm a Repository";
+    }
+
+    /// <summary>
+    ///     Will add the given batch to the batch table
+    /// </summary>
+    /// <param name="batch"></param>
+    /// <returns>TODO: Return the batch?</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Task<BatchModel> AddBatchAsync(BatchModel batch)
+    {
+        var model =  _dbContext.Batch
+            .AddAsync(batch);
+        
+        throw new NotImplementedException();
     }
 }
