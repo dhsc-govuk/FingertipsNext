2216e450d17498776da65a42a477570670c42746
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2025-06-18 14:49:54 +0100

    BREAKING WIP

--- api/DHSC.FingertipsNext.Modules.DataManagement.Repository/IDataManagementRepository.cs
+++ api/DHSC.FingertipsNext.Modules.DataManagement.Repository/IDataManagementRepository.cs
@@ -1,6 +1,9 @@
+using DHSC.FingertipsNext.Modules.DataManagement.Repository.Models;
+
 namespace DHSC.FingertipsNext.Modules.DataManagement.Repository;
 
 public interface IDataManagementRepository
 {
     public string SayHello();
+    Task<BatchModel> AddBatchAsync(BatchModel batch);
 }
--- api/DHSC.FingertipsNext.Modules.DataManager.UnitTests/Repository/DataManagementRepositoryTests.cs
+++ api/DHSC.FingertipsNext.Modules.DataManager.UnitTests/Repository/DataManagementRepositoryTests.cs
@@ -1,20 +1,46 @@
 using DHSC.FingertipsNext.Modules.DataManagement.Repository;
+using Microsoft.EntityFrameworkCore;
 using Shouldly;
 
 namespace DHSC.FingertipsNext.Modules.DataManagement.UnitTests.Repository;
 
-public class DataManagementRepositoryTests
+public class DataManagementRepositoryTests : IDisposable
 {
-    private readonly DataManagementRepository _repository;
+    private readonly DataManagementDbContext _dbContext;
+    private DataManagementRepository _dataManagementRepository;
 
     public DataManagementRepositoryTests()
     {
-        _repository = new DataManagementRepository();
+        _dbContext = new DataManagementDbContext();
+        _dataManagementRepository = new DataManagementRepository(_dbContext);
     }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _dbContext.Dispose();
+        }
+    }
+
+    
     [Fact]
     public void SayHelloTest()
     {
         // assert
-        _repository.SayHello().ShouldBe("I'm a Repository");
+        _dataManagementRepository.SayHello().ShouldBe("I'm a Repository");
+    }
+    [Fact]
+    public void RepositoryInitialisationShouldThrowErrorIfNullDBContextIsProvided()
+    {
+        var act = () => _dataManagementRepository = new DataManagementRepository(null!);
+
+        act.ShouldThrow<ArgumentNullException>()
+            .Message.ShouldBe("Value cannot be null. (Parameter 'healthDataDbContext')");
     }
 }
