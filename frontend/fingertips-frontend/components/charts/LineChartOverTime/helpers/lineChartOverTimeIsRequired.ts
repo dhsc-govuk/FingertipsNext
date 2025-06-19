bc3dbd99897301622e0ff16d90273fb3e86a2042
Stuart <stuart.craigen@bjss.com>
2025-06-19 10:48:39 +0100

    DHSCFT-1008: removed comments

--- frontend/fingertips-frontend/components/charts/LineChartOverTime/helpers/lineChartOverTimeIsRequired.ts
+++ frontend/fingertips-frontend/components/charts/LineChartOverTime/helpers/lineChartOverTimeIsRequired.ts
@@ -18,13 +18,12 @@ export const lineChartOverTimeIsRequired = (searchState: SearchStateParams) => {
     areaCodes?.at(0) === areaCodeForEngland &&
     areaTypeSelected === 'england'
   ) {
-    // yes if ONLY england
     return true;
   }
 
-  if (groupAreaSelected === ALL_AREAS_SELECTED) return false; // not if all areas
-  if (areaCodes?.length > 2) return false; // not if more than 2 areas
-  if (indicatorIds?.length !== 1) return false; // not if we don't have exactly 1 indicator
+  if (groupAreaSelected === ALL_AREAS_SELECTED) return false;
+  if (areaCodes?.length > 2) return false;
+  if (indicatorIds?.length !== 1) return false;
 
-  return Boolean(areaCodes.length > 0); // we must have some areas
+  return Boolean(areaCodes.length > 0);
 };
