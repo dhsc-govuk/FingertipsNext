643517e34eab6efcaeebee55561b83afaa3a5afa
Fawad Ali <alibombaye@users.noreply.github.com>
2025-06-19 16:29:49 +0100

    Merge main

--- frontend/fingertips-frontend/components/viewPlots/OneIndicatorTwoOrMoreAreasViewPlots/OneIndicatorTwoOrMoreAreasViewPlots.test.tsx
+++ frontend/fingertips-frontend/components/viewPlots/OneIndicatorTwoOrMoreAreasViewPlots/OneIndicatorTwoOrMoreAreasViewPlots.test.tsx
@@ -4,7 +4,6 @@ import { act, render, screen, waitFor, within } from '@testing-library/react';
 import { IndicatorWithHealthDataForArea } from '@/generated-sources/ft-api-client';
 import regionsMap from '@/components/organisms/ThematicMap/regions.json';
 import { ALL_AREAS_SELECTED } from '@/lib/areaFilterHelpers/constants';
-import { reactQueryClient } from '@/lib/reactQueryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { LoaderContext } from '@/context/LoaderContext';
 import { mockIndicatorDocument } from '@/mock/data/mockIndicatorDocument';
