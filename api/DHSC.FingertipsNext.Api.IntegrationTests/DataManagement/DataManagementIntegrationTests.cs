cade348cc68d158fa9748bf3fcf7a3a8013ffa52
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-06-20 17:02:50 +0100

    Fix formatting issues

--- api/DHSC.FingertipsNext.Api.IntegrationTests/DataManagement/DataManagementIntegrationTests.cs
+++ api/DHSC.FingertipsNext.Api.IntegrationTests/DataManagement/DataManagementIntegrationTests.cs
@@ -2,28 +2,27 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Shouldly;
 
-
 namespace DHSC.FingertipsNext.Api.IntegrationTests.DataManagement;
 
-public class DataManagementIntegrationTest: IClassFixture<WebApplicationFactory<Program>>, IDisposable
+public class DataManagementIntegrationTests : IClassFixture<WebApplicationFactory<Program>>, IDisposable
 {
     private const string TestDataDir = "TestData";
     private const string BlobName = "blobName";
-    
+
     private readonly HttpClient _apiClient;
     private readonly BlobContainerClient _blobContainerClient;
 
-    public DataManagementIntegrationTest(WebApplicationFactory<Program> factory)
+    public DataManagementIntegrationTests(WebApplicationFactory<Program> factory)
     {
         ArgumentNullException.ThrowIfNull(factory);
         _apiClient = factory.CreateClient();
-        
+
         var connectionString = Environment.GetEnvironmentVariable("UPLOAD_STORAGE_ACCOUNT_CONNECTION_STRING");
         ArgumentNullException.ThrowIfNull(connectionString);
-        
+
         var containerName = Environment.GetEnvironmentVariable("UPLOAD_STORAGE_CONTAINER_NAME");
         ArgumentNullException.ThrowIfNull(containerName);
-        
+
         var blobServiceClient = new BlobServiceClient(connectionString);
         _blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
     }
@@ -33,7 +32,7 @@ public class DataManagementIntegrationTest: IClassFixture<WebApplicationFactory<
         Dispose(true);
         GC.SuppressFinalize(this);
     }
-    
+
     protected virtual void Dispose(bool disposing)
     {
         _blobContainerClient.DeleteBlobIfExists(BlobName);
@@ -46,16 +45,16 @@ public class DataManagementIntegrationTest: IClassFixture<WebApplicationFactory<
         // Arrange
         var blobClient = _blobContainerClient.GetBlobClient(BlobName);
         var blobContentFilePath = Path.Combine(TestDataDir, "blobContent.json");
-        
+
         // Temporarily upload the file ourself, until the API is uploading files to Azure storage.
         await blobClient.UploadAsync(blobContentFilePath);
-        
+
         // Act
         var response = await _apiClient.GetAsync(new Uri("/data_management"));
-        
+
         // Assert
         response.EnsureSuccessStatusCode();
-        
+
         var blobResponse = await blobClient.DownloadContentAsync();
         var blobContent = blobResponse.Value.Content.ToArray();
         var localFileContent = await File.ReadAllBytesAsync(blobContentFilePath);
