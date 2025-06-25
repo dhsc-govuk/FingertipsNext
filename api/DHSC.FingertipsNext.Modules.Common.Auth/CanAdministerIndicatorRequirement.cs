245425d3eae61f01818d81c209a6cdbb02d9e6a4
Evan Harris <evan.harris1@nhs.net>
2025-06-25 17:52:49 +0100

    Removing unnecessary comment

--- api/DHSC.FingertipsNext.Modules.Common.Auth/CanAdministerIndicatorRequirement.cs
+++ api/DHSC.FingertipsNext.Modules.Common.Auth/CanAdministerIndicatorRequirement.cs
@@ -2,10 +2,6 @@
 
 namespace DHSC.FingertipsNext.Modules.Common.Auth
 {
-    /// <summary>
-    /// This policy determines whether a user is able to administer a specific indicator.
-    /// The policy must be evaluated via an inline method call to AuthorizeAsync in order to evaluate the indicator ID.
-    /// </summary>
     public class CanAdministerIndicatorRequirement : IAuthorizationRequirement
     {
         public const string Policy = "AdministerIndicatorPolicy";
