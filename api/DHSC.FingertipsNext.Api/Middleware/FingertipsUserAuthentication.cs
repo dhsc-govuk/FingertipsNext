31555cb793f48e64b8c8bad4d04e301d6833d3ec
Evan Harris <evan.harris1@nhs.net>
2025-06-25 17:45:11 +0100

    Stopping crash without ad settings

--- api/DHSC.FingertipsNext.Api/Middleware/FingertipsUserAuthentication.cs
+++ api/DHSC.FingertipsNext.Api/Middleware/FingertipsUserAuthentication.cs
@@ -10,18 +10,21 @@ namespace DHSC.FingertipsNext.Api.Middleware
     {
         public static IServiceCollection AddFingertipsUserAuth(this IServiceCollection collection, ConfigurationManager config)
         {
-            collection.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(config)
-                .EnableTokenAcquisitionToCallDownstreamApi()
-                .AddInMemoryTokenCaches();
-
-            collection.AddTransient<IAuthorizationHandler, ShouldBeAnAdministratorRequirementHandler>();
-
-            collection.AddAuthorization(options =>
+            if (config.GetSection("AzureAD").Value != null)
             {
-                options.AddPolicy(CanAdministerIndicatorRequirement.Policy, policy =>
-                    policy.Requirements.Add(new CanAdministerIndicatorRequirement()));
-            });
+                collection.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApi(config)
+                    .EnableTokenAcquisitionToCallDownstreamApi()
+                    .AddInMemoryTokenCaches();
+
+                collection.AddTransient<IAuthorizationHandler, ShouldBeAnAdministratorRequirementHandler>();
+
+                collection.AddAuthorization(options =>
+                {
+                    options.AddPolicy(CanAdministerIndicatorRequirement.Policy, policy =>
+                        policy.Requirements.Add(new CanAdministerIndicatorRequirement()));
+                });
+            }
 
             return collection;
         }
