4c682b87d7afca16eb00b4da8a64d072247982ad
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-06-26 11:23:56 +0100

    Get user-assigned managed identity client ID from and environment variable.

--- api/DHSC.FingertipsNext.Modules.DataManager/DataManagementModule.cs
+++ api/DHSC.FingertipsNext.Modules.DataManager/DataManagementModule.cs
@@ -21,18 +21,26 @@ public class DataManagementModule : AbstractMonolithModule, IMonolithModule
         var storageUri = configuration.GetValue<string>("UPLOAD_STORAGE_ACCOUNT_URI");
         ArgumentNullException.ThrowIfNull(storageUri);
 
+        var userAssignedManagedIdentityClientId =
+            configuration.GetValue<string>("USER_ASSIGNED_MANAGED_IDENTITY_CLIENT_ID");
+
         var storageAccountName = configuration.GetValue<string>("UPLOAD_STORAGE_ACCOUNT_NAME");
         var storageAccountKey = configuration.GetValue<string>("UPLOAD_STORAGE_ACCOUNT_KEY");
 
         services.AddAzureClients(clientBuilder =>
         {
-            if (!string.IsNullOrEmpty(storageAccountName) && !string.IsNullOrEmpty(storageAccountKey))
+            if (!string.IsNullOrEmpty(userAssignedManagedIdentityClientId))
+                clientBuilder.AddBlobServiceClient(new Uri(storageUri),
+                        new ManagedIdentityCredential(
+                            ManagedIdentityId.FromUserAssignedClientId(userAssignedManagedIdentityClientId)))
+                    .ConfigureOptions(options => { options.Retry.MaxRetries = 1; });
+            else if (!string.IsNullOrEmpty(storageAccountName) && !string.IsNullOrEmpty(storageAccountKey))
                 clientBuilder.AddBlobServiceClient(new Uri(storageUri),
                         new StorageSharedKeyCredential(storageAccountName, storageAccountKey))
                     .ConfigureOptions(options => { options.Retry.MaxRetries = 1; });
             else
-                clientBuilder.AddBlobServiceClient(new Uri(storageUri), new ManagedIdentityCredential())
-                    .ConfigureOptions(options => { options.Retry.MaxRetries = 1; });
+                throw new ArgumentException(
+                    "Either USER_ASSIGNED_MANAGED_IDENTITY_CLIENT_ID or UPLOAD_STORAGE_ACCOUNT_NAME and UPLOAD_STORAGE_ACCOUNT_KEY must be specified.");
         });
     }
 }
