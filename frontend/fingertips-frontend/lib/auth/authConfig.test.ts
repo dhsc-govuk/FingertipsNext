f2cb6c8b4ec2cec388cd8552f04003ecf2ca14e6
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-07-01 17:55:16 +0100

    update auth config unit tests

--- frontend/fingertips-frontend/lib/auth/authConfig.test.ts
+++ frontend/fingertips-frontend/lib/auth/authConfig.test.ts
@@ -2,8 +2,7 @@ import { AuthProvidersFactory } from '@/lib/auth/config';
 
 describe('build auth providers', () => {
   beforeEach(() => {
-    vi.stubEnv('NODE_ENV', 'development');
-    vi.stubEnv('AUTH_USE_MOCK', undefined);
+    vi.stubEnv('AUTH_USE_PASSWORD_MOCK', undefined);
     vi.stubEnv('AUTH_FTA_ID', undefined);
     vi.stubEnv('AUTH_FTA_SECRET', undefined);
     vi.stubEnv('AUTH_FTA_ISSUER', undefined);
@@ -11,23 +10,16 @@ describe('build auth providers', () => {
     AuthProvidersFactory.reset();
   });
 
-  it('should return config with zero providers if no env set', () => {
+  it('should return config with zero providers if no env vars set', () => {
     expect(AuthProvidersFactory.getProviders()).toHaveLength(0);
   });
 
-  it('should return config with one provider with id "credentials" if AUTH_USE_MOCK set and NODE_ENV equals development', () => {
+  it('should return config with one provider with id "credentials" if AUTH_USE_PASSWORD_MOCK set', () => {
     vi.stubEnv('NODE_ENV', 'development');
-    vi.stubEnv('AUTH_USE_MOCK', 'true');
+    vi.stubEnv('AUTH_USE_PASSWORD_MOCK', 'true');
 
     expect(AuthProvidersFactory.getProviders()).toHaveLength(1);
     expect(AuthProvidersFactory.getProviders()[0].id).toEqual('credentials');
-  });
-
-  it('should return config with zero providers if AUTH_USE_MOCK set and NODE_ENV equals production', () => {
-    vi.stubEnv('NODE_ENV', 'production');
-    vi.stubEnv('AUTH_USE_MOCK', 'true');
-
-    expect(AuthProvidersFactory.getProviders()).toHaveLength(0);
   });
 
   it('should return config with one provider with id "fta" if auth env vars set', () => {
@@ -39,20 +31,9 @@ describe('build auth providers', () => {
     expect(AuthProvidersFactory.getProviders()[0].id).toEqual('fta');
   });
 
-  it('should return config with one provider with id "fta" if auth env vars set and AUTH_USE_MOCK set and NOVE_ENV equals production', () => {
+  it('should return config with two providers with id "fta" if auth env vars set and AUTH_USE_PASSWORD_MOCK set', () => {
     vi.stubEnv('NODE_ENV', 'production');
-    vi.stubEnv('AUTH_USE_MOCK', 'true');
-    vi.stubEnv('AUTH_FTA_ID', 'foo');
-    vi.stubEnv('AUTH_FTA_SECRET', 'bar');
-    vi.stubEnv('AUTH_FTA_ISSUER', 'baz');
-
-    expect(AuthProvidersFactory.getProviders()).toHaveLength(1);
-    expect(AuthProvidersFactory.getProviders()[0].id).toEqual('fta');
-  });
-
-  it('should return config with two providers if auth env vars set and AUTH_USE_MOCK set and NOVE_ENV equals development', () => {
-    vi.stubEnv('NODE_ENV', 'development');
-    vi.stubEnv('AUTH_USE_MOCK', 'true');
+    vi.stubEnv('AUTH_USE_PASSWORD_MOCK', 'true');
     vi.stubEnv('AUTH_FTA_ID', 'foo');
     vi.stubEnv('AUTH_FTA_SECRET', 'bar');
     vi.stubEnv('AUTH_FTA_ISSUER', 'baz');
