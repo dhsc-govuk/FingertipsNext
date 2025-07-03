fb883f97918977f2b69f6e38984912ba9bc16a8b
Fawad Ali <alibombaye@users.noreply.github.com>
2025-07-03 16:46:49 +0100

    add comments for the types

--- frontend/fingertips-frontend/lib/timePeriodHelpers/getTimePeriodLabels.ts
+++ frontend/fingertips-frontend/lib/timePeriodHelpers/getTimePeriodLabels.ts
@@ -1,5 +1,9 @@
 import { format } from 'date-fns';
 
+/**
+ * The following types are temporary. 
+ * Once the swagger has been updated then these types will come from the generated code. 
+ */
 export const Frequency = {
   Annual: 'Annual',
   Monthly: 'Monthly',
