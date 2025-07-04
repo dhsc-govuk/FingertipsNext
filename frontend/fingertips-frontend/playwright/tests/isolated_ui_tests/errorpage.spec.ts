83f2e48225f2923e94a8f568369dda1d88276ec2
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-07-04 09:16:51 +0100

    correct fix for failing ui flake

--- frontend/fingertips-frontend/playwright/tests/isolated_ui_tests/errorpage.spec.ts
+++ frontend/fingertips-frontend/playwright/tests/isolated_ui_tests/errorpage.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '../../page-objects/pageFactory';
 import { ErrorAreaCode, ErrorIndicatorId } from '@/mock/ErrorTriggeringIds';
 
+const errorChartPageURL = `chart?si=Alzheimer&is=${ErrorIndicatorId}&ats=england&gts=england&gs=${ErrorAreaCode}`;
+
 // we are intentionally setting failOnUnhandledError to false as we are testing error handling in this file
 test.use({ failOnUnhandledError: false });
 
@@ -21,14 +23,12 @@ test.describe('Error page tests', () => {
     chartPage,
     axeBuilder,
   }) => {
-    await chartPage.page.goto(`/chart?is=`);
+    await chartPage.page.goto(errorChartPageURL);
 
     await test
       .expect(chartPage.errorPageTitle())
       .toContainText('Sorry, there is a problem with the service');
-    await chartPage.expectNoAccessibilityViolations(axeBuilder, [
-      'document-title',
-    ]);
+    await chartPage.expectNoAccessibilityViolations(axeBuilder);
   });
 
   test('Indicator page displays ErrorPage when API returns unexpected error', async ({
