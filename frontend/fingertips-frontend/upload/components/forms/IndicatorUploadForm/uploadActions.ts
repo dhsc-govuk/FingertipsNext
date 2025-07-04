d3602c5ac7e72c895c2b171b0d454b68994e92d9
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-07-04 09:20:20 +0100

    Removed validation of upload form

--- frontend/fingertips-frontend/upload/components/forms/IndicatorUploadForm/uploadActions.ts
+++ frontend/fingertips-frontend/upload/components/forms/IndicatorUploadForm/uploadActions.ts
@@ -14,71 +14,17 @@ export async function uploadFile(
   _prevState: ApiResponse | undefined,
   formData: FormData
 ): Promise<ApiResponse> {
-  const rawIndicatorId = formData.get('indicatorId');
-  if (!rawIndicatorId) {
-    return {
-      message: 'The indicator ID must be specified.',
-    };
-  }
   const indicatorId = Number(formData.get('indicatorId'));
-  if (isNaN(indicatorId)) {
-    return {
-      message: 'The indicator ID must be a number.',
-    };
-  }
-
-  const file = formData.get('file');
-  if (!(file instanceof File)) {
-    return { message: 'The provided file is not in the expected format.' };
-  }
-
-  const rawPublishDateYear = formData.get('publishDateYear');
-  if (!rawPublishDateYear) {
-    return {
-      message: 'The publish date year must be specified.',
-    };
-  }
-
-  const rawPublishDateMonth = formData.get('publishDateMonth');
-  if (!rawPublishDateMonth) {
-    return {
-      message: 'The publish date month must be specified.',
-    };
-  }
-
-  const rawPublishDateDay = formData.get('publishDateDay');
-  if (!rawPublishDateDay) {
-    return {
-      message: 'The publish date day must be specified.',
-    };
-  }
-
-  const publishDateYear = Number(rawPublishDateYear);
-  if (isNaN(publishDateYear)) {
-    return {
-      message: 'The publish date year must be a number.',
-    };
-  }
-
-  const publishDateMonth = Number(rawPublishDateMonth);
-  if (isNaN(publishDateMonth)) {
-    return {
-      message: 'The publish date month must be a number.',
-    };
-  }
-
-  const publishDateDay = Number(rawPublishDateDay);
-  if (isNaN(publishDateDay)) {
-    return {
-      message: 'The publish date day must be a number.',
-    };
-  }
+  const file = formData.get('file') as File;
+  const publishDateYear = Number(formData.get('publishDateYear'));
+  const publishDateMonth = Number(formData.get('publishDateMonth'));
+  const publishDateDay = Number(formData.get('publishDateDay'));
 
   const isoPublishDate = formatISO(
     new UTCDateMini(publishDateYear, publishDateMonth - 1, publishDateDay)
   );
-  // The ISO-8601 date string will be passed to the API when it is updated
-  // to accept it.
+  // The ISO-8601 date string will be passed to the API when it has been
+  // updated to accept it.
   console.log('Publish date: ', isoPublishDate);
 
   const indicatorApi = ApiClientFactory.getIndicatorsApiClient();
