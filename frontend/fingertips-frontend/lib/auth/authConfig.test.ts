974682a3e86ef293789fff53f8af97b99600125b
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-07-04 10:16:02 +0100

    add unit test for secret shim

--- frontend/fingertips-frontend/lib/auth/authConfig.test.ts
+++ frontend/fingertips-frontend/lib/auth/authConfig.test.ts
@@ -1,4 +1,4 @@
-import { AuthProvidersFactory } from '@/lib/auth/config';
+import { AuthProvidersFactory, buildAuthConfig } from '@/lib/auth/config';
 
 describe('build auth providers', () => {
   beforeEach(() => {
@@ -39,5 +39,26 @@ describe('build auth providers', () => {
     vi.stubEnv('AUTH_FTA_ISSUER', 'baz');
 
     expect(AuthProvidersFactory.getProviders()).toHaveLength(2);
+  });
+});
+
+describe('build config', () => {
+  it('should not populate the secret field if auth secret is set on env', () => {
+    vi.stubEnv('AUTH_SECRET', 'foobar');
+
+    const config = buildAuthConfig();
+
+    expect(config.secret).toBeUndefined();
+  });
+
+  it('should populate the secret field and log a warning if auth secret is not set on env', () => {
+    vi.stubEnv('AUTH_SECRET', undefined);
+
+    const consoleMock = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+    const config = buildAuthConfig();
+
+    expect(config.secret).not.toBeUndefined();
+    expect(consoleMock).toHaveBeenCalled();
   });
 });
