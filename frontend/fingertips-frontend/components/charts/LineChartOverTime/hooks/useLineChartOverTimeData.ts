2c239d7f77908531b11c0598600a1c4d0c541459
stuc-bjss <stuart.craigen@bjss.com>
2025-07-07 10:48:28 +0100

    DHSCFT-999: Workaround to temporarily keep quintiles working

--- frontend/fingertips-frontend/components/charts/LineChartOverTime/hooks/useLineChartOverTimeData.ts
+++ frontend/fingertips-frontend/components/charts/LineChartOverTime/hooks/useLineChartOverTimeData.ts
@@ -7,6 +7,7 @@ import { lineChartOverTimeData } from '@/components/charts/LineChartOverTime/hel
 import { useMemo } from 'react';
 import { lineChartOverTimeIsRequired } from '@/components/charts/LineChartOverTime/helpers/lineChartOverTimeIsRequired';
 import { flattenSegment } from '@/lib/healthDataHelpers/flattenSegment';
+import { BenchmarkComparisonMethod } from '@/generated-sources/ft-api-client';
 
 export const useLineChartOverTimeData = () => {
   const searchState = useSearchStateParams();
@@ -24,7 +25,15 @@ export const useLineChartOverTimeData = () => {
 
   return useMemo(() => {
     if (!healthData || !indicatorMetaData || !isRequired) return null;
-    const segmentedData = flattenSegment(healthData, searchState);
+
+    // the api work to allow segmentation for quintiles is not ready yet
+    // so we need to handle it in the old way for now.
+    const isNotQuintiles =
+      healthData.benchmarkMethod !== BenchmarkComparisonMethod.Quintiles;
+
+    const segmentedData = isNotQuintiles
+      ? flattenSegment(healthData, searchState)
+      : healthData;
     return lineChartOverTimeData(indicatorMetaData, segmentedData, searchState);
   }, [healthData, indicatorMetaData, isRequired, searchState]);
 };
