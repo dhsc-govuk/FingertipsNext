2cefec5085ef872c5429b4c1ec087724aebfece6
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-07-10 11:29:17 +0100

    try and mitigate edge runtime error

--- frontend/fingertips-frontend/lib/auth/config.ts
+++ frontend/fingertips-frontend/lib/auth/config.ts
@@ -1,5 +1,4 @@
 import { AuthProvidersFactory } from '@/lib/auth/providers/providerFactory';
-import { validateAccessToken } from '@/lib/auth/validation';
 import { NextAuthConfig } from 'next-auth';
 import 'next-auth/jwt';
 
@@ -25,8 +24,20 @@ export function buildAuthConfig(): NextAuthConfig {
     callbacks: {
       jwt: async ({ token, user: _, account }) => {
         if (account?.access_token) {
-          if (await validateAccessToken(account.access_token)) {
-            return { ...token, accessToken: account?.access_token };
+          // this callback gets invoked on both the node and edge runtime,
+          // but the state where account is present (signin)
+          // only happens on the node runtime
+          // BUT
+          // nextjs still complains that the user api code isn't built for the edge runtime
+          // even if it will never be called
+          if (process.env.NEXT_RUNTIME === 'nodejs') {
+            const { validateAccessToken } = await import(
+              '@/lib/auth/validation'
+            );
+
+            if (await validateAccessToken(account.access_token)) {
+              return { ...token, accessToken: account?.access_token };
+            }
           }
         }
         return token;
