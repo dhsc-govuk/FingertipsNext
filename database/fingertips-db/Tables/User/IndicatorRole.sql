82f0bee47200b3ab0eff6b4970b3eebd48a83e4d
Evan Harris <evan.harris@bjss.com>
2025-07-11 12:10:54 +0100

    Removing constraint from indicator role table

--- database/fingertips-db/Tables/User/IndicatorRole.sql
+++ database/fingertips-db/Tables/User/IndicatorRole.sql
@@ -6,13 +6,3 @@ CREATE TABLE [User].[IndicatorRole]
     [IndicatorKey] SMALLINT NOT NULL,  -- ID of the indicator dimension
 );
 GO;
-
--- Add the foreign key constraint to the IndicatorRole table
-ALTER TABLE [User].[IndicatorRole] WITH CHECK 
-ADD CONSTRAINT [FK_IndicatorRole_IndicatorDimension] 
-FOREIGN KEY ([IndicatorKey])
-REFERENCES [dbo].[IndicatorDimension] ([IndicatorKey]);
-GO;
-
--- Enable the foreign key constraint
-ALTER TABLE [User].[IndicatorRole] CHECK CONSTRAINT [FK_IndicatorRole_IndicatorDimension];
