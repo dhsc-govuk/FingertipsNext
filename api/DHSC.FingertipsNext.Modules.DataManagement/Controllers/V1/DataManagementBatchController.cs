766bf7305982b0ce4f0f45e2c93ca75e8a73fe37
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-07-16 16:10:56 +0100

    Add auth to list batches endpoint

--- api/DHSC.FingertipsNext.Modules.DataManagement/Controllers/V1/DataManagementBatchController.cs
+++ api/DHSC.FingertipsNext.Modules.DataManagement/Controllers/V1/DataManagementBatchController.cs
@@ -1,13 +1,18 @@
+using System.Security.Claims;
+using DHSC.FingertipsNext.Modules.Common.Auth;
 using DHSC.FingertipsNext.Modules.DataManagement.Schemas;
 using DHSC.FingertipsNext.Modules.DataManagement.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace DHSC.FingertipsNext.Modules.DataManagement.Controllers.V1;
 
 [ApiController]
 [Route("batches")]
-public class DataManagementBatchController(IDataManagementService dataManagementService) : ControllerBase
+[Authorize]
+public class DataManagementBatchController(IConfiguration configuration, IIndicatorPermissionsLookupService permissionsLookupService, IDataManagementService dataManagementService) : ControllerBase
 {
     /// <summary>
     ///     Get details of all health data upload batches that are for indicators that you have permissions to modify.
@@ -17,9 +22,21 @@ public class DataManagementBatchController(IDataManagementService dataManagement
     [ProducesResponseType(typeof(Batch[]), StatusCodes.Status200OK)]
     public async Task<IActionResult> ListBatches()
     {
-        // The indicator IDs a user has access to will be determined based on their authorisation roles in a subsequent change.
-        int[] indicatorIds = [];
+        var roleIds = User.FindAll(ClaimTypes.Role).Select(c => c.Value).ToArray();
+        var userRoles = new List<Guid>();
 
-        return new OkObjectResult(await dataManagementService.ListBatches(indicatorIds));
+        foreach (var id in roleIds)
+            if (Guid.TryParse(id, out var roleGuid))
+                userRoles.Add(roleGuid);
+
+        var userIndicatorPermissions = await permissionsLookupService.GetIndicatorsForRoles(userRoles);
+        var indicatorPermissionsArray = userIndicatorPermissions.ToArray();
+
+        var adminRole = configuration["AdminRole"];
+        ArgumentException.ThrowIfNullOrWhiteSpace(adminRole);
+
+        if (indicatorPermissionsArray.Length == 0 && !User.IsInRole(adminRole)) return new ForbidResult();
+
+        return new OkObjectResult(await dataManagementService.ListBatches(indicatorPermissionsArray));
     }
 }
