90ba38f1997c6fa723bdd2966ec703d7f1eab946
Howard May <howard.may@bjss.com>
2025-07-17 16:20:17 +0100

    DHSCFT-1144: Frontend unit test updates

--- frontend/fingertips-frontend/mock/data/expectAnyIndicatorSegment.ts
+++ frontend/fingertips-frontend/mock/data/expectAnyIndicatorSegment.ts
@@ -1,10 +1,12 @@
 import { IndicatorSegment } from '@/generated-sources/ft-api-client';
 import { expectAnyHealthDataPoint } from '@/mock/data/expectAnyHealthDataPoint';
+import { expectAnyAgeData } from '@/mock/data/expectAnyAgeData';
 import { expectAnySexData } from '@/mock/data/expectAnySexData';
 
 export const expectAnyIndicatorSegment = (
   overrides?: Partial<IndicatorSegment>
 ) => ({
+  age: expectAnyAgeData(),
   sex: expectAnySexData(),
   isAggregate: expect.any(Boolean),
   healthData: [expectAnyHealthDataPoint()],
