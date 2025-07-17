5a0e1a18ec684d098fb25da887fbcfc52e8b0568
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2025-07-17 14:32:42 +0100

    DHSCFT-1039_View_unpublished_data_LineCharts_AreasTable_no_RBAC_2 (#758)
    Co-authored-by: Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
    Co-authored-by: williamwillis11 <92571853+williamwillis11@users.noreply.github.com>

--- frontend/fingertips-frontend/components/charts/Inequalities/InequalitiesBarChartAndTable/InequalitiesBarChartAndTable.test.tsx
+++ frontend/fingertips-frontend/components/charts/Inequalities/InequalitiesBarChartAndTable/InequalitiesBarChartAndTable.test.tsx
@@ -21,6 +21,7 @@ import { mockIndicatorWithHealthDataForArea } from '@/mock/data/mockIndicatorWit
 import { mockHealthDataForArea } from '@/mock/data/mockHealthDataForArea';
 import { mockHealthDataPoints } from '@/mock/data/mockHealthDataPoint';
 import { mockDeprivationData } from '@/mock/data/mockDeprivationData';
+import { SessionProvider } from 'next-auth/react';
 
 mockGetIsLoading.mockReturnValue(false);
 mockUsePathname.mockReturnValue('some-mock-path');
@@ -45,9 +46,11 @@ const testRender = async (testHealthData: IndicatorWithHealthDataForArea) => {
   queryClient.setQueryData(['/indicator/41101'], mockIndicatorDocument());
   await act(() =>
     render(
-      <QueryClientProvider client={queryClient}>
-        <InequalitiesBarChartAndTable />
-      </QueryClientProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <InequalitiesBarChartAndTable />
+        </QueryClientProvider>
+      </SessionProvider>
     )
   );
 };
---
+++ frontend/fingertips-frontend/components/charts/hooks/useApiGetHealthDataForAnIndicator.test.ts
@@ -0,0 +1,104 @@
+import {
+  mockGetHealthDataForAnIndicator,
+  mockGetHealthDataForAnIndicatorIncludingUnpublishedData,
+} from '@/mock/utils/mockApiClient';
+// MUST BE AT THE TOP DUE TO HOISTING OF MOCKED MODULES
+import { testRenderWrapper } from '@/mock/utils/testRenderQueryClient';
+import { useApiGetHealthDataForAnIndicator } from './useApiGetHealthDataForAnIndicator';
+import { renderHook, waitFor } from '@testing-library/react';
+import { oneIndicatorRequestParams } from '../helpers/oneIndicatorRequestParams';
+import { SearchParams } from '@/lib/searchStateManager';
+import { QueryClient } from '@tanstack/query-core';
+import {
+  EndPoints,
+  queryKeyFromRequestParams,
+} from '../helpers/queryKeyFromRequestParams';
+
+describe('useApiGetHealthDataForAnIndicator', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  const params = oneIndicatorRequestParams(
+    { [SearchParams.IndicatorsSelected]: ['123'] },
+    []
+  );
+  const queryKeyForHealthData = queryKeyFromRequestParams(
+    EndPoints.HealthDataForAnIndicator,
+    params
+  );
+  const queryKeyForHealthDataIncludingUnpublished = queryKeyFromRequestParams(
+    EndPoints.HealthDataForAnIndicatorIncludingUnpublished,
+    params
+  );
+
+  it('should call the published healthdata endpoint when there is no session', async () => {
+    // arrange
+    const queryClient = new QueryClient();
+    // act
+    renderHook(() => useApiGetHealthDataForAnIndicator(params), {
+      wrapper: testRenderWrapper({}, queryClient, null),
+    });
+
+    // assert
+    await waitFor(() => {
+      expect(mockGetHealthDataForAnIndicator).toHaveBeenCalledWith({
+        ancestorCode: undefined,
+        areaCodes: ['E92000001'],
+        areaType: 'england',
+        benchmarkRefType: 'England',
+        indicatorId: 123,
+      });
+    });
+    await waitFor(() => {
+      const actualData = queryClient.getQueryData([queryKeyForHealthData]);
+      expect(actualData).not.toBeUndefined();
+    });
+
+    expect(
+      mockGetHealthDataForAnIndicatorIncludingUnpublishedData
+    ).not.toHaveBeenCalled();
+    await waitFor(() => {
+      const actualData = queryClient.getQueryData([
+        queryKeyForHealthDataIncludingUnpublished,
+      ]);
+      expect(actualData).toBeUndefined();
+    });
+  });
+
+  it('should call the unpublished healthdata data endpoint if there is a session', async () => {
+    // arrange
+    const queryClient = new QueryClient();
+    // act
+    renderHook(() => useApiGetHealthDataForAnIndicator(params), {
+      wrapper: testRenderWrapper({}, queryClient, {
+        expires: 'some string',
+      }),
+    });
+
+    // assert
+    await waitFor(() => {
+      expect(
+        mockGetHealthDataForAnIndicatorIncludingUnpublishedData
+      ).toHaveBeenCalledWith({
+        ancestorCode: undefined,
+        areaCodes: ['E92000001'],
+        areaType: 'england',
+        benchmarkRefType: 'England',
+        indicatorId: 123,
+      });
+    });
+
+    await waitFor(() => {
+      const actualData = queryClient.getQueryData([
+        queryKeyForHealthDataIncludingUnpublished,
+      ]);
+      expect(actualData).not.toBeUndefined();
+    });
+
+    expect(mockGetHealthDataForAnIndicator).not.toHaveBeenCalled();
+    await waitFor(() => {
+      const actualData = queryClient.getQueryData([queryKeyForHealthData]);
+      expect(actualData).toBeUndefined();
+    });
+  });
+});
