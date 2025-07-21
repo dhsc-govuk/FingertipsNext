61d9a49263b9d3172b87ff44163f317f6e79f6ff
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-07-21 10:35:31 +0100

    Refactored auth checks in DataManagementBatchController.cs

--- api/DHSC.FingertipsNext.Modules.DataManagement/Controllers/V1/DataManagementBatchController.cs
+++ api/DHSC.FingertipsNext.Modules.DataManagement/Controllers/V1/DataManagementBatchController.cs
@@ -12,8 +12,42 @@ namespace DHSC.FingertipsNext.Modules.DataManagement.Controllers.V1;
 [ApiController]
 [Route("batches")]
 [Authorize]
-public class DataManagementBatchController(IConfiguration configuration, IIndicatorPermissionsLookupService permissionsLookupService, IDataManagementService dataManagementService) : ControllerBase
+public class DataManagementBatchController : ControllerBase
 {
+    private readonly string _adminRole;
+    private readonly IDataManagementService _dataManagementService;
+    private readonly IIndicatorPermissionsLookupService _indicatorPermissionsLookupService;
+
+    public DataManagementBatchController(IConfiguration configuration, IIndicatorPermissionsLookupService permissionsLookupService, IDataManagementService dataManagementService)
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+        var adminRole = configuration["AdminRole"];
+        ArgumentException.ThrowIfNullOrWhiteSpace(adminRole);
+        _adminRole = adminRole;
+
+        _indicatorPermissionsLookupService = permissionsLookupService;
+        _dataManagementService = dataManagementService;
+    }
+
+    private static List<Guid> GetRoles(ClaimsPrincipal user)
+    {
+        var userRoleIds = user.FindAll(ClaimTypes.Role).Select(c => c.Value).ToArray();
+        var userRoles = new List<Guid>();
+
+        foreach (var id in userRoleIds)
+            if (Guid.TryParse(id, out var roleGuid))
+                userRoles.Add(roleGuid);
+
+        return userRoles;
+    }
+
+    private async Task<int[]> GetIndicatorIdsFromRoles(IEnumerable<Guid> roles)
+    {
+        var userIndicatorPermissions = await _indicatorPermissionsLookupService.GetIndicatorsForRoles(roles);
+
+        return userIndicatorPermissions.ToArray();
+    }
+
     /// <summary>
     ///     Get details of all health data upload batches that are for indicators that you have permissions to modify.
     /// </summary>
@@ -25,21 +59,10 @@ public class DataManagementBatchController(IConfiguration configuration, IIndica
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> ListBatches()
     {
-        var roleIds = User.FindAll(ClaimTypes.Role).Select(c => c.Value).ToArray();
-        var userRoles = new List<Guid>();
+        var userRoles = GetRoles(User);
+        var userIndicatorPermissions = await GetIndicatorIdsFromRoles(userRoles);
+        if (userIndicatorPermissions.Length == 0 && !User.IsInRole(_adminRole)) return new ForbidResult();
 
-        foreach (var id in roleIds)
-            if (Guid.TryParse(id, out var roleGuid))
-                userRoles.Add(roleGuid);
-
-        var userIndicatorPermissions = await permissionsLookupService.GetIndicatorsForRoles(userRoles);
-        var indicatorPermissionsArray = userIndicatorPermissions.ToArray();
-
-        var adminRole = configuration["AdminRole"];
-        ArgumentException.ThrowIfNullOrWhiteSpace(adminRole);
-
-        if (indicatorPermissionsArray.Length == 0 && !User.IsInRole(adminRole)) return new ForbidResult();
-
-        return new OkObjectResult(await dataManagementService.ListBatches(indicatorPermissionsArray));
+        return new OkObjectResult(await _dataManagementService.ListBatches(userIndicatorPermissions));
     }
 }
