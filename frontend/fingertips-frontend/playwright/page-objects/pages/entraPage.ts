9e7da8fe73cd474c1097b1f6c3467b008892abbe
williamwillis11 <92571853+williamwillis11@users.noreply.github.com>
2025-07-21 12:52:56 +0100

    remove unused entraPage functions

--- frontend/fingertips-frontend/playwright/page-objects/pages/entraPage.ts
+++ frontend/fingertips-frontend/playwright/page-objects/pages/entraPage.ts
@@ -5,28 +5,9 @@ export default class EntraPage extends BasePage {
   readonly entraWelcomeText = 'Sign in to access ';
   readonly emailAriaLabel = 'Enter your email address';
   readonly passwordAriaLabel = 'Enter the password for ';
-  readonly incorrectPasswordMessage = `We couldn't find an account with this email address or password.`;
-  readonly unRegisteredEmailMessage = `We couldn't find an account with this email address.`;
-  readonly createAccountText = 'Create account';
 
   async checkOnEntraSignInPage() {
     await expect(this.page.getByText(this.entraWelcomeText)).toBeVisible();
-  }
-
-  async checkOnCreateAccountPage() {
-    await expect(this.page.getByText(this.createAccountText)).toBeVisible();
-  }
-
-  async checkIncorrectPasswordMessageIsDisplayed() {
-    await expect(
-      this.page.getByText(this.incorrectPasswordMessage)
-    ).toBeVisible();
-  }
-
-  async checkUnRegisteredEmailMessageIsDisplayed() {
-    await expect(
-      this.page.getByText(this.unRegisteredEmailMessage)
-    ).toBeVisible();
   }
 
   async clickNext() {
@@ -38,12 +19,6 @@ export default class EntraPage extends BasePage {
   async clickSignIn() {
     await this.clickAndAwaitLoadingComplete(
       this.page.getByRole('button', { name: 'Sign in' })
-    );
-  }
-
-  async clickCreateAccount() {
-    await this.clickAndAwaitLoadingComplete(
-      this.page.getByRole('link', { name: 'No account? Create one' })
     );
   }
 
