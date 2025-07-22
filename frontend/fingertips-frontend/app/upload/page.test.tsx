4762254775ea8c9c0cb77a7ddbfaf901a47bdfc1
Gareth Allan <157592212+gareth-allan-bjss@users.noreply.github.com>
2025-07-22 11:56:12 +0100

    Remove unnecessary mocks in upload page test

--- frontend/fingertips-frontend/app/upload/page.test.tsx
+++ frontend/fingertips-frontend/app/upload/page.test.tsx
@@ -1,25 +1,12 @@
-// MUST BE AT THE TOP DUE TO HOISTING OF MOCKED MODULES
-import { mockAuth } from '@/mock/utils/mockAuth';
 import { BatchesApi } from '@/generated-sources/ft-api-client';
 import { ApiClientFactory } from '@/lib/apiClient/apiClientFactory';
 import { mockBatch } from '@/mock/data/mockBatch';
-import { Session } from 'next-auth';
 import { mockDeep } from 'vitest-mock-extended';
 import UploadPage from './page';
-import { getJWT } from '@/lib/auth/getJWT';
-
-vi.mock('@/lib/auth/getJWT', () => {
-  return { getJWT: vi.fn() };
-});
 
 const mockAuthenticatedBatchesApi = mockDeep<BatchesApi>();
 ApiClientFactory.getAuthenticatedBatchesApiClient = () =>
   Promise.resolve(mockAuthenticatedBatchesApi);
-
-const expectedAccessToken = 'access-token';
-const mockSession = mockDeep<Session>();
-mockAuth.mockResolvedValue(mockSession);
-vi.mocked(getJWT).mockResolvedValue({ accessToken: expectedAccessToken });
 
 describe('Upload page component', () => {
   it('should make an API call to fetch batches', async () => {
