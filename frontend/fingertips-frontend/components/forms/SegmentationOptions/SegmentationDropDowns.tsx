1ae0c345b0558f8dc8f6170a8cf83d462d0298d8
Stuart <stuart.craigen@bjss.com>
2025-07-22 14:34:09 +0100

    DHSCFT-965: spine chart segmentation - pr comments

--- frontend/fingertips-frontend/components/forms/SegmentationOptions/SegmentationDropDowns.tsx
+++ frontend/fingertips-frontend/components/forms/SegmentationOptions/SegmentationDropDowns.tsx
@@ -7,7 +7,6 @@ import { useSearchStateParams } from '@/components/hooks/useSearchStateParams';
 import { SearchParams, SearchStateManager } from '@/lib/searchStateManager';
 import { valueOrDefault } from '@/components/forms/SegmentationOptions/helpers/valueOrDefault';
 import { usePathname } from 'next/navigation';
-
 import { SegmentationId } from '@/lib/common-types';
 
 interface SegmentationDropDownsProps {
