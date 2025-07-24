08ccf8fd315d1914b0ef40887b8d944e13522287
stuc-bjss <stuart.craigen@bjss.com>
2025-07-24 10:45:35 +0100

    Feature/dhscft 1059 chart titles segmentation (#787)
    Co-authored-by: GitHub Actions Bot <actions@github.com>

--- frontend/fingertips-frontend/lib/healthDataHelpers/segmentName.ts
+++ frontend/fingertips-frontend/lib/healthDataHelpers/segmentName.ts
@@ -1,11 +1,11 @@
 import { IndicatorSegment } from '@/generated-sources/ft-api-client';
 import { segmentNameFromInfo } from '@/lib/healthDataHelpers/segmentNameFromInfo';
-import { SegmentationId } from '@/lib/common-types';
+import { SegmentationId, SegmentInfo } from '@/lib/common-types';
 
 export const segmentName = (segment: IndicatorSegment) => {
-  const info = {
-    [SegmentationId.Sex]: segment.sex.value,
-    [SegmentationId.Age]: '', // segment.age.value,
+  const info: SegmentInfo = {
+    [SegmentationId.Sex]: segment.sex?.value ?? '',
+    [SegmentationId.Age]: segment.age?.value ?? '',
     [SegmentationId.Frequency]: '', // segment.frequency.value,
   };
 
