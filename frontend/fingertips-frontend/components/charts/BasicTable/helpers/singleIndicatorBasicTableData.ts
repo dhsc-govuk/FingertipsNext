f5d57ad8b75e2ead3330bc47bda49af1d72a1d80
stuc-bjss <stuart.craigen@bjss.com>
2025-07-25 14:03:17 +0100

    DHSCFT-1077: Basic table date period labels (#793)
    Co-authored-by: GitHub Actions Bot <actions@github.com>

--- frontend/fingertips-frontend/components/charts/BasicTable/helpers/singleIndicatorBasicTableData.ts
+++ frontend/fingertips-frontend/components/charts/BasicTable/helpers/singleIndicatorBasicTableData.ts
@@ -2,30 +2,37 @@ import { BasicTableData } from '@/components/charts/BasicTable/basicTable.types'
 import { sortHealthDataPointsByDescendingYear } from '@/lib/chartHelpers/chartHelpers';
 import { segmentName } from '@/lib/healthDataHelpers/segmentName';
 import { filterDefined } from '@/lib/chartHelpers/filterDefined';
-import { HealthDataForArea } from '@/generated-sources/ft-api-client';
+import {
+  Frequency,
+  HealthDataForArea,
+} from '@/generated-sources/ft-api-client';
 import { IndicatorDocument } from '@/lib/search/searchTypes';
 import { indicatorSegmentsSorted } from '@/lib/healthDataHelpers/indicatorSegmentsSorted';
+import { formatDatePointLabel } from '@/lib/timePeriodHelpers/getTimePeriodLabels';
 
 export const singleIndicatorBasicTableData = (
   area: HealthDataForArea,
-  indicatorMetaData: IndicatorDocument
+  indicatorMetaData: IndicatorDocument,
+  frequency: Frequency
 ): BasicTableData[] | null => {
   const { indicatorSegments = [] } = area;
   const sortedSegments = indicatorSegmentsSorted(indicatorSegments);
 
   return sortedSegments
     .map((segment) => {
-      const byYear = sortHealthDataPointsByDescendingYear(segment.healthData);
-      const point = byYear.at(0);
+      const byDatePeriod = sortHealthDataPointsByDescendingYear(
+        segment.healthData
+      );
+      const point = byDatePeriod.at(0);
       const segName = segmentName(segment);
 
       if (!point) return;
 
-      const { count, value, trend } = point;
+      const { count, value, trend, datePeriod } = point;
       const data: BasicTableData = {
         indicatorId: Number(indicatorMetaData.indicatorID),
         indicatorName: `${indicatorMetaData.indicatorName} (${segName})`,
-        period: point.year.toString(),
+        period: formatDatePointLabel(datePeriod, frequency, 1),
         unitLabel: indicatorMetaData.unitLabel,
         areaName: area.areaName,
         areaCode: area.areaCode,
