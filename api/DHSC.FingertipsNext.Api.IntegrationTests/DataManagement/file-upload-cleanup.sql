76d988dd67afd61a681c2e7984c615514f544f4b
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-07-28 15:06:18 +0100

    update file upload clean up

--- api/DHSC.FingertipsNext.Api.IntegrationTests/DataManagement/file-upload-cleanup.sql
+++ api/DHSC.FingertipsNext.Api.IntegrationTests/DataManagement/file-upload-cleanup.sql
@@ -1,3 +1,8 @@
+DELETE HealthMeasure
+FROM dbo.HealthMeasure hm
+         INNER JOIN DataManagement.Batch b ON hm.BatchId = b.batchId
+WHERE b.OriginalFileName = 'file-upload-integration-test.csv'
+
 DELETE
 FROM DataManagement.Batch
 WHERE OriginalFileName = 'file-upload-integration-test.csv'
