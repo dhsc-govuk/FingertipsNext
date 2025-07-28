38943d50de046f78cf98e45481fef669cceeba2c
adawari-thomas-bjss <adawari.thomas@bjss.com>
2025-07-28 15:43:13 +0100

    remove not needed sql

--- api/DHSC.FingertipsNext.Api.IntegrationTests/DataManagement/file-upload-cleanup.sql
+++ api/DHSC.FingertipsNext.Api.IntegrationTests/DataManagement/file-upload-cleanup.sql
@@ -1,8 +1,3 @@
-DELETE HealthMeasure
-FROM dbo.HealthMeasure hm
-         INNER JOIN DataManagement.Batch b ON hm.BatchId = b.batchId
-WHERE b.OriginalFileName = 'file-upload-integration-test.csv'
-
 DELETE
 FROM DataManagement.Batch
 WHERE OriginalFileName = 'file-upload-integration-test.csv'
