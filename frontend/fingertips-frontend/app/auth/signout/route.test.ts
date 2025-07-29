cbe1a83580966b9282567467e7ff80b8dd14a43f
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2025-07-29 12:07:24 +0100

    refactor route tests

--- frontend/fingertips-frontend/app/auth/signout/route.test.ts
+++ frontend/fingertips-frontend/app/auth/signout/route.test.ts
@@ -8,7 +8,10 @@ import { mockRedirect } from '@/mock/utils/mockNextNavigation';
 import { signOutHandler } from '@/lib/auth/handlers';
 import { GET } from '@/app/auth/signout/route';
 import { NextRequest } from 'next/server';
-import { buildLogoutURLWithRedirect } from '@/lib/auth/providers/fingertipsAuthProvider';
+import {
+  FTA_SIGNOUT_REDIRECT_PARAM,
+  getLogoutEndpoint,
+} from '@/lib/auth/providers/fingertipsAuthProvider';
 import { Mock } from 'vitest';
 import { mockSession } from '@/mock/utils/mockAuth';
 
@@ -25,7 +28,7 @@ vi.mock('@/lib/auth/providers/fingertipsAuthProvider', async () => {
 
   return {
     ...originalModule,
-    buildLogoutURLWithRedirect: vi.fn(),
+    getLogoutEndpoint: vi.fn(),
   };
 });
 
@@ -48,7 +51,7 @@ describe('sign out route handler', () => {
 
   it('should redirect to origin if logout endpoint not set', async () => {
     mockAuth.mockResolvedValue(undefined);
-    (buildLogoutURLWithRedirect as Mock).mockReturnValue(undefined);
+    (getLogoutEndpoint as Mock).mockReturnValue(undefined);
 
     await GET(req);
 
@@ -57,13 +60,12 @@ describe('sign out route handler', () => {
 
   it('should redirect to logout url if logout endpoint set', async () => {
     mockAuth.mockResolvedValue(undefined);
-    const expectedLogoutEndpoint = 'https://some-external-url.foobar/';
-    (buildLogoutURLWithRedirect as Mock).mockReturnValue(
-      expectedLogoutEndpoint
-    );
+    const logoutEndpoint = 'https://some-external-url.foobar/';
+    (getLogoutEndpoint as Mock).mockReturnValue(logoutEndpoint);
+    const expectedLogoutURL = `${logoutEndpoint}?${FTA_SIGNOUT_REDIRECT_PARAM}=${origin}`;
 
     await GET(req);
 
-    expect(mockRedirect).toHaveBeenCalledWith(expectedLogoutEndpoint);
+    expect(mockRedirect).toHaveBeenCalledWith(expectedLogoutURL);
   });
 });
